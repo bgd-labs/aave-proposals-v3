be8b6a568a7b36e2f9bab47e9e764be30887a113
Rozengarden <github@rozengarden.eu>
2023-10-29 20:24:26 +0100

    generator output + filling in .md

---
+++ src/20231029_AaveV3Ethereum_ACIPhaseII/AaveV3Ethereum_ACIPhaseII_20231029.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+ * @title ACI Phase II
+ * @author Alice Rozengarden (@Rozengarden - Aave-chan initiative)
+ * - Snapshot: https://signal.aave.com/#/proposal/0x04e7059fc5b2c33d4e4554d68d27ef67c1f6d9d310b07493116bdfbf15c25bbc
+ * - Discussion: https://governance.aave.com/t/arfc-aci-phase-ii/15138
+ */
+contract AaveV3Ethereum_ACIPhaseII_20231029 is IProposalGenericExecutor {
+
+}
