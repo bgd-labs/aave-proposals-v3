d5771dc6ba62246c2ef5e81a66c13973047904ee
yonikesel <yonikesel@chaoslabs.xyz>
2023-11-03 13:03:26 +0200

    Update payload address

--- src/20231030_AaveV2Ethereum_AaveV2EthereumLTReduction/AaveV2EthereumLTReduction_20231030.s.sol
+++ src/20231030_AaveV2Ethereum_AaveV2EthereumLTReduction/AaveV2EthereumLTReduction_20231030.s.sol
@@ -36,7 +36,7 @@ contract CreateProposal is EthereumScript {
     // compose actions for validation
     IPayloadsControllerCore.ExecutionAction[]
       memory actionsEthereum = new IPayloadsControllerCore.ExecutionAction[](1);
-    actionsEthereum[0] = GovV3Helpers.buildAction(address(0));
+    actionsEthereum[0] = GovV3Helpers.buildAction(0xc62cAb55a4376F916D107D7e09356E9799c090fa);
     payloads[0] = GovV3Helpers.buildMainnetPayload(vm, actionsEthereum);
 
     // create proposal
