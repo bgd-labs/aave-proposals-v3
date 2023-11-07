d9dccbbc07fa259fabb29c9d20e305e692e79b75
efecarranza <fcarr004@fiu.edu>
2023-11-06 19:21:59 -0500

    feat: add part 2 of aave funding

---
+++ src/20231106_AaveV3Ethereum_AaveFundingUpdates/AaveFundingUpdates.md
@@ -0,0 +1,38 @@
+---
+title: "Aave Funding Updates"
+author: "TokenLogic, efecarranza.eth"
+discussions: "https://governance.aave.com/t/arfc-aave-funding-update/15194"
+---
+
+## Simple Summary
+
+This AIP aims to consolidate Aave DAO's stablecoin holdings. It is divided into different parts that are detailed below. Part one is depositing DAI into aEthDAI, part two is migrating aDAI and aUSDT into aEthDAI and aEthUSDT, part three bridges assets from Polygon to Mainnet and part four swaps some holdings into GHO.
+
+## Motivation
+
+This proposal intends to ensure the DAO is well capitalised. A separate DAO budget publication will be shared during Q4 2023.
+
+The DAO has sufficient USDC, DAI and USDT deposited across the various instances of Aave Protocol to support existing streams and the AURA purchase 3.
+
+However, with the emergence of new service provider proposal expected, the DAO needs to adjust its stable coin holdings. This publication supports transferring additional funds from Polygon to Mainnet and acquiring GHO on the market to ensure the DAO is well capitalised for the next 6 months. It also aims to move assets from the old V2 to the newer V3 aToken.
+
+## Specification
+
+- Deposit 1M units of USDC from Treasury into Aave v3 (received from Polygon bridge)
+- Deposit 750k units of USDT from Treasury into Aave v3 (received from Polygon bridge)
+- Deposit 500k units of DAI from Treasury into Aave v3 (received from Polygon bridge)
+
+Swap the following asset holdings to GHO:
+
+- 700k units of USDC held in Treasury (received from Polygon bridge)
+
+## References
+
+- Implementation: [AaveV3Ethereum](https://github.com/bgd-labs/aave-proposals-v3/blob/main/src/20231106_AaveV3Ethereum_AaveFundingUpdates/AaveV3Ethereum_AaveFundingUpdates_20231106.sol)
+- Tests: [AaveV3Ethereum](https://github.com/bgd-labs/aave-proposals-v3/blob/main/src/20231106_AaveV3Ethereum_AaveFundingUpdates/AaveV3Ethereum_AaveFundingUpdates_20231106.t.sol)
+- [Snapshot](https://snapshot.org/#/aave.eth/proposal/0x099f88e1728760952be26fcb8fc99b26c29336e6a109820b391751b108399ee5)
+- [Discussion](https://governance.aave.com/t/arfc-aave-funding-update/15194)
+
+## Copyright
+
+Copyright and related rights waived via [CC0](https://creativecommons.org/publicdomain/zero/1.0/).
---
+++ src/20231106_AaveV3Ethereum_AaveFundingUpdates/AaveV3Ethereum_AaveFundingUpdates_20231106.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IERC20} from 'solidity-utils/contracts/oz-common/interfaces/IERC20.sol';
+import {SafeERC20} from 'solidity-utils/contracts/oz-common/SafeERC20.sol';
+import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
+import {MiscEthereum} from 'aave-address-book/MiscEthereum.sol';
+import {AaveSwapper} from 'aave-helpers/swaps/AaveSwapper.sol';
+import {IProposalGenericExecutor} from 'aave-helpers/interfaces/IProposalGenericExecutor.sol';
+
+/**
+ * @title Aave Funding Updates
+ * @author efecarranza.eth
+ * - Snapshot: https://snapshot.org/#/aave.eth/proposal/0x099f88e1728760952be26fcb8fc99b26c29336e6a109820b391751b108399ee5
+ * - Discussion: https://governance.aave.com/t/arfc-aave-funding-update/15194
+ */
+contract AaveV3Ethereum_AaveFundingUpdates_20231106 is IProposalGenericExecutor {
+  using SafeERC20 for IERC20;
+
+  AaveSwapper public constant SWAPPER = AaveSwapper(MiscEthereum.AAVE_SWAPPER);
+
+  uint256 public constant USDC_TO_SWAP = 700_000e18;
+  uint256 public constant USDC_TO_DEPOSIT = 1_000_000e18;
+  uint256 public constant USDT_TO_DEPOSIT = 750_000e18;
+  uint256 public constant DAI_TO_DEPOSIT = 500_000e18;
+
+  address public constant MILKMAN = 0x11C76AD590ABDFFCD980afEC9ad951B160F02797;
+  address public constant PRICE_CHECKER = 0xe80a1C615F75AFF7Ed8F08c9F21f9d00982D666c;
+
+  function execute() external {
+    // Deposit into V3
+
+    AaveV3Ethereum.COLLECTOR.transfer(
+      AaveV3EthereumAssets.USDC_UNDERLYING,
+      address(this),
+      USDC_TO_DEPOSIT
+    );
+    IERC20(AaveV3EthereumAssets.USDC_UNDERLYING).forceApprove(
+      address(AaveV3Ethereum.POOL),
+      USDC_TO_DEPOSIT
+    );
+    AaveV3Ethereum.POOL.deposit(
+      AaveV3EthereumAssets.USDC_UNDERLYING,
+      USDC_TO_DEPOSIT,
+      address(AaveV3Ethereum.COLLECTOR),
+      0
+    );
+
+    AaveV3Ethereum.COLLECTOR.transfer(
+      AaveV3EthereumAssets.USDT_UNDERLYING,
+      address(this),
+      USDT_TO_DEPOSIT
+    );
+    IERC20(AaveV3EthereumAssets.USDT_UNDERLYING).forceApprove(
+      address(AaveV3Ethereum.POOL),
+      USDT_TO_DEPOSIT
+    );
+    AaveV3Ethereum.POOL.deposit(
+      AaveV3EthereumAssets.USDT_UNDERLYING,
+      USDT_TO_DEPOSIT,
+      address(AaveV3Ethereum.COLLECTOR),
+      0
+    );
+
+    AaveV3Ethereum.COLLECTOR.transfer(
+      AaveV3EthereumAssets.DAI_UNDERLYING,
+      address(this),
+      DAI_TO_DEPOSIT
+    );
+    IERC20(AaveV3EthereumAssets.DAI_UNDERLYING).forceApprove(
+      address(AaveV3Ethereum.POOL),
+      DAI_TO_DEPOSIT
+    );
+    AaveV3Ethereum.POOL.deposit(
+      AaveV3EthereumAssets.DAI_UNDERLYING,
+      DAI_TO_DEPOSIT,
+      address(AaveV3Ethereum.COLLECTOR),
+      0
+    );
+
+    // Swap USDC
+
+    SWAPPER.swap(
+      MILKMAN,
+      PRICE_CHECKER,
+      AaveV3EthereumAssets.USDC_UNDERLYING,
+      AaveV3EthereumAssets.GHO_UNDERLYING,
+      AaveV3EthereumAssets.USDC_ORACLE,
+      AaveV3EthereumAssets.GHO_ORACLE,
+      address(AaveV3Ethereum.COLLECTOR),
+      USDC_TO_SWAP,
+      100
+    );
+  }
+}
