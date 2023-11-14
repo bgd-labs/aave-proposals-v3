9669b83676e92ac8733b6274393bc7e2ec28ca8a
Rozengarden <github@rozengarden.eu>
2023-11-14 19:02:04 +0100

    Fix base test

--- src/20231113_Multi_IncreaseStablecoinOptimalBorrowRates/AaveV3Base_IncreaseStablecoinOptimalBorrowRates_20231113.t.sol
+++ src/20231113_Multi_IncreaseStablecoinOptimalBorrowRates/AaveV3Base_IncreaseStablecoinOptimalBorrowRates_20231113.t.sol
@@ -15,7 +15,7 @@ contract AaveV3Base_IncreaseStablecoinOptimalBorrowRates_20231113_Test is Protoc
   AaveV3Base_IncreaseStablecoinOptimalBorrowRates_20231113 internal proposal;
 
   function setUp() public {
-    vm.createSelectFork(vm.rpcUrl('base'), 6558600);
+    vm.createSelectFork(vm.rpcUrl('base'), 6580110);
     proposal = new AaveV3Base_IncreaseStablecoinOptimalBorrowRates_20231113();
   }
 
