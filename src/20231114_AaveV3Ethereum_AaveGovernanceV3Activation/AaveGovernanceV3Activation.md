25b3b47801f1f989e0c2689f3169a792d4ac085b
Rustem Kurmaev <rustboyar@gmail.com>
2023-11-14 16:22:01 +0100

    Governance V3 Activation

---
+++ src/20231114_AaveV3Ethereum_AaveGovernanceV3Activation/AaveGovernanceV3Activation.md
@@ -0,0 +1,22 @@
+---
+title: "Aave Governance V3 Activation"
+author: "BGD Labs (@bgdlabs)"
+discussions: "https://governance.aave.com/t/bgd-aave-governance-v3-activation-plan/14993/10"
+---
+
+## Simple Summary
+
+## Motivation
+
+## Specification
+
+## References
+
+- Implementation: [AaveV3Ethereum]()
+- Tests: [AaveV3Ethereum]()
+- [Snapshot](TODO)
+- [Discussion](https://governance.aave.com/t/bgd-aave-governance-v3-activation-plan/14993/10)
+
+## Copyright
+
+Copyright and related rights waived via [CC0](https://creativecommons.org/publicdomain/zero/1.0/).
---
+++ src/20231114_AaveV3Ethereum_AaveGovernanceV3Activation/AaveGovernanceV3Activation_20231114.s.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
+import {EthereumScript} from 'aave-helpers/ScriptUtils.sol';
+import {AaveV3Ethereum_AaveGovernanceV3Activation_20231114} from './AaveV3Ethereum_AaveGovernanceV3Activation_20231114.sol';
+
+/**
+ * @dev Create Long Proposal
+ * command: make deploy-ledger contract=src/20231114_AaveV3Ethereum_AaveGovernanceV3Activation/AaveGovernanceV3Activation_20231114.s.sol:CreateLongProposal chain=mainnet
+ */
+contract CreateLongProposal is EthereumScript {
+  function run() external {
+    // create payloads
+    GovHelpers.Payload[] memory payloads = new GovHelpers.Payload[](1);
+    payloads[0] = GovHelpers.buildMainnet(address(0));
+
+    // create proposal
+    vm.startBroadcast();
+    GovHelpers.createProposal(
+      payloads,
+      GovHelpers.ipfsHashFile(
+        vm,
+        'src/20231114_AaveV3Ethereum_AaveGovernanceV3Activation/AaveGovernanceV3Activation.md'
+      ),
+      AaveGovernanceV2.LONG_EXECUTOR
+    );
+  }
+}
+
+/**
+ * @dev Create Proposal
+ * command: make deploy-ledger contract=src/20231114_AaveV3Ethereum_AaveGovernanceV3Activation/AaveGovernanceV3Activation_20231114.s.sol:CreateProposal chain=mainnet
+ */
+contract CreateProposal is EthereumScript {
+  function run() external {
+    // create payloads
+    GovHelpers.Payload[] memory payloads = new GovHelpers.Payload[](1);
+    payloads[0] = GovHelpers.buildMainnet(address(0));
+
+    // create proposal
+    vm.startBroadcast();
+    GovHelpers.createProposal(
+      payloads,
+      GovHelpers.ipfsHashFile(
+        vm,
+        'src/20231114_AaveV3Ethereum_AaveGovernanceV3Activation/AaveGovernanceV3Activation.md'
+      )
+    );
+  }
+}
