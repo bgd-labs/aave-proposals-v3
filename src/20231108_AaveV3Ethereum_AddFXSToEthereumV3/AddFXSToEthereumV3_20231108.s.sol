328484fae06941fb39b958757e17dedf45733e43
marczeller <pierarte@gmail.com>
2023-11-16 13:27:52 +0100

    deployed payload

--- src/20231108_AaveV3Ethereum_AddFXSToEthereumV3/AddFXSToEthereumV3_20231108.s.sol
+++ src/20231108_AaveV3Ethereum_AddFXSToEthereumV3/AddFXSToEthereumV3_20231108.s.sol
@@ -36,7 +36,7 @@ contract CreateProposal is EthereumScript {
     // compose actions for validation
     IPayloadsControllerCore.ExecutionAction[]
       memory actionsEthereum = new IPayloadsControllerCore.ExecutionAction[](1);
-    actionsEthereum[0] = GovV3Helpers.buildAction(address(0));
+    actionsEthereum[0] = GovV3Helpers.buildAction(0x333d00b056a72787e4432fbcD075b32953677AEd);
     payloads[0] = GovV3Helpers.buildMainnetPayload(vm, actionsEthereum);
 
     // create proposal
