1776f346393e496b4a9b7b26ff57f6f7eb978d0f
Mark Hinschberger <foodaka@users.noreply.github.com>
2023-11-28 15:16:30 +0000

    update payload and aip text

--- src/20231122_AaveV3Ethereum_GHO_Incident_Report/GHO_Incident_Report_20231122.s.sol
+++ src/20231122_AaveV3Ethereum_GHO_Incident_Report/GHO_Incident_Report_20231122.s.sol
@@ -36,7 +36,9 @@ contract CreateProposal is EthereumScript {
     // compose actions for validation
     IPayloadsControllerCore.ExecutionAction[]
       memory actionsEthereum = new IPayloadsControllerCore.ExecutionAction[](1);
-    actionsEthereum[0] = GovV3Helpers.buildAction(address(0));
+    actionsEthereum[0] = GovV3Helpers.buildAction(
+      address(0x5107F3274893dAF384241370CB84c9991780A786)
+    );
     payloads[0] = GovV3Helpers.buildMainnetPayload(vm, actionsEthereum);
 
     // create proposal
