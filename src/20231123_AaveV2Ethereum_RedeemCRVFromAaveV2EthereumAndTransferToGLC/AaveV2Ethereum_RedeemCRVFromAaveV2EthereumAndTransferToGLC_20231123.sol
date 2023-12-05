9fa61c826cb2662eb27a688b01c59b78e83d60b4
efecarranza <fcarr004@fiu.edu>
2023-12-05 09:27:05 -0500

    feat: update README and numbers to latest block

--- src/20231123_AaveV2Ethereum_RedeemCRVFromAaveV2EthereumAndTransferToGLC/AaveV2Ethereum_RedeemCRVFromAaveV2EthereumAndTransferToGLC_20231123.sol
+++ src/20231123_AaveV2Ethereum_RedeemCRVFromAaveV2EthereumAndTransferToGLC/AaveV2Ethereum_RedeemCRVFromAaveV2EthereumAndTransferToGLC_20231123.sol
@@ -4,13 +4,14 @@ pragma solidity 0.8.19;
 
 import {IERC20} from 'solidity-utils/contracts/oz-common/interfaces/IERC20.sol';
 import {AaveV2Ethereum, AaveV2EthereumAssets} from 'aave-address-book/AaveV2Ethereum.sol';
+import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
 import {IProposalGenericExecutor} from 'aave-helpers/interfaces/IProposalGenericExecutor.sol';
 
 /**
  * @title Redeem CRV from AaveV2Ethereum and Transfer to GLC
  * @author efecarranza.eth
- * - Snapshot: TODO
- * - Discussion: TODO
+ * - Snapshot: https://snapshot.org/#/aave.eth/proposal/0xf92c5647c7f60a4a3db994b4953fc4408f5946cafdc0cebcd4c5924f40e04d36
+ * - Discussion: https://governance.aave.com/t/arfc-deploy-acrv-crv-to-vecrv/11628
  */
 contract AaveV2Ethereum_RedeemCRVFromAaveV2EthereumAndTransferToGLC_20231123 is
   IProposalGenericExecutor
@@ -25,6 +26,12 @@ contract AaveV2Ethereum_RedeemCRVFromAaveV2EthereumAndTransferToGLC_20231123 is
     );
 
     AaveV2Ethereum.COLLECTOR.transfer(
+      AaveV3EthereumAssets.CRV_A_TOKEN,
+      address(this),
+      IERC20(AaveV3EthereumAssets.CRV_A_TOKEN).balanceOf(address(AaveV3Ethereum.COLLECTOR))
+    );
+
+    AaveV2Ethereum.COLLECTOR.transfer(
       AaveV2EthereumAssets.CRV_UNDERLYING,
       address(this),
       IERC20(AaveV2EthereumAssets.CRV_UNDERLYING).balanceOf(address(AaveV2Ethereum.COLLECTOR))
@@ -32,6 +39,12 @@ contract AaveV2Ethereum_RedeemCRVFromAaveV2EthereumAndTransferToGLC_20231123 is
 
     AaveV2Ethereum.POOL.withdraw(
       AaveV2EthereumAssets.CRV_UNDERLYING,
+      type(uint256).max,
+      address(this)
+    );
+
+    AaveV3Ethereum.POOL.withdraw(
+      AaveV3EthereumAssets.CRV_UNDERLYING,
       type(uint256).max,
       address(this)
     );
