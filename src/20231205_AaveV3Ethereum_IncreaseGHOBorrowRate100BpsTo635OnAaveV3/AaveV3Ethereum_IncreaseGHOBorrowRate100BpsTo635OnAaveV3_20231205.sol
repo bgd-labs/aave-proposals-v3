01dfcbd8fbca5186c039ff645cbee670bd2ad8c7
Rozengarden <github@rozengarden.eu>
2023-12-05 22:41:45 +0100

    test & Test result
    + smoll modif to payload

--- src/20231205_AaveV3Ethereum_IncreaseGHOBorrowRate100BpsTo635OnAaveV3/AaveV3Ethereum_IncreaseGHOBorrowRate100BpsTo635OnAaveV3_20231205.sol
+++ src/20231205_AaveV3Ethereum_IncreaseGHOBorrowRate100BpsTo635OnAaveV3/AaveV3Ethereum_IncreaseGHOBorrowRate100BpsTo635OnAaveV3_20231205.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {IProposalGenericExecutor} from 'aave-helpers/interfaces/IProposalGenericExecutor.sol';
 import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
 
 /**
@@ -10,9 +9,7 @@ import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethe
  * - Snapshot: No snapshot for direct to AIP
  * - Discussion: https://governance.aave.com/t/arfc-increase-gho-borrow-rate-100-bps-to-6-35-on-aave-v3/15744
  */
-contract AaveV3Ethereum_IncreaseGHOBorrowRate100BpsTo635OnAaveV3_20231205 is
-  IProposalGenericExecutor
-{
+contract AaveV3Ethereum_IncreaseGHOBorrowRate100BpsTo635OnAaveV3_20231205 {
   address public constant INTEREST_RATE_STRATEGY = 0x00524e8E4C5FD2b8D8aa1226fA16b39Cad69B8A0;
   function execute() external {
     AaveV3Ethereum.POOL_CONFIGURATOR.setReserveInterestRateStrategyAddress(
--- src/20231205_AaveV3Ethereum_IncreaseGHOBorrowRate100BpsTo635OnAaveV3/AaveV3Ethereum_IncreaseGHOBorrowRate100BpsTo635OnAaveV3_20231205.t.sol
+++ src/20231205_AaveV3Ethereum_IncreaseGHOBorrowRate100BpsTo635OnAaveV3/AaveV3Ethereum_IncreaseGHOBorrowRate100BpsTo635OnAaveV3_20231205.t.sol
@@ -1,12 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {AaveV3Ethereum} from 'aave-address-book/AaveV3Ethereum.sol';
+import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
+import {IAaveV3ConfigEngine} from 'aave-helpers/v3-config-engine/IAaveV3ConfigEngine.sol';
+import {GovV3Helpers} from 'aave-helpers/GovV3Helpers.sol';
+import {AaveV3PayloadEthereum} from 'aave-helpers/v3-config-engine/AaveV3PayloadEthereum.sol';
+import {EngineFlags} from 'aave-helpers/v3-config-engine/EngineFlags.sol';
 
 import 'forge-std/Test.sol';
 import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
 import {AaveV3Ethereum_IncreaseGHOBorrowRate100BpsTo635OnAaveV3_20231205} from './AaveV3Ethereum_IncreaseGHOBorrowRate100BpsTo635OnAaveV3_20231205.sol';
 
+contract mock_proposal is AaveV3PayloadEthereum {
+  function capsUpdates() public pure override returns (IAaveV3ConfigEngine.CapsUpdate[] memory) {
+    IAaveV3ConfigEngine.CapsUpdate[] memory capsUpdate = new IAaveV3ConfigEngine.CapsUpdate[](1);
+
+    capsUpdate[0] = IAaveV3ConfigEngine.CapsUpdate({
+      asset: AaveV3EthereumAssets.GHO_UNDERLYING,
+      supplyCap: EngineFlags.KEEP_CURRENT,
+      borrowCap: 50_000_000
+    });
+
+    return capsUpdate;
+  }
+}
 /**
  * @dev Test for AaveV3Ethereum_IncreaseGHOBorrowRate100BpsTo635OnAaveV3_20231205
  * command: make test-contract filter=AaveV3Ethereum_IncreaseGHOBorrowRate100BpsTo635OnAaveV3_20231205
@@ -15,16 +32,21 @@ contract AaveV3Ethereum_IncreaseGHOBorrowRate100BpsTo635OnAaveV3_20231205_Test i
   ProtocolV3TestBase
 {
   AaveV3Ethereum_IncreaseGHOBorrowRate100BpsTo635OnAaveV3_20231205 internal proposal;
-
+  mock_proposal internal mock;
   function setUp() public {
-    vm.createSelectFork(vm.rpcUrl('mainnet'), 18722018);
+    vm.createSelectFork(vm.rpcUrl('mainnet'), 18722560);
     proposal = new AaveV3Ethereum_IncreaseGHOBorrowRate100BpsTo635OnAaveV3_20231205();
+    mock = new mock_proposal();
   }
 
   /**
    * @dev executes the generic test suite including e2e and config snapshots
    */
   function test_defaultProposalExecution() public {
+    GovV3Helpers.executePayload(
+      vm,
+      address(mock)
+    );
     defaultTest(
       'AaveV3Ethereum_IncreaseGHOBorrowRate100BpsTo635OnAaveV3_20231205',
       AaveV3Ethereum.POOL,
