4d62a4472f9ee50763a740f0692e65524357685d
sakulstra <lukasstrassel@googlemail.com>
2023-12-06 10:16:52 +0100

    feat: migrate to create2 for payload deployments

--- generator/common.ts
+++ generator/common.ts
@@ -22,7 +22,7 @@ export const AVAILABLE_CHAINS = [
   'Harmony',
   'Metis',
   'Base',
-  'Bsc',
+  'BNB',
   'Gnosis',
 ] as const;
 
@@ -103,7 +103,7 @@ export const CHAIN_TO_CHAIN_ID = {
   Avalanche: avalanche.id,
   Metis: metis.id,
   Base: base.id,
-  Bsc: bsc.id,
+  BNB: bsc.id,
   Gnosis: gnosis.id,
 };
 
