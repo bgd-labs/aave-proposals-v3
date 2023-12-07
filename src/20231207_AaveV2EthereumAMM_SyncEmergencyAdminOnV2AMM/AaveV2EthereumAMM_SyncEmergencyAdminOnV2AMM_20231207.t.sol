c9429be2cb03c2a2117d419422a15aadd03e32b2
Rustem Kurmaev <rustboyar@gmail.com>
2023-12-07 16:03:50 +0100

    Aip and test

--- src/20231207_AaveV2EthereumAMM_SyncEmergencyAdminOnV2AMM/AaveV2EthereumAMM_SyncEmergencyAdminOnV2AMM_20231207.t.sol
+++ src/20231207_AaveV2EthereumAMM_SyncEmergencyAdminOnV2AMM/AaveV2EthereumAMM_SyncEmergencyAdminOnV2AMM_20231207.t.sol
@@ -2,15 +2,19 @@
 pragma solidity ^0.8.0;
 
 import 'forge-std/Test.sol';
-import {ProtocolV2TestBase, ReserveConfig} from 'aave-helpers/ProtocolV2TestBase.sol';
+import {GovV3Helpers} from 'aave-helpers/GovV3Helpers.sol';
 import {AaveV2EthereumAMM} from 'aave-address-book/AaveV2EthereumAMM.sol';
+import {MiscEthereum} from 'aave-address-book/MiscEthereum.sol';
+import {Errors} from 'aave-address-book/AaveV2.sol';
 import {AaveV2EthereumAMM_SyncEmergencyAdminOnV2AMM_20231207} from './AaveV2EthereumAMM_SyncEmergencyAdminOnV2AMM_20231207.sol';
 
 /**
  * @dev Test for AaveV2EthereumAMM_SyncEmergencyAdminOnV2AMM_20231207
  * command: make test-contract filter=AaveV2EthereumAMM_SyncEmergencyAdminOnV2AMM_20231207
  */
-contract AaveV2EthereumAMM_SyncEmergencyAdminOnV2AMM_20231207_Test is ProtocolV2TestBase {
+contract AaveV2EthereumAMM_SyncEmergencyAdminOnV2AMM_20231207_Test is Test {
+  event Paused();
+
   AaveV2EthereumAMM_SyncEmergencyAdminOnV2AMM_20231207 internal proposal;
 
   function setUp() public {
@@ -22,10 +26,24 @@ contract AaveV2EthereumAMM_SyncEmergencyAdminOnV2AMM_20231207_Test is ProtocolV2
    * @dev executes the generic test suite including e2e and config snapshots
    */
   function test_defaultProposalExecution() public {
-    defaultTest(
-      'AaveV2EthereumAMM_SyncEmergencyAdminOnV2AMM_20231207',
-      AaveV2EthereumAMM.POOL,
-      address(proposal)
-    );
+    GovV3Helpers.executePayload(vm, address(proposal));
+
+    // check that old guardian could not pause
+    vm.startPrank(AaveV2EthereumAMM.EMERGENCY_ADMIN);
+
+    vm.expectRevert(bytes(Errors.LPC_CALLER_NOT_EMERGENCY_ADMIN));
+    AaveV2EthereumAMM.POOL_CONFIGURATOR.setPoolPause(true);
+
+    vm.stopPrank();
+
+    // check that new guardian can pause
+    vm.startPrank(MiscEthereum.PROTOCOL_GUARDIAN);
+
+    vm.expectEmit();
+    emit Paused();
+
+    AaveV2EthereumAMM.POOL_CONFIGURATOR.setPoolPause(true);
+
+    vm.stopPrank();
   }
 }
--- src/20231207_AaveV2EthereumAMM_SyncEmergencyAdminOnV2AMM/SyncEmergencyAdminOnV2AMM.md
+++ src/20231207_AaveV2EthereumAMM_SyncEmergencyAdminOnV2AMM/SyncEmergencyAdminOnV2AMM.md
@@ -6,9 +6,17 @@ discussions: "https://governance.aave.com/t/bgd-technical-maintenance-proposals/
 
 ## Simple Summary
 
+This proposal aligns the emergency admin address on the deprecated Aave v2 AMM pool with the one of Aave v2 and v3 Ethereum.
+
 ## Motivation
 
+During an internal security review procedure of Aave, we detected that the emergency admin on Aave v2 AMM is a legacy address, not aligned with the Aave Guardian.
+
+Even if the v2 AMM pool is deprecated (frozen) and almost off-boarded, for hygiene we think it is appropriate to have consistency on all Aave instances in the same network, and simpler for operations.
+
 ## Specification
+
+Upon execution, the proposal will call setEmergencyAdmin(0xCA76Ebd8617a03126B6FB84F9b1c1A0fB71C2633) on the addresses provider contract of v2 AMM, passing the address of the Aave Ethereum Guardian.
 
 ## References
 
