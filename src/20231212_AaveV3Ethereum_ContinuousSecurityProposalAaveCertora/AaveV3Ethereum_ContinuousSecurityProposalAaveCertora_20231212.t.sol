d0162430c3e4f94fa5d2914bf2efc7a6204c492e
Marc Zeller <pierarte@gmail.com>
2023-12-13 15:17:46 +0000

    lint and merge

--- src/20231212_AaveV3Ethereum_ContinuousSecurityProposalAaveCertora/AaveV3Ethereum_ContinuousSecurityProposalAaveCertora_20231212.t.sol
+++ src/20231212_AaveV3Ethereum_ContinuousSecurityProposalAaveCertora/AaveV3Ethereum_ContinuousSecurityProposalAaveCertora_20231212.t.sol
@@ -8,6 +8,7 @@ import 'forge-std/Test.sol';
 import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
 import {AaveV3Ethereum_ContinuousSecurityProposalAaveCertora_20231212} from './AaveV3Ethereum_ContinuousSecurityProposalAaveCertora_20231212.sol';
 import {IERC20} from 'solidity-utils/contracts/oz-common/interfaces/IERC20.sol';
+
 /**
  * @dev Test for AaveV3Ethereum_ContinuousSecurityProposalAaveCertora_20231212
  * command: make test-contract filter=AaveV3Ethereum_ContinuousSecurityProposalAaveCertora_20231212
@@ -25,7 +26,7 @@ contract AaveV3Ethereum_ContinuousSecurityProposalAaveCertora_20231212_Test is P
     (AAVE_STREAM_AMOUNT / STREAM_DURATION) * STREAM_DURATION;
 
   function setUp() public {
-    vm.createSelectFork(vm.rpcUrl('mainnet'), 18771153);
+    vm.createSelectFork(vm.rpcUrl('mainnet'), 18778078);
     proposal = new AaveV3Ethereum_ContinuousSecurityProposalAaveCertora_20231212();
   }
 
@@ -33,17 +34,25 @@ contract AaveV3Ethereum_ContinuousSecurityProposalAaveCertora_20231212_Test is P
    * @dev executes the generic test suite including e2e and config snapshots
    */
   function test_defaultProposalExecution() public {
-    uint256 nextCollectorStreamIDGHO =  AaveV3Ethereum.COLLECTOR.getNextStreamId();
+    uint256 nextCollectorStreamIDGHO = AaveV3Ethereum.COLLECTOR.getNextStreamId();
     uint256 nextReserveStreamIDAAVE = ICollector(MiscEthereum.ECOSYSTEM_RESERVE).getNextStreamId();
-    uint256 CERTORAGHOBalanceBefore = IERC20(AaveV3EthereumAssets.GHO_UNDERLYING).balanceOf(CERTORA_TREASURY);
-    uint256 CERTORAAAVEBalanceBefore = IERC20(AaveV3EthereumAssets.AAVE_UNDERLYING).balanceOf(CERTORA_TREASURY);
+    uint256 CERTORAGHOBalanceBefore = IERC20(AaveV3EthereumAssets.GHO_UNDERLYING).balanceOf(
+      CERTORA_TREASURY
+    );
+    uint256 CERTORAAAVEBalanceBefore = IERC20(AaveV3EthereumAssets.AAVE_UNDERLYING).balanceOf(
+      CERTORA_TREASURY
+    );
 
-    uint256 CollectorV3GHOBalanceBefore = IERC20(AaveV3EthereumAssets.GHO_UNDERLYING).balanceOf(address(AaveV3Ethereum.COLLECTOR));
-    uint256 ReserveAAVEBalanceBefore = IERC20(AaveV3EthereumAssets.AAVE_UNDERLYING).balanceOf(address(MiscEthereum.AAVE_ECOSYSTEM_RESERVE_CONTROLLER));
+    uint256 CollectorV3GHOBalanceBefore = IERC20(AaveV3EthereumAssets.GHO_UNDERLYING).balanceOf(
+      address(AaveV3Ethereum.COLLECTOR)
+    );
+    uint256 ReserveAAVEBalanceBefore = IERC20(AaveV3EthereumAssets.AAVE_UNDERLYING).balanceOf(
+      address(MiscEthereum.AAVE_ECOSYSTEM_RESERVE_CONTROLLER)
+    );
 
     executePayload(vm, address(proposal));
-// Checking if the streams have been created properly
-// scoping to avoid the "stack too deep" error
+    // Checking if the streams have been created properly
+    // scoping to avoid the "stack too deep" error
     {
       (
         address senderGHO,
@@ -53,6 +62,7 @@ contract AaveV3Ethereum_ContinuousSecurityProposalAaveCertora_20231212_Test is P
         uint256 startTimeGHO,
         uint256 stopTimeGHO,
         uint256 remainingBalanceGHO,
+
       ) = AaveV3Ethereum.COLLECTOR.getStream(nextCollectorStreamIDGHO);
       assertEq(senderGHO, address(AaveV3Ethereum.COLLECTOR));
       assertEq(recipientGHO, CERTORA_TREASURY);
@@ -70,7 +80,10 @@ contract AaveV3Ethereum_ContinuousSecurityProposalAaveCertora_20231212_Test is P
         uint256 startTimeAAVE,
         uint256 stopTimeAAVE,
         uint256 remainingBalanceAAVE,
-      ) = ICollector(address(MiscEthereum.AAVE_ECOSYSTEM_RESERVE_CONTROLLER)).getStream(nextReserveStreamIDAAVE);
+
+      ) = ICollector(address(MiscEthereum.AAVE_ECOSYSTEM_RESERVE_CONTROLLER)).getStream(
+          nextReserveStreamIDAAVE
+        );
       assertEq(senderAAVE, address(MiscEthereum.AAVE_ECOSYSTEM_RESERVE_CONTROLLER));
       assertEq(recipientAAVE, CERTORA_TREASURY);
       assertEq(depositAAVE, ACTUAL_STREAM_AMOUNT_AAVE);
@@ -84,7 +97,9 @@ contract AaveV3Ethereum_ContinuousSecurityProposalAaveCertora_20231212_Test is P
     // GHO stream
 
     AaveV3Ethereum.COLLECTOR.withdrawFromStream(nextCollectorStreamIDGHO, ACTUAL_STREAM_AMOUNT_GHO);
-    uint256 nextCERTORAGHOBalance = IERC20(AaveV3EthereumAssets.GHO_UNDERLYING).balanceOf(CERTORA_TREASURY);
+    uint256 nextCERTORAGHOBalance = IERC20(AaveV3EthereumAssets.GHO_UNDERLYING).balanceOf(
+      CERTORA_TREASURY
+    );
 
     assertEq(CERTORAGHOBalanceBefore, nextCERTORAGHOBalance - ACTUAL_STREAM_AMOUNT_GHO);
 
@@ -95,11 +110,17 @@ contract AaveV3Ethereum_ContinuousSecurityProposalAaveCertora_20231212_Test is P
     );
 
     assertEq(CollectorV3GHOBalanceAfter, CollectorV3GHOBalanceBefore - ACTUAL_STREAM_AMOUNT_GHO);
-    
+
     // AAVE stream
 
-    MiscEthereum.AAVE_ECOSYSTEM_RESERVE_CONTROLLER.withdrawFromStream(address(MiscEthereum.AAVE_ECOSYSTEM_RESERVE_CONTROLLER), nextReserveStreamIDAAVE, ACTUAL_STREAM_AMOUNT_AAVE);
-    uint256 nextCERTORAAAVEBalance = IERC20(AaveV3EthereumAssets.AAVE_UNDERLYING).balanceOf(CERTORA_TREASURY);
+    MiscEthereum.AAVE_ECOSYSTEM_RESERVE_CONTROLLER.withdrawFromStream(
+      address(MiscEthereum.AAVE_ECOSYSTEM_RESERVE_CONTROLLER),
+      nextReserveStreamIDAAVE,
+      ACTUAL_STREAM_AMOUNT_AAVE
+    );
+    uint256 nextCERTORAAAVEBalance = IERC20(AaveV3EthereumAssets.AAVE_UNDERLYING).balanceOf(
+      CERTORA_TREASURY
+    );
 
     assertEq(CERTORAAAVEBalanceBefore, nextCERTORAAAVEBalance - ACTUAL_STREAM_AMOUNT_AAVE);
 
