79bc498a14e395134711bbf2d47bb59a55bc4df1
sakulstra <lukasstrassel@googlemail.com>
2023-12-14 16:58:55 +0100

    fix: patch test

--- src/20231207_AaveV3Ethereum_TokenLogicKarpatkeyServiceProviderPartnership/AaveV3Ethereum_TokenLogicKarpatkeyServiceProviderPartnership_20231207.t.sol
+++ src/20231207_AaveV3Ethereum_TokenLogicKarpatkeyServiceProviderPartnership/AaveV3Ethereum_TokenLogicKarpatkeyServiceProviderPartnership_20231207.t.sol
@@ -19,9 +19,7 @@ contract AaveV3Ethereum_TokenLogicKarpatkeyServiceProviderPartnership_20231207_T
 
   function setUp() public {
     vm.createSelectFork(vm.rpcUrl('mainnet'), 18736059);
-    proposal = AaveV3Ethereum_TokenLogicKarpatkeyServiceProviderPartnership_20231207(
-      0x485CDe091918e1EaC67495a73DBa7bbcf1Da4F86
-    );
+    proposal = new AaveV3Ethereum_TokenLogicKarpatkeyServiceProviderPartnership_20231207();
   }
 
   /**
