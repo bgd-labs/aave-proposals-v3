49bdaae920170e6a4f523f21197b6751dc325cd2
sakulstra <lukasstrassel@googlemail.com>
2023-12-18 12:27:38 +0100

    fix: add test

--- src/20231218_AaveV3Ethereum_SpearbitAudit/AaveV3Ethereum_SpearbitAudit_20231218.t.sol
+++ src/20231218_AaveV3Ethereum_SpearbitAudit/AaveV3Ethereum_SpearbitAudit_20231218.t.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.0;
 
 import 'forge-std/Test.sol';
+import {MiscEthereum} from 'aave-address-book/MiscEthereum.sol';
+import {GovV3Helpers, GovV3StorageHelpers, GovernanceV3Ethereum} from 'aave-helpers/GovV3Helpers.sol';
 import {GovHelpers, AaveGovernanceV2} from 'aave-helpers/GovHelpers.sol';
 import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
 import {AaveV3Ethereum_SpearbitAudit_20231218} from './AaveV3Ethereum_SpearbitAudit_20231218.sol';
 import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
 import {IERC20} from 'solidity-utils/contracts/oz-common/interfaces/IERC20.sol';
+import {DeploymentHelper} from './SpearbitAudit_20231218.s.sol';
 
 /**
  * @dev Test for AaveV3Ethereum_SpearbitAudit_20231218
@@ -21,18 +24,24 @@ contract AaveV3Ethereum_SpearbitAudit_20231218_Test is ProtocolV3TestBase {
     AaveGovernanceV2.GOV.execute(proposalId);
   }
 
+  function _executeV3Proposal(Vm vm, uint256 proposalId) internal {
+    GovV3StorageHelpers.readyProposal(vm, proposalId);
+    GovernanceV3Ethereum.GOVERNANCE.executeProposal(proposalId);
+  }
+
   function setUp() public {
     vm.createSelectFork(vm.rpcUrl('mainnet'), 18812072);
     proposal = new AaveV3Ethereum_SpearbitAudit_20231218();
-
-    _executeV2Proposal(395); // long 395
-    GovHelpers.passVoteAndExecute(vm, 415); // short 415
   }
 
   /**
    * @dev executes the generic test suite including e2e and config snapshots
    */
   function test_defaultProposalExecution() public {
+    _executeV2Proposal(395); // long 395
+    GovHelpers.passVoteAndExecute(vm, 415); // short 415
+    GovV3Helpers.executePayload(vm, 26); // mediator call
+
     uint256 daiBalanceBefore = IERC20(AaveV3EthereumAssets.DAI_A_TOKEN).balanceOf(
       address(AaveV3Ethereum.COLLECTOR)
     );
@@ -46,7 +55,13 @@ contract AaveV3Ethereum_SpearbitAudit_20231218_Test is ProtocolV3TestBase {
       address(proposal.CANTINA())
     );
 
-    defaultTest('AaveV3Ethereum_SpearbitAudit_20231218', AaveV3Ethereum.POOL, address(proposal));
+    uint40 payloadId = DeploymentHelper.createPayloads();
+    deal(MiscEthereum.ECOSYSTEM_RESERVE, 1 ether);
+    vm.startPrank(MiscEthereum.ECOSYSTEM_RESERVE);
+    uint256 proposalId = DeploymentHelper.createProposal(vm);
+    vm.stopPrank();
+    _executeV3Proposal(vm, proposalId);
+    GovV3Helpers.executePayload(vm, payloadId);
 
     uint256 usdcBalanceAfter = IERC20(AaveV3EthereumAssets.USDC_A_TOKEN).balanceOf(
       address(AaveV3Ethereum.COLLECTOR)
--- src/20231218_AaveV3Ethereum_SpearbitAudit/SpearbitAudit_20231218.s.sol
+++ src/20231218_AaveV3Ethereum_SpearbitAudit/SpearbitAudit_20231218.s.sol
@@ -1,23 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import {Vm} from 'forge-std/Vm.sol';
 import {GovV3Helpers, IPayloadsControllerCore, PayloadsControllerUtils} from 'aave-helpers/GovV3Helpers.sol';
 import {EthereumScript} from 'aave-helpers/ScriptUtils.sol';
 import {AaveV3Ethereum_SpearbitAudit_20231218} from './AaveV3Ethereum_SpearbitAudit_20231218.sol';
 
 library DeploymentHelper {
-  function createPayloads() internal {}
-
-  function createProposal() internal {}
-}
-
-/**
- * @dev Deploy Ethereum
- * deploy-command: make deploy-ledger contract=src/20231218_AaveV3Ethereum_SpearbitAudit/SpearbitAudit_20231218.s.sol:DeployEthereum chain=mainnet
- * verify-command: npx catapulta-verify -b broadcast/SpearbitAudit_20231218.s.sol/1/run-latest.json
- */
-contract DeployEthereum is EthereumScript {
-  function run() external broadcast {
+  function createPayloads() internal returns (uint40) {
     // deploy payloads
     address payload0 = GovV3Helpers.deployDeterministic(
       type(AaveV3Ethereum_SpearbitAudit_20231218).creationCode
@@ -29,16 +19,10 @@ contract DeployEthereum is EthereumScript {
     actions[0] = GovV3Helpers.buildAction(payload0);
 
     // register action at payloadsController
-    GovV3Helpers.createPayload(actions);
+    return GovV3Helpers.createPayload(actions);
   }
-}
 
-/**
- * @dev Create Proposal
- * command: make deploy-ledger contract=src/20231218_AaveV3Ethereum_SpearbitAudit/SpearbitAudit_20231218.s.sol:CreateProposal chain=mainnet
- */
-contract CreateProposal is EthereumScript {
-  function run() external broadcast {
+  function createProposal(Vm vm) internal returns (uint256) {
     // create payloads
     PayloadsControllerUtils.Payload[] memory payloads = new PayloadsControllerUtils.Payload[](1);
 
@@ -51,10 +35,32 @@ contract CreateProposal is EthereumScript {
     payloads[0] = GovV3Helpers.buildMainnetPayload(vm, actionsEthereum);
 
     // create proposal
-    GovV3Helpers.createProposal(
-      vm,
-      payloads,
-      GovV3Helpers.ipfsHashFile(vm, 'src/20231218_AaveV3Ethereum_SpearbitAudit/SpearbitAudit.md')
-    );
+    return
+      GovV3Helpers.createProposal(
+        vm,
+        payloads,
+        GovV3Helpers.ipfsHashFile(vm, 'src/20231218_AaveV3Ethereum_SpearbitAudit/SpearbitAudit.md')
+      );
+  }
+}
+
+/**
+ * @dev Deploy Ethereum
+ * deploy-command: make deploy-ledger contract=src/20231218_AaveV3Ethereum_SpearbitAudit/SpearbitAudit_20231218.s.sol:DeployEthereum chain=mainnet
+ * verify-command: npx catapulta-verify -b broadcast/SpearbitAudit_20231218.s.sol/1/run-latest.json
+ */
+contract DeployEthereum is EthereumScript {
+  function run() external broadcast {
+    DeploymentHelper.createPayloads();
+  }
+}
+
+/**
+ * @dev Create Proposal
+ * command: make deploy-ledger contract=src/20231218_AaveV3Ethereum_SpearbitAudit/SpearbitAudit_20231218.s.sol:CreateProposal chain=mainnet
+ */
+contract CreateProposal is EthereumScript {
+  function run() external broadcast {
+    DeploymentHelper.createProposal(vm);
   }
 }
