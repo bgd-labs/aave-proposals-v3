825c3fc8f87d89929b1040b39f07074ccb5cacac
Andrey <kyzia.ru@gmail.com>
2024-01-05 23:46:19 +0700

    156 cleanup (#157)
    * 156 cleanup
    
    * remove boilerplate on run() of GenericDeploy
    
    * remove unused import
    
    * tests cleanup

--- src/20240104_Multi_Patch/AaveV3Avalanche_Patch_20240104.t.sol
+++ src/20240104_Multi_Patch/AaveV3Avalanche_Patch_20240104.t.sol
@@ -1,28 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {AaveV3Avalanche} from 'aave-address-book/AaveV3Avalanche.sol';
-
-import 'forge-std/Test.sol';
-import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
+import {AaveV3Generic_Patch_20240104_Test} from './AaveV3Generic_Patch_20240104_Test.sol';
 import {AaveV3Avalanche_Patch_20240104} from './AaveV3Avalanche_Patch_20240104.sol';
 
 /**
  * @dev Test for AaveV3Avalanche_Patch_20240104
  * command: make test-contract filter=AaveV3Avalanche_Patch_20240104
  */
-contract AaveV3Avalanche_Patch_20240104_Test is ProtocolV3TestBase {
-  AaveV3Avalanche_Patch_20240104 internal proposal;
-
+contract AaveV3Avalanche_Patch_20240104_Test is AaveV3Generic_Patch_20240104_Test {
   function setUp() public {
     vm.createSelectFork(vm.rpcUrl('avalanche'), 39925983);
-    proposal = new AaveV3Avalanche_Patch_20240104();
-  }
-
-  /**
-   * @dev execution will fail because logic is not yet deployed
-   */
-  function testFail_defaultProposalExecution() public {
-    executePayload(vm, address(proposal));
+    proposal = address(new AaveV3Avalanche_Patch_20240104());
   }
 }
