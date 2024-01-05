13e6b80417a30c5cdc3a502ac3ba60737cf4c93c
Andrei Kozlov <kyzia.ru@gmail.com>
2024-01-05 23:26:53 +0700

    remove unused import

--- src/20240104_Multi_Patch/AaveV3Polygon_Patch_20240104.t.sol
+++ src/20240104_Multi_Patch/AaveV3Polygon_Patch_20240104.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import {AaveV3Polygon} from 'aave-address-book/AaveV3Polygon.sol';
 
 import 'forge-std/Test.sol';
-import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
+import {ProtocolV3TestBase} from 'aave-helpers/ProtocolV3TestBase.sol';
 import {AaveV3Polygon_Patch_20240104} from './AaveV3Polygon_Patch_20240104.sol';
 
 /**
