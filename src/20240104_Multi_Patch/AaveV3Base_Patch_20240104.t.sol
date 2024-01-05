f864cf0ef5f35065f4df84e60aa57cfe2b27b237
Andrei Kozlov <kyzia.ru@gmail.com>
2024-01-05 23:32:36 +0700

    tests cleanup

--- src/20240104_Multi_Patch/AaveV3Base_Patch_20240104.t.sol
+++ src/20240104_Multi_Patch/AaveV3Base_Patch_20240104.t.sol
@@ -1,28 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {AaveV3Base} from 'aave-address-book/AaveV3Base.sol';
-
-import 'forge-std/Test.sol';
-import {ProtocolV3TestBase} from 'aave-helpers/ProtocolV3TestBase.sol';
+import {AaveV3Generic_Patch_20240104_Test} from './AaveV3Generic_Patch_20240104_Test.sol';
 import {AaveV3Base_Patch_20240104} from './AaveV3Base_Patch_20240104.sol';
 
 /**
  * @dev Test for AaveV3Base_Patch_20240104
  * command: make test-contract filter=AaveV3Base_Patch_20240104
  */
-contract AaveV3Base_Patch_20240104_Test is ProtocolV3TestBase {
-  AaveV3Base_Patch_20240104 internal proposal;
-
+contract AaveV3Base_Patch_20240104_Test is AaveV3Generic_Patch_20240104_Test {
   function setUp() public {
     vm.createSelectFork(vm.rpcUrl('base'), 8792857);
-    proposal = new AaveV3Base_Patch_20240104();
-  }
-
-  /**
-   * @dev execution will fail because logic is not yet deployed
-   */
-  function testFail_defaultProposalExecution() public {
-    executePayload(vm, address(proposal));
+    proposal = address(new AaveV3Base_Patch_20240104());
   }
 }
--- src/20240104_Multi_Patch/AaveV3Optimism_Patch_20240104.t.sol
+++ src/20240104_Multi_Patch/AaveV3Optimism_Patch_20240104.t.sol
@@ -1,28 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {AaveV3Optimism} from 'aave-address-book/AaveV3Optimism.sol';
-
-import 'forge-std/Test.sol';
-import {ProtocolV3TestBase} from 'aave-helpers/ProtocolV3TestBase.sol';
+import {AaveV3Generic_Patch_20240104_Test} from './AaveV3Generic_Patch_20240104_Test.sol';
 import {AaveV3Optimism_Patch_20240104} from './AaveV3Optimism_Patch_20240104.sol';
 
 /**
  * @dev Test for AaveV3Optimism_Patch_20240104
  * command: make test-contract filter=AaveV3Optimism_Patch_20240104
  */
-contract AaveV3Optimism_Patch_20240104_Test is ProtocolV3TestBase {
-  AaveV3Optimism_Patch_20240104 internal proposal;
-
+contract AaveV3Optimism_Patch_20240104_Test is AaveV3Generic_Patch_20240104_Test {
   function setUp() public {
     vm.createSelectFork(vm.rpcUrl('optimism'), 114388130);
-    proposal = new AaveV3Optimism_Patch_20240104();
-  }
-
-  /**
-   * @dev execution will fail because logic is not yet deployed
-   */
-  function testFail_defaultProposalExecution() public {
-    executePayload(vm, address(proposal));
+    proposal = address(new AaveV3Optimism_Patch_20240104());
   }
 }
