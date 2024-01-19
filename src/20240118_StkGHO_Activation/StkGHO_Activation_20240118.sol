5df6a3e13e8247d80bab97e8bb8b1650473449d7
miguelmtzinf <miguelmtz.mail@gmail.com>
2024-01-19 16:23:46 +0100

    fix: Fix AIP payload

--- src/20240118_StkGHO_Activation/StkGHO_Activation_20240118.sol
+++ src/20240118_StkGHO_Activation/StkGHO_Activation_20240118.sol
@@ -1,41 +1,41 @@
 // SPDX-License-Identifier: MIT
-
-pragma solidity 0.8.19;
+pragma solidity ^0.8.0;
 
 import {IProposalGenericExecutor} from 'aave-helpers/interfaces/IProposalGenericExecutor.sol';
 import {MiscEthereum} from 'aave-address-book/MiscEthereum.sol';
-import {AaveV3Ethereum} from 'aave-address-book/AaveV3Ethereum.sol';
-import {AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
+import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
+import {AaveSafetyModule} from 'aave-address-book/AaveSafetyModule.sol';
 import {IStakeToken} from './IStakeToken.sol';
 
 /**
  * @title StkGHO Activation
  * @author the3d.eth
- * - Snapshot: 'https://snapshot.org/#/aave.eth/proposal/0x4bc99a842adab6cdd8c7d5c7a787ee4c0056be554fde0d008d53b45b3e795065
+ * - Snapshot: https://snapshot.org/#/aave.eth/proposal/0x4bc99a842adab6cdd8c7d5c7a787ee4c0056be554fde0d008d53b45b3e795065
  * - Discussion: https://governance.aave.com/t/arfc-upgrade-safety-module-with-stkgho/15635
  */
 contract StkGHO_Activation_20240118 is IProposalGenericExecutor {
-  uint256 public constant STKGHO_EMISSION_PER_SECOND = 578703703703704; // 50 AAVE/day
-  uint256 public constant DISTRIBUTION_DURATION = 90 days; // three months
-  address public constant STKGHO_PROXY = 0x1a88Df1cFe15Af22B3c4c783D4e6F7F9e0C1885d;
+  uint128 public constant AAVE_EMISSION_PER_SECOND = uint128(50e18) / 1 days; // 50 AAVE per day
+  uint256 public constant DISTRIBUTION_DURATION = 90 days; // 3 months
 
   function execute() external {
-    IStakeToken(STKGHO_PROXY).setDistributionEnd(block.timestamp + DISTRIBUTION_DURATION);
-
+    // Configure distribution
+    IStakeToken(AaveSafetyModule.STK_GHO).setDistributionEnd(
+      block.timestamp + DISTRIBUTION_DURATION
+    );
     IStakeToken.AssetConfigInput[] memory enableConfigs = new IStakeToken.AssetConfigInput[](1);
     enableConfigs[0] = IStakeToken.AssetConfigInput({
-      emissionPerSecond: uint128(STKGHO_EMISSION_PER_SECOND),
+      emissionPerSecond: AAVE_EMISSION_PER_SECOND,
       totalStaked: 0, // it's overwritten internally
-      underlyingAsset: AaveV3Ethereum.GHO_TOKEN
+      underlyingAsset: AaveSafetyModule.STK_GHO
     });
+    IStakeToken(AaveSafetyModule.STK_GHO).configureAssets(enableConfigs);
 
-    IStakeToken(STKGHO_PROXY).configureAssets(enableConfigs);
-
+    // Allowance to pull funds from Ecosystem Reserve
     MiscEthereum.AAVE_ECOSYSTEM_RESERVE_CONTROLLER.approve(
       MiscEthereum.ECOSYSTEM_RESERVE,
       AaveV3EthereumAssets.AAVE_UNDERLYING,
-      STKGHO_PROXY,
-      STKGHO_EMISSION_PER_SECOND * DISTRIBUTION_DURATION
+      AaveSafetyModule.STK_GHO,
+      AAVE_EMISSION_PER_SECOND * DISTRIBUTION_DURATION
     );
   }
 }
