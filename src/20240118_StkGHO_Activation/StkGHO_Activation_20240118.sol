c3d48cd8c26dfff590e117798eb2988b248d7c18
Emilio  Frangella <frangellaemilio@gmail.com>
2024-01-19 15:44:05 +0100

    feat: removed submodule

--- src/20240118_StkGHO_Activation/StkGHO_Activation_20240118.sol
+++ src/20240118_StkGHO_Activation/StkGHO_Activation_20240118.sol
@@ -5,10 +5,8 @@ pragma solidity 0.8.19;
 import {IProposalGenericExecutor} from 'aave-helpers/interfaces/IProposalGenericExecutor.sol';
 import {MiscEthereum} from 'aave-address-book/MiscEthereum.sol';
 import {AaveV3Ethereum} from 'aave-address-book/AaveV3Ethereum.sol';
-import {IAggregatedStakeToken} from 'stake-token/src/contracts/IAggregatedStakeToken.sol';
-import {DistributionTypes} from 'stake-token/src/contracts/lib/DistributionTypes.sol';
-import {IAaveDistributionManager} from 'stake-token/src/contracts/IAaveDistributionManager.sol';
 import {AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
+import {IStakeToken} from './IStakeToken.sol';
 
 /**
  * @title StkGHO Activation
@@ -22,17 +20,21 @@ contract StkGHO_Activation_20240118 is IProposalGenericExecutor {
   address public constant STKGHO_PROXY = 0x1a88Df1cFe15Af22B3c4c783D4e6F7F9e0C1885d;
 
   function execute() external {
-     IAggregatedStakeToken(STKGHO_PROXY).setDistributionEnd(
+    
+    IStakeToken(STKGHO_PROXY).setDistributionEnd(
       block.timestamp + DISTRIBUTION_DURATION
     );
-    DistributionTypes.AssetConfigInput[]
-      memory enableConfigs = new DistributionTypes.AssetConfigInput[](1);
-    enableConfigs[0] = DistributionTypes.AssetConfigInput({
+
+    IStakeToken.AssetConfigInput[]
+      memory enableConfigs = new IStakeToken.AssetConfigInput[](1);
+    enableConfigs[0] = IStakeToken.AssetConfigInput({
       emissionPerSecond: uint128(STKGHO_EMISSION_PER_SECOND),
       totalStaked: 0, // it's overwritten internally
       underlyingAsset: AaveV3Ethereum.GHO_TOKEN
     });
-    IAaveDistributionManager(STKGHO_PROXY).configureAssets(enableConfigs);
+
+    IStakeToken(STKGHO_PROXY).configureAssets(enableConfigs);
+
     MiscEthereum.AAVE_ECOSYSTEM_RESERVE_CONTROLLER.approve(
       MiscEthereum.ECOSYSTEM_RESERVE,
       AaveV3EthereumAssets.AAVE_UNDERLYING,
