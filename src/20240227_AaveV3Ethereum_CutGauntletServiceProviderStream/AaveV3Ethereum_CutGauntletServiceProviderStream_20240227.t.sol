d0bf214922264d6adcbe5d73f69515c61148b471
Rozengarden <github@rozengarden.eu>
2024-02-27 20:23:42 +0100

    Add test for gauntlet stream cutting

--- src/20240227_AaveV3Ethereum_CutGauntletServiceProviderStream/AaveV3Ethereum_CutGauntletServiceProviderStream_20240227.t.sol
+++ src/20240227_AaveV3Ethereum_CutGauntletServiceProviderStream/AaveV3Ethereum_CutGauntletServiceProviderStream_20240227.t.sol
@@ -6,6 +6,9 @@ import {AaveV3Ethereum} from 'aave-address-book/AaveV3Ethereum.sol';
 import 'forge-std/Test.sol';
 import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
 import {AaveV3Ethereum_CutGauntletServiceProviderStream_20240227} from './AaveV3Ethereum_CutGauntletServiceProviderStream_20240227.sol';
+import {AaveV2Ethereum, AaveV2EthereumAssets} from 'aave-address-book/AaveV2Ethereum.sol';
+import {AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
+import {IERC20} from 'solidity-utils/contracts/oz-common/interfaces/IERC20.sol';
 
 /**
  * @dev Test for AaveV3Ethereum_CutGauntletServiceProviderStream_20240227
@@ -23,10 +26,62 @@ contract AaveV3Ethereum_CutGauntletServiceProviderStream_20240227_Test is Protoc
    * @dev executes the generic test suite including e2e and config snapshots
    */
   function test_defaultProposalExecution() public {
+    address GAUNTLET_RECIPIENT = proposal.GAUNTLET_RECIPIENT();
+    uint256 COLLECTOR_aUSDT_STREAM = proposal.COLLECTOR_aUSDT_STREAM();
+    uint256 COLLECTOR_GHO_STREAM = proposal.COLLECTOR_GHO_STREAM();
+
+    (, address recipient, , address tokenAddress, , , , ) = AaveV2Ethereum.COLLECTOR.getStream(
+      COLLECTOR_aUSDT_STREAM
+    );
+
+    assertEq(recipient, GAUNTLET_RECIPIENT);
+    assertEq(tokenAddress, AaveV2EthereumAssets.USDT_A_TOKEN);
+
+    (, recipient, , tokenAddress, , , , ) = AaveV2Ethereum.COLLECTOR.getStream(
+      COLLECTOR_GHO_STREAM
+    );
+
+    assertEq(recipient, GAUNTLET_RECIPIENT);
+    assertEq(tokenAddress, AaveV3EthereumAssets.GHO_UNDERLYING);
+
+    uint256 claimableAusdt = AaveV2Ethereum.COLLECTOR.balanceOf(
+      COLLECTOR_aUSDT_STREAM,
+      GAUNTLET_RECIPIENT
+    );
+
+    uint256 claimableGho = AaveV2Ethereum.COLLECTOR.balanceOf(
+      COLLECTOR_GHO_STREAM,
+      GAUNTLET_RECIPIENT
+    );
+
+    uint256 ausdtBalanceBefore = IERC20(AaveV2EthereumAssets.USDT_A_TOKEN).balanceOf(
+      GAUNTLET_RECIPIENT
+    );
+    uint256 ghoBalanceBefore = IERC20(AaveV3EthereumAssets.GHO_UNDERLYING).balanceOf(
+      GAUNTLET_RECIPIENT
+    );
+
     defaultTest(
       'AaveV3Ethereum_CutGauntletServiceProviderStream_20240227',
       AaveV3Ethereum.POOL,
-      address(proposal)
+      address(proposal),
+      false
     );
+
+    vm.expectRevert('stream does not exist');
+    AaveV2Ethereum.COLLECTOR.getStream(COLLECTOR_aUSDT_STREAM);
+
+    vm.expectRevert('stream does not exist');
+    AaveV2Ethereum.COLLECTOR.getStream(COLLECTOR_GHO_STREAM);
+
+    uint256 ausdtBalanceAfter = IERC20(AaveV2EthereumAssets.USDT_A_TOKEN).balanceOf(
+      GAUNTLET_RECIPIENT
+    );
+    uint256 ghoBalanceAfter = IERC20(AaveV3EthereumAssets.GHO_UNDERLYING).balanceOf(
+      GAUNTLET_RECIPIENT
+    );
+
+    assertEq(ausdtBalanceAfter, ausdtBalanceBefore + claimableAusdt);
+    assertEq(ghoBalanceAfter, ghoBalanceBefore + claimableGho);
   }
 }
