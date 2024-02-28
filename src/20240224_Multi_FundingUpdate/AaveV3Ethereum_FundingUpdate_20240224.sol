19c606a84263da5d2a333dd7203a08c7c7df1938
efecarranza <fcarr004@fiu.edu>
2024-02-28 14:40:09 -0500

    chore: add eth proposal

--- src/20240224_Multi_FundingUpdate/AaveV3Ethereum_FundingUpdate_20240224.sol
+++ src/20240224_Multi_FundingUpdate/AaveV3Ethereum_FundingUpdate_20240224.sol
@@ -10,7 +10,20 @@ import {IProposalGenericExecutor} from 'aave-helpers/interfaces/IProposalGeneric
  * - Discussion: https://governance.aave.com/t/arfc-funding-update/16675
  */
 contract AaveV3Ethereum_FundingUpdate_20240224 is IProposalGenericExecutor {
+  address public constant ALC_SAFE = 0x205e795336610f5131Be52F09218AF19f0f3eC60;
+
+  uint256 public constant USDC_V2_TO_MIGRATE = 300_000e6;
+  uint256 public constant USDC_V3_TO_SWAP = 1_250_000e6;
+  uint256 public constant USDT_V3_TO_SWAP = 1_500_000e6;
+  uint256 public constant USDT_V2_TO_SWAP = 200_000e6;
+  uint256 public constant BUSD_V2_TO_SWAP = 50 ether;
+
   function execute() external {
-    // custom code goes here
+    _migrate();
+    _swap();
   }
+
+  function _swap() internal {}
+
+  function _migrate() internal {}
 }
