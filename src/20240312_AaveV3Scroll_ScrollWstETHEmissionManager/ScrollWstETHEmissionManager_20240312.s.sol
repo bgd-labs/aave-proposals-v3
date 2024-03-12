50d8c28e978ee32869733e1a960e755718fd9da4
Rozengarden <github@rozengarden.eu>
2024-03-12 23:38:54 +0100

    wsteth emission admin on scroll

---
+++ src/20240312_AaveV3Scroll_ScrollWstETHEmissionManager/ScrollWstETHEmissionManager_20240312.s.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {GovV3Helpers, IPayloadsControllerCore, PayloadsControllerUtils} from 'aave-helpers/GovV3Helpers.sol';
+import {EthereumScript, ScrollScript} from 'aave-helpers/ScriptUtils.sol';
+import {AaveV3Scroll_ScrollWstETHEmissionManager_20240312} from './AaveV3Scroll_ScrollWstETHEmissionManager_20240312.sol';
+
+/**
+ * @dev Deploy Scroll
+ * deploy-command: make deploy-ledger contract=src/20240312_AaveV3Scroll_ScrollWstETHEmissionManager/ScrollWstETHEmissionManager_20240312.s.sol:DeployScroll chain=scroll
+ * verify-command: npx catapulta-verify -b broadcast/ScrollWstETHEmissionManager_20240312.s.sol/534352/run-latest.json
+ */
+contract DeployScroll is ScrollScript {
+  function run() external broadcast {
+    // deploy payloads
+    address payload0 = GovV3Helpers.deployDeterministic(
+      type(AaveV3Scroll_ScrollWstETHEmissionManager_20240312).creationCode
+    );
+
+    // compose action
+    IPayloadsControllerCore.ExecutionAction[]
+      memory actions = new IPayloadsControllerCore.ExecutionAction[](1);
+    actions[0] = GovV3Helpers.buildAction(payload0);
+
+    // register action at payloadsController
+    GovV3Helpers.createPayload(actions);
+  }
+}
+
+/**
+ * @dev Create Proposal
+ * command: make deploy-ledger contract=src/20240312_AaveV3Scroll_ScrollWstETHEmissionManager/ScrollWstETHEmissionManager_20240312.s.sol:CreateProposal chain=mainnet
+ */
+contract CreateProposal is EthereumScript {
+  function run() external {
+    // create payloads
+    PayloadsControllerUtils.Payload[] memory payloads = new PayloadsControllerUtils.Payload[](1);
+
+    // compose actions for validation
+    IPayloadsControllerCore.ExecutionAction[]
+      memory actionsScroll = new IPayloadsControllerCore.ExecutionAction[](1);
+    actionsScroll[0] = GovV3Helpers.buildAction(
+      type(AaveV3Scroll_ScrollWstETHEmissionManager_20240312).creationCode
+    );
+    payloads[0] = GovV3Helpers.buildScrollPayload(vm, actionsScroll);
+
+    // create proposal
+    vm.startBroadcast();
+    GovV3Helpers.createProposal(
+      vm,
+      payloads,
+      GovV3Helpers.ipfsHashFile(
+        vm,
+        'src/20240312_AaveV3Scroll_ScrollWstETHEmissionManager/ScrollWstETHEmissionManager.md'
+      )
+    );
+  }
+}
