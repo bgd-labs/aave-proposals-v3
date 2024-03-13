01843af25a408288ad169e2153650838a9da227a
Andrei Kozlov <kyzia.ru@gmail.com>
2024-03-13 12:15:28 +0100

    add BaseCCCImplementationUpdatePayloadTest

---
+++ src/20240313_Multi_UpdateADIImplementationAndCCIPAdapters/AaveV3Optimism_UpdateADIImplementationAndCCIPAdapters_20240313.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {MiscOptimism} from 'aave-address-book/MiscOptimism.sol';
+import {GovernanceV3Optimism} from 'aave-address-book/GovernanceV3Optimism.sol';
+import {BaseCCCImplementationUpdatePayload} from './BaseCCCImplementationUpdatePayload.sol';
+
+/**
+ * @title aDI and bridge adapters update
+ * @author BGD Labs @bgdlabs
+ * - Snapshot: TODO
+ * - Discussion: TODO
+ */
+contract AaveV3Optimism_UpdateADIImplementationAndCCIPAdapters_20240313 is
+  BaseCCCImplementationUpdatePayload(
+    BaseCCCImplementationUpdatePayload.CCCImplementationUpdateInput({
+      ccc: GovernanceV3Optimism.CROSS_CHAIN_CONTROLLER,
+      proxyAdmin: MiscOptimism.PROXY_ADMIN,
+      newCCCImplementation: 0xa5cc218513305221201f196760E9e64e9D49d98A
+    })
+  )
+{
+
+}
