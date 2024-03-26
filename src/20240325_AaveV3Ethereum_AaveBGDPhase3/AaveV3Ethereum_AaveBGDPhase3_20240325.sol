40335a0b56b2320ba68c13e7447820df32ea047b
Harsh Pandey <harshsatishpandey@gmail.com>
2024-03-26 16:18:33 +0530

    Update src/20240325_AaveV3Ethereum_AaveBGDPhase3/AaveV3Ethereum_AaveBGDPhase3_20240325.sol
    Co-authored-by: Ernesto Boado <ebdmrr@gmail.com>

--- src/20240325_AaveV3Ethereum_AaveBGDPhase3/AaveV3Ethereum_AaveBGDPhase3_20240325.sol
+++ src/20240325_AaveV3Ethereum_AaveBGDPhase3/AaveV3Ethereum_AaveBGDPhase3_20240325.sol
@@ -84,7 +84,7 @@ contract AaveV3Ethereum_AaveBGDPhase3_20240325 is IProposalGenericExecutor {
       SCOPE_2_AUSDC_STREAM,
       AaveV3EthereumAssets.USDC_A_TOKEN,
       block.timestamp + 120 days, // start of the stream will be after 4 months
-      block.timestamp + 120 days + 1 // end of the stream will be one block after the start
+      block.timestamp + 120 days + 1 // end of the stream will be one second after the start
     );
   }
 }
