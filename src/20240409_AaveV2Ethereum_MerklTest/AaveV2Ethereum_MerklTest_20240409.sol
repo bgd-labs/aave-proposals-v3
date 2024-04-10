5d9ba8ddd992b162697a261da10f714cd3f9d481
sakulstra <lukasstrassel@googlemail.com>
2024-04-10 20:35:21 +0200

    fix: adjust script

--- src/20240409_AaveV2Ethereum_MerklTest/AaveV2Ethereum_MerklTest_20240409.sol
+++ src/20240409_AaveV2Ethereum_MerklTest/AaveV2Ethereum_MerklTest_20240409.sol
@@ -2,63 +2,39 @@
 pragma solidity ^0.8.0;
 
 import {IERC20} from 'solidity-utils/contracts/oz-common/interfaces/IERC20.sol';
-import {AaveV2EthereumAssets} from 'aave-address-book/AaveV2Ethereum.sol';
+import {AaveV2Ethereum, AaveV2EthereumAssets} from 'aave-address-book/AaveV2Ethereum.sol';
 import {IProposalGenericExecutor} from 'aave-helpers/interfaces/IProposalGenericExecutor.sol';
 
 interface IDistributionCreator {
-  struct DistributionParameters {
-    // ID of the reward (populated once created). This can be left as a null bytes32 when creating distributions
+  struct CampaignParameters {
+    // POPULATED ONCE CREATED
+
+    // ID of the campaign. This can be left as a null bytes32 when creating campaigns
     // on Merkl.
-    bytes32 rewardId;
-    // Address of the UniswapV3 pool that needs to be incentivized
-    address uniV3Pool;
-    // Address of the reward token for the incentives
+    bytes32 campaignId;
+    // CHOSEN BY CAMPAIGN CREATOR
+
+    // Address of the campaign creator, if marked as address(0), it will be overriden with the
+    // address of the `msg.sender` creating the campaign
+    address creator;
+    // Address of the token used as a reward
     address rewardToken;
     // Amount of `rewardToken` to distribute across all the epochs
     // Amount distributed per epoch is `amount/numEpoch`
     uint256 amount;
-    // List of all position wrappers to consider or not for this contract. Some wrappers like Gamma or Arrakis
-    // are automatically detected and so there is no need to specify them here. Check out the docs to find out
-    // which need to be specified and which are not automatically detected.
-    address[] positionWrappers;
-    // Type (blacklist==3, whitelist==0, ...) encoded as a `uint32` for each wrapper in the list above. Mapping between
-    // wrapper types and their corresponding `uint32` value can be found in Angle Docs
-    uint32[] wrapperTypes;
-    // In the incentivization formula, how much of the fees should go to holders of token0
-    // in base 10**4
-    uint32 propToken0;
-    // Proportion for holding token1 (in base 10**4)
-    uint32 propToken1;
-    // Proportion for providing a useful liquidity (in base 10**4) that generates fees
-    uint32 propFees;
-    // Timestamp at which the incentivization should start. This is in the same units as `block.timestamp`.
-    uint32 epochStart;
-    // Amount of epochs for which incentivization should last. Epochs are expressed in hours here, so for a
-    // campaign of 1 week `numEpoch` should for instance be 168.
-    uint32 numEpoch;
-    // Whether out of range liquidity should still be incentivized or not
-    // This should be equal to 1 if out of range liquidity should still be incentivized
-    // and 0 otherwise.
-    uint32 isOutOfRangeIncentivized;
-    // How much more addresses with a maximum boost can get with respect to addresses
-    // which do not have a boost (in base 4). In the case of Curve where addresses get 2.5x more
-    // this would be 25000.
-    uint32 boostedReward;
-    // Address of the token which dictates who gets boosted rewards or not. This is optional
-    // and if the zero address is given no boost will be taken into account. In the case of Curve, this address
-    // would for instance be the veBoostProxy address, or in other cases the veToken address.
-    address boostingAddress;
-    // Additional data passed when distributing rewards. This parameter may be used in case
-    // the reward distribution script needs to look into other parameters beyond the ones above.
-    // In most cases, when creating a campaign on Merkl, you can leave this as an empty bytes.
-    bytes additionalData;
+    // Type of campaign
+    uint32 campaignType;
+    // Timestamp at which the campaign should start
+    uint32 startTimestamp;
+    // Duration of the campaign in seconds. Has to be a multiple of EPOCH = 3600
+    uint32 duration;
+    // Extra data to pass to specify the campaign
+    bytes campaignData;
   }
 
   function acceptConditions() external;
 
-  function createDistribution(
-    DistributionParameters memory newDistribution
-  ) external returns (uint256 distributionAmount);
+  function createCampaign(CampaignParameters memory newCampaign) external returns (bytes32);
 }
 
 /**
@@ -69,13 +45,40 @@ interface IDistributionCreator {
  */
 contract AaveV2Ethereum_MerklTest_20240409 is IProposalGenericExecutor {
   IDistributionCreator public constant DISTRIBUTION_CREATOR =
-    0x8BB4C975Ff3c250e0ceEA271728547f3802B36Fd;
+    IDistributionCreator(0x8BB4C975Ff3c250e0ceEA271728547f3802B36Fd);
   address public constant REWARD_TOKEN = AaveV2EthereumAssets.USDC_A_TOKEN;
   uint256 public constant REWARD_AMOUNT = 100_000e6;
 
   function execute() external {
-    IERC20(REWARD_TOKEN).approve(0x8BB4C975Ff3c250e0ceEA271728547f3802B36Fd, REWARD_AMOUNT);
+    // 1. send funds to executor
+    // 0.5% fee for angle merkle distributor
+    uint256 amountPlusFee = REWARD_AMOUNT + (REWARD_AMOUNT * 5) / 1000;
+    AaveV2Ethereum.COLLECTOR.transfer(
+      AaveV2EthereumAssets.USDC_A_TOKEN,
+      address(this),
+      amountPlusFee + 1 // account for imprecision on transfer
+    );
+    AaveV2Ethereum.POOL.withdraw(
+      AaveV2EthereumAssets.USDC_UNDERLYING,
+      amountPlusFee,
+      address(this)
+    );
+    // 2. approve to merkl
+    IERC20(REWARD_TOKEN).approve(0x8BB4C975Ff3c250e0ceEA271728547f3802B36Fd, amountPlusFee);
+    // 3. accept tos
     DISTRIBUTION_CREATOR.acceptConditions();
-    // DISTRIBUTION_CREATOR.createDistribution(newDistribution);
+    // 4.create campaign
+    IDistributionCreator.CampaignParameters memory newCampaign = IDistributionCreator
+      .CampaignParameters({
+        campaignId: '',
+        creator: address(0),
+        rewardToken: AaveV2EthereumAssets.USDC_UNDERLYING,
+        amount: 300_000e6,
+        campaignType: 4,
+        startTimestamp: uint32(block.timestamp + 2 hours),
+        duration: 1 hours,
+        campaignData: ''
+      });
+    DISTRIBUTION_CREATOR.createCampaign(newCampaign);
   }
 }
