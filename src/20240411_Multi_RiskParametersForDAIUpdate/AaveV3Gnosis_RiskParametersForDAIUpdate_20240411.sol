e9064ffe5f32214d69ea5bd8010bd2aacba75c77
Rozengarden <github@rozengarden.eu>
2024-04-11 09:09:26 +0200

    actualy do the correct things on gnosis

--- src/20240411_Multi_RiskParametersForDAIUpdate/AaveV3Gnosis_RiskParametersForDAIUpdate_20240411.sol
+++ src/20240411_Multi_RiskParametersForDAIUpdate/AaveV3Gnosis_RiskParametersForDAIUpdate_20240411.sol
@@ -5,7 +5,6 @@ import {AaveV3GnosisAssets} from 'aave-address-book/AaveV3Gnosis.sol';
 import {AaveV3PayloadGnosis} from 'aave-helpers/v3-config-engine/AaveV3PayloadGnosis.sol';
 import {EngineFlags} from 'aave-helpers/v3-config-engine/EngineFlags.sol';
 import {IAaveV3ConfigEngine} from 'aave-helpers/v3-config-engine/IAaveV3ConfigEngine.sol';
-import {IV3RateStrategyFactory} from 'aave-helpers/v3-config-engine/IV3RateStrategyFactory.sol';
 /**
  * @title Risk Parameters for DAI Update
  * @author Aave Chan Initiative
@@ -13,43 +12,32 @@ import {IV3RateStrategyFactory} from 'aave-helpers/v3-config-engine/IV3RateStrat
  * - Discussion: https://governance.aave.com/t/arfc-risk-parameters-for-dai-update/17211
  */
 contract AaveV3Gnosis_RiskParametersForDAIUpdate_20240411 is AaveV3PayloadGnosis {
-  function rateStrategiesUpdates()
+  function collateralsUpdates()
     public
     pure
     override
-    returns (IAaveV3ConfigEngine.RateStrategyUpdate[] memory)
+    returns (IAaveV3ConfigEngine.CollateralUpdate[] memory)
   {
-    IAaveV3ConfigEngine.RateStrategyUpdate[]
-      memory rateStrategies = new IAaveV3ConfigEngine.RateStrategyUpdate[](2);
-    rateStrategies[0] = IAaveV3ConfigEngine.RateStrategyUpdate({
+    IAaveV3ConfigEngine.CollateralUpdate[]
+      memory collateralUpdate = new IAaveV3ConfigEngine.CollateralUpdate[](2);
+
+    collateralUpdate[0] = IAaveV3ConfigEngine.CollateralUpdate({
       asset: AaveV3GnosisAssets.WXDAI_UNDERLYING,
-      params: IV3RateStrategyFactory.RateStrategyParams({
-        optimalUsageRatio: EngineFlags.KEEP_CURRENT,
-        baseVariableBorrowRate: EngineFlags.KEEP_CURRENT,
-        variableRateSlope1: EngineFlags.KEEP_CURRENT,
-        variableRateSlope2: EngineFlags.KEEP_CURRENT,
-        stableRateSlope1: EngineFlags.KEEP_CURRENT,
-        stableRateSlope2: EngineFlags.KEEP_CURRENT,
-        baseStableRateOffset: EngineFlags.KEEP_CURRENT,
-        stableRateExcessOffset: EngineFlags.KEEP_CURRENT,
-        optimalStableToTotalDebtRatio: EngineFlags.KEEP_CURRENT
-      })
+      ltv: 63_00,
+      liqThreshold: 77_00,
+      liqBonus: EngineFlags.KEEP_CURRENT,
+      debtCeiling: EngineFlags.KEEP_CURRENT,
+      liqProtocolFee: EngineFlags.KEEP_CURRENT
     });
-    rateStrategies[1] = IAaveV3ConfigEngine.RateStrategyUpdate({
+    collateralUpdate[1] = IAaveV3ConfigEngine.CollateralUpdate({
       asset: AaveV3GnosisAssets.sDAI_UNDERLYING,
-      params: IV3RateStrategyFactory.RateStrategyParams({
-        optimalUsageRatio: EngineFlags.KEEP_CURRENT,
-        baseVariableBorrowRate: EngineFlags.KEEP_CURRENT,
-        variableRateSlope1: EngineFlags.KEEP_CURRENT,
-        variableRateSlope2: EngineFlags.KEEP_CURRENT,
-        stableRateSlope1: EngineFlags.KEEP_CURRENT,
-        stableRateSlope2: EngineFlags.KEEP_CURRENT,
-        baseStableRateOffset: EngineFlags.KEEP_CURRENT,
-        stableRateExcessOffset: EngineFlags.KEEP_CURRENT,
-        optimalStableToTotalDebtRatio: EngineFlags.KEEP_CURRENT
-      })
+      ltv: 63_00,
+      liqThreshold: 77_00,
+      liqBonus: EngineFlags.KEEP_CURRENT,
+      debtCeiling: EngineFlags.KEEP_CURRENT,
+      liqProtocolFee: EngineFlags.KEEP_CURRENT
     });
 
-    return rateStrategies;
+    return collateralUpdate;
   }
 }
