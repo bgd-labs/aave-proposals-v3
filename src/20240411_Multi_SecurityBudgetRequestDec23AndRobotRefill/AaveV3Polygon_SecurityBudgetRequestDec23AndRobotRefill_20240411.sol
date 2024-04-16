2688f9e29a468e3f8653494fb7add926a5c2e05b
Harsh Pandey <harshsatishpandey@gmail.com>
2024-04-16 19:21:23 +0530

    chore: remove robot refill from security budget aip

--- src/20240411_Multi_SecurityBudgetRequestDec23AndRobotRefill/AaveV3Polygon_SecurityBudgetRequestDec23AndRobotRefill_20240411.sol
+++ src/20240411_Multi_SecurityBudgetRequestDec23AndRobotRefill/AaveV3Polygon_SecurityBudgetRequestDec23AndRobotRefill_20240411.sol
@@ -5,7 +5,7 @@ import {IProposalGenericExecutor} from 'aave-helpers/interfaces/IProposalGeneric
 import {AaveV2Polygon, AaveV2PolygonAssets} from 'aave-address-book/AaveV2Polygon.sol';
 
 /**
- * @title Security Budget Request Dec 23 and Robot Refill
+ * @title Security Budget Request December 2023
  * @author BGD Labs @bgdlabs
  * - Snapshot: https://snapshot.org/#/aave.eth/proposal/0xf95bc210e3e93c2112c694cb158db22c93504155b48c03d9358e4c41c33ee782
  * - Discussion: https://governance.aave.com/t/arfc-bgd-security-budget-request-december-2023/15783
@@ -13,16 +13,10 @@ import {AaveV2Polygon, AaveV2PolygonAssets} from 'aave-address-book/AaveV2Polygo
 contract AaveV3Polygon_SecurityBudgetRequestDec23AndRobotRefill_20240411 is
   IProposalGenericExecutor
 {
-  uint256 public constant WMATIC_AMOUNT_REIMBURSEMENT = 3500 ether;
-  uint256 public constant A_WMATIC_AMOUNT_REIMBURSEMENT = 500 ether;
+  uint256 public constant A_WMATIC_AMOUNT_REIMBURSEMENT = 4000 ether;
   address public constant BGD_RECIPIENT = 0xbCEB4f363f2666E2E8E430806F37e97C405c130b;
 
   function execute() external {
-    AaveV2Polygon.COLLECTOR.transfer(
-      AaveV2PolygonAssets.WMATIC_UNDERLYING,
-      BGD_RECIPIENT,
-      WMATIC_AMOUNT_REIMBURSEMENT
-    );
     AaveV2Polygon.COLLECTOR.transfer(
       AaveV2PolygonAssets.WMATIC_A_TOKEN,
       BGD_RECIPIENT,
--- src/20240411_Multi_SecurityBudgetRequestDec23AndRobotRefill/AaveV3Polygon_SecurityBudgetRequestDec23AndRobotRefill_20240411.t.sol
+++ src/20240411_Multi_SecurityBudgetRequestDec23AndRobotRefill/AaveV3Polygon_SecurityBudgetRequestDec23AndRobotRefill_20240411.t.sol
@@ -18,7 +18,7 @@ contract AaveV3Polygon_SecurityBudgetRequestDec23AndRobotRefill_20240411_Test is
   AaveV3Polygon_SecurityBudgetRequestDec23AndRobotRefill_20240411 internal proposal;
 
   function setUp() public {
-    vm.createSelectFork(vm.rpcUrl('polygon'), 55697469);
+    vm.createSelectFork(vm.rpcUrl('polygon'), 55891985);
     proposal = new AaveV3Polygon_SecurityBudgetRequestDec23AndRobotRefill_20240411();
   }
 
@@ -37,20 +37,7 @@ contract AaveV3Polygon_SecurityBudgetRequestDec23AndRobotRefill_20240411_Test is
     uint256 collectorAWmaticBalanceBefore = IERC20(AaveV2PolygonAssets.WMATIC_A_TOKEN).balanceOf(
       address(AaveV3Polygon.COLLECTOR)
     );
-    uint256 collectorWmaticBalanceBefore = IERC20(AaveV2PolygonAssets.WMATIC_UNDERLYING).balanceOf(
-      address(AaveV3Polygon.COLLECTOR)
-    );
-
-    // Validate the Collector has enough aWMatic v2
-    assertGe(collectorAWmaticBalanceBefore, proposal.A_WMATIC_AMOUNT_REIMBURSEMENT());
-
-    // Validate the Collector has enough WMATIC
-    assertGe(collectorWmaticBalanceBefore, proposal.WMATIC_AMOUNT_REIMBURSEMENT());
-
     uint256 recipientAWmaticBalanceBefore = IERC20(AaveV2PolygonAssets.WMATIC_A_TOKEN).balanceOf(
-      proposal.BGD_RECIPIENT()
-    );
-    uint256 recipientWmaticBalanceBefore = IERC20(AaveV2PolygonAssets.WMATIC_UNDERLYING).balanceOf(
       proposal.BGD_RECIPIENT()
     );
 
@@ -59,8 +46,8 @@ contract AaveV3Polygon_SecurityBudgetRequestDec23AndRobotRefill_20240411_Test is
     uint256 recipientAWmaticBalanceAfter = IERC20(AaveV2PolygonAssets.WMATIC_A_TOKEN).balanceOf(
       proposal.BGD_RECIPIENT()
     );
-    uint256 recipientWmaticBalanceAfter = IERC20(AaveV2PolygonAssets.WMATIC_UNDERLYING).balanceOf(
-      proposal.BGD_RECIPIENT()
+    uint256 collectorAWmaticBalanceAfter = IERC20(AaveV2PolygonAssets.WMATIC_A_TOKEN).balanceOf(
+      address(AaveV3Polygon.COLLECTOR)
     );
 
     assertApproxEqAbs(
@@ -68,27 +55,10 @@ contract AaveV3Polygon_SecurityBudgetRequestDec23AndRobotRefill_20240411_Test is
       recipientAWmaticBalanceBefore + proposal.A_WMATIC_AMOUNT_REIMBURSEMENT(),
       1
     );
-    assertApproxEqAbs(
-      recipientWmaticBalanceAfter,
-      recipientWmaticBalanceBefore + proposal.WMATIC_AMOUNT_REIMBURSEMENT(),
-      1
-    );
-
-    uint256 collectorAWmaticBalanceAfter = IERC20(AaveV2PolygonAssets.WMATIC_A_TOKEN).balanceOf(
-      address(AaveV3Polygon.COLLECTOR)
-    );
-    uint256 collectorWmaticBalanceAfter = IERC20(AaveV2PolygonAssets.WMATIC_UNDERLYING).balanceOf(
-      address(AaveV3Polygon.COLLECTOR)
-    );
 
     assertApproxEqAbs(
       collectorAWmaticBalanceAfter,
       collectorAWmaticBalanceBefore - proposal.A_WMATIC_AMOUNT_REIMBURSEMENT(),
-      3
-    );
-    assertApproxEqAbs(
-      collectorWmaticBalanceAfter,
-      collectorWmaticBalanceBefore - proposal.WMATIC_AMOUNT_REIMBURSEMENT(),
       3
     );
   }
