5d8bea9a5e6063ccf309b0bf0a5f79a6b63634ef
Eyal Ovadya <eyal.ovadya7@gmail.com>
2024-04-18 14:50:19 +0300

    Fix

--- src/20240415_AaveV3Optimism_RiskParameterUpdatesOPOnV3Optimism/config.ts
+++ src/20240415_AaveV3Optimism_RiskParameterUpdatesOPOnV3Optimism/config.ts
@@ -8,7 +8,8 @@ export const config: ConfigFile = {
     author: 'Chaos Labs',
     discussion:
       'https://governance.aave.com/t/arfc-chaos-labs-risk-parameter-updates-op-on-v3-optimism/17326',
-    snapshot: '',
+    snapshot:
+      'https://snapshot.org/#/aave.eth/proposal/0xbaaf99c1e738da5755166fc6b1429265243507fdb9638a31ca177dd81a8b7313',
   },
   poolOptions: {
     AaveV3Optimism: {
