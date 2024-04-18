8c6f62691306aef3f9ca76c3b59eeb7482a09a11
Eyal Ovadya <eyal.ovadya7@gmail.com>
2024-04-18 15:25:20 +0300

    Fix

--- src/20240415_AaveV3Ethereum_ChaosLabsEngagementAmendment/AaveV3Ethereum_ChaosLabsEngagementAmendment_20240415.sol
+++ src/20240415_AaveV3Ethereum_ChaosLabsEngagementAmendment/AaveV3Ethereum_ChaosLabsEngagementAmendment_20240415.sol
@@ -13,13 +13,11 @@ contract AaveV3Ethereum_ChaosLabsEngagementAmendment_20240415 is IProposalGeneri
   address public constant CHAOS_LABS_TREASURY = 0xbC540e0729B732fb14afA240aA5A047aE9ba7dF0;
   uint256 public constant STREAM_AMOUNT_GHO = 400_000 ether;
   uint256 public constant STREAM_DURATION = 200 days;
-  uint256 public constant ACTUAL_STREAM_AMOUNT_GHO =
-    (STREAM_AMOUNT_GHO / STREAM_DURATION) * STREAM_DURATION;
 
   function execute() external {
     AaveV3Ethereum.COLLECTOR.createStream(
       CHAOS_LABS_TREASURY,
-      ACTUAL_STREAM_AMOUNT_GHO,
+      STREAM_AMOUNT_GHO,
       AaveV3EthereumAssets.GHO_UNDERLYING,
       block.timestamp,
       block.timestamp + STREAM_DURATION
