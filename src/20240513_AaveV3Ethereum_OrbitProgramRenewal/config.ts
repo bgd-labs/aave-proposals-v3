1d3df8c3ba5ddcd9f818f811399f9bbd6d3c4900
marczeller <pierarte@gmail.com>
2024-05-30 11:11:20 +0000

    lint fix

--- src/20240513_AaveV3Ethereum_OrbitProgramRenewal/config.ts
+++ src/20240513_AaveV3Ethereum_OrbitProgramRenewal/config.ts
@@ -7,7 +7,8 @@ export const config: ConfigFile = {
     shortName: 'OrbitProgramRenewal',
     date: '20240513',
     discussion: 'https://governance.aave.com/t/arfc-orbit-program-renewal-may-2024/17683',
-    snapshot: 'https://snapshot.org/#/aave.eth/proposal/0x4a10e2a8ca95024d7cf0791aa82ed262c816ff0ee78bc2f3ab3487e70d731361',
+    snapshot:
+      'https://snapshot.org/#/aave.eth/proposal/0x4a10e2a8ca95024d7cf0791aa82ed262c816ff0ee78bc2f3ab3487e70d731361',
     votingNetwork: 'POLYGON',
   },
   poolOptions: {AaveV3Ethereum: {configs: {OTHERS: {}}, cache: {blockNumber: 19862601}}},
