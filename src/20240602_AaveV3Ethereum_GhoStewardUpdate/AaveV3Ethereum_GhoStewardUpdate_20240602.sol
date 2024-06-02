c462c27a1487e3441ffeba1865a062b815eff3c3
marczeller <pierarte@gmail.com>
2024-06-02 09:05:24 +0000

    GHo steward Update

---
+++ src/20240602_AaveV3Ethereum_GhoStewardUpdate/AaveV3Ethereum_GhoStewardUpdate_20240602.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IProposalGenericExecutor} from 'aave-helpers/interfaces/IProposalGenericExecutor.sol';
+import {IGhoStewardV2} from './interfaces/IGhoStewardV2.sol';
+import {IGhoToken} from './interfaces/IGho.sol';
+import {IGsm} from './interfaces/IGsm.sol';
+import {AaveV3Ethereum} from 'aave-address-book/AaveV3Ethereum.sol';
+import {MiscEthereum} from 'aave-address-book/MiscEthereum.sol';
+
+/**
+ * @title Gho Steward Update
+ * @author ACI
+ * - Discussion: https://governance.aave.com/t/arfc-gho-stewards/16466/11
+ */
+contract AaveV3Ethereum_GhoStewardUpdate_20240602 is IProposalGenericExecutor {
+  address public constant GHO_STEWARD = 0x586de14a41af900d6fda3c57f1dacddd5b2a2c97;
+
+  function execute() external {
+    // Give Risk admin role to the steward
+    AaveV3Ethereum.ACL_MANAGER.addRiskAdmin(GHO_STEWARD);
+
+    // Give bucket manager role to the steward
+    IGhoToken(MiscEthereum.GHO_TOKEN).grantRole(
+      IGhoToken(MiscEthereum.GHO_TOKEN).BUCKET_MANAGER_ROLE(),
+      GHO_STEWARD
+    );
+
+    // Give configurator role on usdc, usdt gsm to the stewards
+    IGsm(MiscEthereum.GSM_USDC).grantRole(
+      IGsm(MiscEthereum.GSM_USDC).CONFIGURATOR_ROLE(),
+      GHO_STEWARD
+    );
+    IGsm(MiscEthereum.GSM_USDT).grantRole(
+      IGsm(MiscEthereum.GSM_USDT).CONFIGURATOR_ROLE(),
+      GHO_STEWARD
+    );
+
+    // Whitelist all the facilitators on the stewards, including: GhoAToken, GhoFlashMinter, GSM USDC, GSM USDT
+    IGhoStewardV2(GHO_STEWARD).setControlledFacilitator(
+      IGhoToken(MiscEthereum.GHO_TOKEN).getFacilitatorsList(),
+      true
+    );
+  }
+}
---
+++ src/20240602_AaveV3Ethereum_GhoStewardUpdate/GhoStewardUpdate.md
@@ -0,0 +1,22 @@
+---
+title: "Gho Steward Update"
+author: "ACI"
+discussions: "https://governance.aave.com/t/arfc-gho-stewards/16466/11"
+---
+
+## Simple Summary
+
+## Motivation
+
+## Specification
+
+## References
+
+- Implementation: [AaveV3Ethereum](https://github.com/bgd-labs/aave-proposals-v3/blob/main/src/20240602_AaveV3Ethereum_GhoStewardUpdate/AaveV3Ethereum_GhoStewardUpdate_20240602.sol)
+- Tests: [AaveV3Ethereum](https://github.com/bgd-labs/aave-proposals-v3/blob/main/src/20240602_AaveV3Ethereum_GhoStewardUpdate/AaveV3Ethereum_GhoStewardUpdate_20240602.t.sol)
+- [Snapshot](TODO)
+- [Discussion](https://governance.aave.com/t/arfc-gho-stewards/16466/11)
+
+## Copyright
+
+Copyright and related rights waived via [CC0](https://creativecommons.org/publicdomain/zero/1.0/).
