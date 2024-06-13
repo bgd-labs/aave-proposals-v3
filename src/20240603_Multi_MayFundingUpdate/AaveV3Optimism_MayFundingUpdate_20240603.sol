0f33426c524cb8996dc65b64ae4bbc28a288148f
defijesus.eth <defijesus@duck.com>
2024-06-13 04:34:58 +0100

    initial commit

---
+++ src/20240603_Multi_MayFundingUpdate/AaveV3Optimism_MayFundingUpdate_20240603.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IProposalGenericExecutor} from 'aave-helpers/interfaces/IProposalGenericExecutor.sol';
+
+/**
+ * @title May Funding Update
+ * @author karpatkey_TokenLogic
+ * - Snapshot: Direct-to-AIP
+ * - Discussion: https://governance.aave.com/t/arfc-may-funding-update/17768
+ */
+contract AaveV3Optimism_MayFundingUpdate_20240603 is IProposalGenericExecutor {
+  function execute() external {
+    // custom code goes here
+  }
+}
