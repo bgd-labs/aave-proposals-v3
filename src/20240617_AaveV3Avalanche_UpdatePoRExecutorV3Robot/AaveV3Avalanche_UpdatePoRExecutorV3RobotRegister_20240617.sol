9d97c54842ce1e38817def55604851b486aa5e67
Rustem Kurmaev <rustboyar@gmail.com>
2024-06-17 15:41:13 +0200

    Proposal to update proof of reserve robot

---
+++ src/20240617_AaveV3Avalanche_UpdatePoRExecutorV3Robot/AaveV3Avalanche_UpdatePoRExecutorV3RobotRegister_20240617.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {AaveV3Avalanche, AaveV3AvalancheAssets} from 'aave-address-book/AaveV3Avalanche.sol';
+import {IProposalGenericExecutor} from 'aave-helpers/interfaces/IProposalGenericExecutor.sol';
+import {IAaveCLRobotOperator} from './interfaces/IAaveCLRobotOperator.sol';
+import {IERC20} from 'solidity-utils/contracts/oz-common/interfaces/IERC20.sol';
+import {SafeERC20} from 'solidity-utils/contracts/oz-common/SafeERC20.sol';
+import {SafeCast} from 'solidity-utils/contracts/oz-common/SafeCast.sol';
+
+/**
+ * @title Update PoR Executor V3 Robot
+ * @author BGD Labs (@bgdlabs)
+ * - Snapshot: TODO
+ * - Discussion: TODO
+ */
+contract AaveV3Avalanche_UpdatePoRExecutorV3RobotRegister_20240617 is IProposalGenericExecutor {
+  using SafeERC20 for IERC20;
+  using SafeCast for uint256;
+
+  address public constant ROBOT_OPERATOR = 0x06d958772304e7220fc3E463756CE01Ed0D24db2;
+  uint256 public constant OLD_POR_ROBOT_ID =
+    26365172985027002678612464504385127359853428975895609819521748772469113961166;
+
+  address public constant PROOF_OF_RESERVE_ROBOT_ADDRESS =
+    0x7aE2930B50CFEbc99FE6DB16CE5B9C7D8d09332C;
+  address public constant PROOF_OF_RESERVE_EXECUTOR_V3 = 0xB94e515615c244Ab25f7A6e592e3Cb7EE31E99F4;
+  uint256 public constant LINK_AMOUNT = 15 ether;
+
+  function execute() external {
+    IAaveCLRobotOperator(ROBOT_OPERATOR).withdrawLink(OLD_POR_ROBOT_ID);
+
+    AaveV3Avalanche.COLLECTOR.transfer(
+      AaveV3AvalancheAssets.LINKe_UNDERLYING,
+      address(this),
+      LINK_AMOUNT
+    );
+
+    IERC20(AaveV3AvalancheAssets.LINKe_UNDERLYING).forceApprove(ROBOT_OPERATOR, LINK_AMOUNT);
+
+    IAaveCLRobotOperator(ROBOT_OPERATOR).register(
+      'Proof Of Reserve Robot V3',
+      PROOF_OF_RESERVE_ROBOT_ADDRESS,
+      abi.encode(PROOF_OF_RESERVE_EXECUTOR_V3),
+      2_500_000,
+      LINK_AMOUNT.toUint96(),
+      0,
+      ''
+    );
+  }
+}
---
+++ src/20240617_AaveV3Avalanche_UpdatePoRExecutorV3Robot/AaveV3Avalanche_UpdatePoRExecutorV3RobotRegister_20240617.t.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {AaveV3Avalanche} from 'aave-address-book/AaveV3Avalanche.sol';
+
+import 'forge-std/Test.sol';
+import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
+import {AaveV3Avalanche_UpdatePoRExecutorV3RobotCancel_20240617} from './AaveV3Avalanche_UpdatePoRExecutorV3RobotCancel_20240617.sol';
+import {AaveV3Avalanche_UpdatePoRExecutorV3RobotRegister_20240617} from './AaveV3Avalanche_UpdatePoRExecutorV3RobotRegister_20240617.sol';
+import {IAaveCLRobotOperator} from './interfaces/IAaveCLRobotOperator.sol';
+
+/**
+ * @dev Test for AaveV3Avalanche_UpdatePoRExecutorV3RobotRegister_20240617
+ * command: FOUNDRY_PROFILE=avalanche forge test --match-path=src/20240617_AaveV3Avalanche_UpdatePoRExecutorV3Robot/AaveV3Avalanche_UpdatePoRExecutorV3RobotRegister_20240617.t.sol -vv
+ */
+contract AaveV3Avalanche_UpdatePoRExecutorV3RobotRegister_20240617_Test is ProtocolV3TestBase {
+  AaveV3Avalanche_UpdatePoRExecutorV3RobotCancel_20240617 internal cancelProposal;
+  AaveV3Avalanche_UpdatePoRExecutorV3RobotRegister_20240617 internal proposal;
+
+  event KeeperRegistered(uint256 indexed id, address indexed upkeep, uint96 indexed amount);
+
+  function setUp() public {
+    vm.createSelectFork(vm.rpcUrl('avalanche'), 46823054);
+    cancelProposal = new AaveV3Avalanche_UpdatePoRExecutorV3RobotCancel_20240617();
+    proposal = new AaveV3Avalanche_UpdatePoRExecutorV3RobotRegister_20240617();
+
+    // execute the payload to cancel the robot and withraw link to collector
+    executePayload(vm, address(cancelProposal));
+
+    // after robot cancel we need to wait for some blocks to withdraw so we fast-forward
+    vm.roll(block.number + 50);
+    IAaveCLRobotOperator(cancelProposal.ROBOT_OPERATOR()).withdrawLink(
+      cancelProposal.OLD_POR_ROBOT_ID()
+    );
+  }
+
+  /**
+   * @dev executes the generic test suite including e2e and config snapshots
+   */
+  function test_defaultProposalExecution() public {
+    defaultTest(
+      'AaveV3Avalanche_UpdatePoRExecutorV3RobotRegister_20240617',
+      AaveV3Avalanche.POOL,
+      address(proposal)
+    );
+  }
+
+  function test_robotsRegistered() public {
+    vm.expectEmit(false, true, true, true);
+    emit KeeperRegistered(
+      uint256(0),
+      proposal.PROOF_OF_RESERVE_ROBOT_ADDRESS(),
+      uint96(proposal.LINK_AMOUNT())
+    );
+
+    executePayload(vm, address(proposal));
+  }
+}
