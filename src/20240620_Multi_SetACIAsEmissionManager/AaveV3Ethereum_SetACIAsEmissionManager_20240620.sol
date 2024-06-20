860e6954c17741b9c5b0a0440313483cd535e4c8
Marc Zeller <21088542+marczeller@users.noreply.github.com>
2024-06-20 18:19:40 +0200

    Update src/20240620_Multi_SetACIAsEmissionManager/AaveV3Ethereum_SetACIAsEmissionManager_20240620.sol
    Co-authored-by: sendra <sendra@users.noreply.github.com>

--- src/20240620_Multi_SetACIAsEmissionManager/AaveV3Ethereum_SetACIAsEmissionManager_20240620.sol
+++ src/20240620_Multi_SetACIAsEmissionManager/AaveV3Ethereum_SetACIAsEmissionManager_20240620.sol
@@ -9,7 +9,7 @@ import {IEmissionManager} from 'aave-v3-periphery/contracts/rewards/interfaces/I
  * @title Set ACI as Emission Manager
  * @author ACI
  * - Snapshot: https://snapshot.org/#/aave.eth/proposal/0x364de11d3a298f2c76721a8926cb32823cc29d0a95eadecbc0a98c628a38194b
- * - Discussion: https://governance.aave.com/t/arfc-set-aci-as-emission-manager-for-liquidity-mining-programs/17898#arfc-set-aci-as-emission-manager-for-liquidity-mining-programs-1"
+ * - Discussion: https://governance.aave.com/t/arfc-set-aci-as-emission-manager-for-liquidity-mining-programs/17898#arfc-set-aci-as-emission-manager-for-liquidity-mining-programs-1
  */
 
 contract AaveV3Ethereum_SetACIAsEmissionManager_20240620 is IProposalGenericExecutor {
