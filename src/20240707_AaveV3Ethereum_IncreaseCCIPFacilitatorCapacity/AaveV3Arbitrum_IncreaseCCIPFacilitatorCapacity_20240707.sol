f8c65e9d27cb03bc2f137228c6e53b4221e303d9
efecarranza <fcarr004@fiu.edu>
2024-07-09 11:55:32 -0400

    feat: add arbitrum side

---
+++ src/20240707_AaveV3Ethereum_IncreaseCCIPFacilitatorCapacity/AaveV3Arbitrum_IncreaseCCIPFacilitatorCapacity_20240707.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {AaveV3ArbitrumAssets} from 'aave-address-book/AaveV3Arbitrum.sol';
+import {IProposalGenericExecutor} from 'aave-helpers/interfaces/IProposalGenericExecutor.sol';
+
+interface IGHOToken {
+  function setFacilitatorBucketCapacity(address facilitator, uint128 limit) external;
+}
+
+/**
+ * @title Increase CCIP Facilitator Capacity
+ * @author @karpatkey_TokenLogic
+ * - Snapshot: Direct-to-AIP
+ * - Discussion: https://governance.aave.com/t/arfc-gho-arb-increase-ccip-facilitator-capacity/18169
+ */
+contract AaveV3Arbitrum_IncreaseCCIPFacilitatorCapacity_20240707 is IProposalGenericExecutor {
+  address public constant FACILITATOR = 0xF168B83598516A532a85995b52504a2Fa058C068;
+  uint128 public constant NEW_LIMIT = 2_500_000 ether;
+
+  function execute() external {
+    IGHOToken(AaveV3ArbitrumAssets.GHO_UNDERLYING).setFacilitatorBucketCapacity(
+      FACILITATOR,
+      NEW_LIMIT
+    );
+  }
+}
--- src/20240707_AaveV3Ethereum_IncreaseCCIPFacilitatorCapacity/AaveV3Ethereum_IncreaseCCIPFacilitatorCapacity_20240707.sol
+++ src/20240707_AaveV3Ethereum_IncreaseCCIPFacilitatorCapacity/AaveV3Ethereum_IncreaseCCIPFacilitatorCapacity_20240707.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import {MiscEthereum} from 'aave-address-book/MiscEthereum.sol';
 import {IProposalGenericExecutor} from 'aave-helpers/interfaces/IProposalGenericExecutor.sol';
 
 interface UpgradeableLockReleaseTokenPool {
@@ -14,9 +15,9 @@ interface UpgradeableLockReleaseTokenPool {
  * - Discussion: https://governance.aave.com/t/arfc-gho-arb-increase-ccip-facilitator-capacity/18169
  */
 contract AaveV3Ethereum_IncreaseCCIPFacilitatorCapacity_20240707 is IProposalGenericExecutor {
-  address public constant GHO_TOKEN_POOL = 0x5756880B6a1EAba0175227bf02a7E87c1e02B28C;
   uint256 public constant NEW_LIMIT = 2_500_000 ether;
+
   function execute() external {
-    UpgradeableLockReleaseTokenPool(GHO_TOKEN_POOL).setBridgeLimit(NEW_LIMIT);
+    UpgradeableLockReleaseTokenPool(MiscEthereum.GHO_CCIP_TOKEN_POOL).setBridgeLimit(NEW_LIMIT);
   }
 }
