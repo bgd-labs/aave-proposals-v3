8afc65062f8f75343ad8e32d6d9fd94a53251611
Alice <121383428+Rozengarden@users.noreply.github.com>
2024-07-19 16:19:56 +0200

    Update src/20240716_Multi_ReduceReserveFactorOnWstETH/AaveV3Arbitrum_ReduceReserveFactorOnWstETH_20240716.sol
    Co-authored-by: Harsh Pandey <harshsatishpandey@gmail.com>

--- src/20240716_Multi_ReduceReserveFactorOnWstETH/AaveV3Arbitrum_ReduceReserveFactorOnWstETH_20240716.sol
+++ src/20240716_Multi_ReduceReserveFactorOnWstETH/AaveV3Arbitrum_ReduceReserveFactorOnWstETH_20240716.sol
@@ -25,9 +25,9 @@ contract AaveV3Arbitrum_ReduceReserveFactorOnWstETH_20240716 is AaveV3PayloadArb
       asset: AaveV3ArbitrumAssets.wstETH_UNDERLYING,
       enabledToBorrow: EngineFlags.KEEP_CURRENT,
       flashloanable: EngineFlags.KEEP_CURRENT,
-      stableRateModeEnabled: EngineFlags.DISABLED,
-      borrowableInIsolation: EngineFlags.DISABLED,
-      withSiloedBorrowing: EngineFlags.DISABLED,
+      stableRateModeEnabled: EngineFlags.KEEP_CURRENT,
+      borrowableInIsolation: EngineFlags.KEEP_CURRENT,
+      withSiloedBorrowing: EngineFlags.KEEP_CURRENT,
       reserveFactor: 5_00
     });
 
