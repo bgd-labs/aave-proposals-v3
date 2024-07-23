cfcb7cd124a398047d8c0fb7c033c2e0dec8b6bb
sakulstra <lukasstrassel@googlemail.com>
2024-07-23 14:22:35 +0200

    fix: generator isolation flag no longer set wrongfully

--- generator/features/assetListing.spec.ts
+++ generator/features/assetListing.spec.ts
@@ -35,4 +35,24 @@ describe('feature: assetListing', () => {
     const files = await generateFiles(MOCK_OPTIONS, poolConfigs);
     expect(files).toMatchSnapshot();
   });
+
+  it('regression: isolation mode should be flase when ceiling is zero', async () => {
+    const poolConfigs: PoolConfigs = {
+      [MOCK_OPTIONS.pools[0]]: {
+        pool: MOCK_OPTIONS.pools[0],
+        artifacts: [
+          assetListing.build({
+            options: MOCK_OPTIONS,
+            pool: 'AaveV3Ethereum',
+            cfg: [{...assetListingConfig[0], debtCeiling: '0'}],
+            cache: {blockNumber: 42},
+          }),
+        ],
+        configs: {[FEATURE.ASSET_LISTING]: [{...assetListingConfig[0], debtCeiling: 0}]},
+        cache: {blockNumber: 42},
+      },
+    };
+    const files = await generateFiles(MOCK_OPTIONS, poolConfigs);
+    expect(files).toMatchSnapshot();
+  });
 });
