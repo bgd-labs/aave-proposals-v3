c63bac4965415f43fa80999dfe235a3dc003a168
Rustem Kurmaev <rustboyar@gmail.com>
2024-08-02 17:21:52 +0200

    Renewal of Aave Guardian (#376)
    * AaveV2 protocol guardian update
    
    * Aave V3 protocol guardian update
    
    * aip text
    
    * add governance update
    
    * governance guardian update
    
    * upda imports and test blocks
    
    * update snapshot link

---
+++ src/20240708_Multi_RenewalOfAaveGuardian2024/AaveV3Optimism_RenewalOfAaveGuardian2024_20240708.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {AaveV3Optimism} from 'aave-address-book/AaveV3Optimism.sol';
+import {MiscOptimism} from 'aave-address-book/MiscOptimism.sol';
+import {GovernanceV3Optimism} from 'aave-address-book/GovernanceV3Optimism.sol';
+
+import {RenewalV3Params, RenewalV3BasePayload} from './RenewalV3BasePayload.sol';
+import {ProtocolGuardians, GovernanceGuardians} from './Guardians.sol';
+/**
+ * @title Renewal of Aave Guardian 2024
+ * @author BGD Labs @bgdlabs
+ * - Snapshot: https://snapshot.org/#/aave.eth/proposal/0x003ce30676805f71e5b356745fb3f01e5f82b8d1655750aaef46c7ed4a0a3578
+ * - Discussion: https://governance.aave.com/t/arfc-renewal-of-aave-guardian-2024/17523
+ */
+contract AaveV3Optimism_RenewalOfAaveGuardian2024_20240708 is RenewalV3BasePayload {
+  constructor()
+    RenewalV3BasePayload(
+      RenewalV3Params({
+        aclManager: AaveV3Optimism.ACL_MANAGER,
+        governance: address(0),
+        payloadsController: address(GovernanceV3Optimism.PAYLOADS_CONTROLLER),
+        protocolGuardian: ProtocolGuardians.OPTIMISM_GUARDIAN,
+        governanceGuardian: GovernanceGuardians.OPTIMISM_GUARDIAN,
+        oldGuardian: MiscOptimism.PROTOCOL_GUARDIAN
+      })
+    )
+  {}
+}
