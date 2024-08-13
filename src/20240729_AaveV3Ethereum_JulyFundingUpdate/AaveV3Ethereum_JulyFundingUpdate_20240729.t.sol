7ca17148da9572cea53104c48740357b157cc364
defijesus.eth <defijesus@duck.com>
2024-08-13 15:59:52 +0100

    add swap prices tests

--- src/20240729_AaveV3Ethereum_JulyFundingUpdate/AaveV3Ethereum_JulyFundingUpdate_20240729.t.sol
+++ src/20240729_AaveV3Ethereum_JulyFundingUpdate/AaveV3Ethereum_JulyFundingUpdate_20240729.t.sol
@@ -64,7 +64,12 @@ contract AaveV3Ethereum_JulyFundingUpdate_20240729_Test is ProtocolV3TestBase {
     assertEq(wethAllowanceAmount, proposal.WETH_AMOUNT());
   }
 
-  function test_swapEvents() public {
+  function test_swapEventsAndPrices() public {
+    uint256 expectedInUsde = 112014478259269445050414;
+    uint256 expectedInUsdt = 499999999999;
+    uint256 expectedInFrax = 31935428199756049088619;
+    uint256 expectedInDpi = 133444159197713527567;
+
     vm.expectEmit(true, true, true, true, MiscEthereum.AAVE_SWAPPER);
     emit SwapRequested(
       proposal.MILKMAN(),
@@ -72,7 +77,7 @@ contract AaveV3Ethereum_JulyFundingUpdate_20240729_Test is ProtocolV3TestBase {
       AaveV3EthereumAssets.GHO_UNDERLYING,
       proposal.USDE_FEED(),
       proposal.GHO_USD_FEED(),
-      112014478259269445050414, // Hardcoded as dynamic
+      expectedInUsde, // Hardcoded as dynamic
       address(AaveV3Ethereum.COLLECTOR),
       50
     );
@@ -84,7 +89,7 @@ contract AaveV3Ethereum_JulyFundingUpdate_20240729_Test is ProtocolV3TestBase {
       AaveV3EthereumAssets.GHO_UNDERLYING,
       proposal.USDT_FEED(),
       proposal.GHO_USD_FEED(),
-      499999999999, // Hardcoded as dynamic
+      expectedInUsdt, // Hardcoded as dynamic
       address(AaveV3Ethereum.COLLECTOR),
       50
     );
@@ -96,7 +101,7 @@ contract AaveV3Ethereum_JulyFundingUpdate_20240729_Test is ProtocolV3TestBase {
       AaveV3EthereumAssets.GHO_UNDERLYING,
       proposal.FRAX_FEED(),
       proposal.GHO_USD_FEED(),
-      31935428199756049088619, // Hardcoded as dynamic
+      expectedInFrax, // Hardcoded as dynamic
       address(AaveV3Ethereum.COLLECTOR),
       100
     );
@@ -108,12 +113,56 @@ contract AaveV3Ethereum_JulyFundingUpdate_20240729_Test is ProtocolV3TestBase {
       AaveV3EthereumAssets.GHO_UNDERLYING,
       proposal.DPI_FEED(),
       proposal.GHO_USD_FEED(),
-      133444159197713527567, // Hardcoded as dynamic
+      expectedInDpi, // Hardcoded as dynamic
       address(AaveV3Ethereum.COLLECTOR),
       500
     );
 
     executePayload(vm, address(proposal));
+
+    _baseSwapTest(
+      proposal.PRICE_CHECKER(),
+      expectedInUsde, // DAI/USD ~ 1.0 exchange rate on  30/07/2024
+      expectedInUsde,
+      15e14, // 1e18 is 100%, 15e14 is 0.15%
+      AaveV3EthereumAssets.USDe_UNDERLYING,
+      AaveV3EthereumAssets.GHO_UNDERLYING,
+      proposal.USDE_FEED(),
+      AaveV3EthereumAssets.GHO_ORACLE
+    );
+
+    _baseSwapTest(
+      proposal.PRICE_CHECKER(),
+      499999e18, // usdt is 6 decimals we get 18 out
+      expectedInUsdt,
+      3e14, // 1e18 is 100%, 3e14 is 0.03%
+      AaveV3EthereumAssets.USDT_UNDERLYING,
+      AaveV3EthereumAssets.GHO_UNDERLYING,
+      proposal.USDT_FEED(),
+      AaveV3EthereumAssets.GHO_ORACLE
+    );
+
+    _baseSwapTest(
+      proposal.PRICE_CHECKER(),
+      expectedInFrax, // FRAX/USD ~ 1.0 exchange rate on  30/07/2024
+      expectedInFrax,
+      4e15, // 1e18 is 100%, 4e15 is 0.4%
+      AaveV3EthereumAssets.FRAX_UNDERLYING,
+      AaveV3EthereumAssets.GHO_UNDERLYING,
+      proposal.FRAX_FEED(),
+      AaveV3EthereumAssets.GHO_ORACLE
+    );
+
+    _baseSwapTest(
+      proposal.PRICE_CHECKER(),
+      expectedInDpi * 90, // DPI/USD ~ 90 exchange rate on  30/07/2024
+      expectedInDpi,
+      20e14, // 1e18 is 100%, 20e14 is 0.20%
+      AaveV2EthereumAssets.DPI_UNDERLYING,
+      AaveV3EthereumAssets.GHO_UNDERLYING,
+      proposal.DPI_FEED(),
+      AaveV3EthereumAssets.GHO_ORACLE
+    );
   }
 
   function test_withdrawAndSwapForGho() public {
@@ -185,4 +234,78 @@ contract AaveV3Ethereum_JulyFundingUpdate_20240729_Test is ProtocolV3TestBase {
     assertApproxEqAbs(collectorAdpiv2BalanceAfter, 1e18, 50e18);
     assertApproxEqAbs(swapperDpiBalance, collectorAdpiv2BalanceBefore, 50e18);
   }
+
+  function test_swapPrices() public {}
+
+  /// Basic test for swaps that checks the prices returned make sense.
+  /// For example, if both oracles are the same base (USD or ETH), then
+  /// getting prices from tradingview.com and using them as `expectedOut`
+  /// should work with this test and the other parameters.
+  ///
+  /// Example: On August 3, 2024, I want to swap 3013 USDC for 1 WETH. If
+  /// both oracles have the same base USD, then this is going to work. However,
+  /// if for example USDC has base ETH, it's going to fail because:
+  /// USDC/ETH = 1 / 3013, rather than USDC/USD = 1 / 1. If the opposite oracle
+  /// is ETH/USD then it's going to be 3013 / 1. Sending expected out of 1 ETH
+  /// will fail this test because the actual oracles are going to return 1/3013 ETH.
+  ///
+  /// This test will also ensure the oracles have the necessary functions or it will
+  /// revert. For example, if the oracle does not have the `decimals()` function as
+  /// some oracles do, then it will fail ahead of time.
+  ///
+  /// @param priceChecker The price checker used by Milkman
+  /// @param expectedOut The amount we've manually checked we should be getting
+  /// @param amountIn The amount of from token to be swapped
+  /// @param maxDelta The maximum amount of difference in % between expectedOut and calculatedOut
+  /// @param from The token to swap from
+  /// @param to The token to swap to
+  /// @param fromOracle The oracle of the token to swap from
+  /// @param toOracle The oracle of the token to swap to
+  function _baseSwapTest(
+    address priceChecker,
+    uint256 expectedOut,
+    uint256 amountIn,
+    uint256 maxDelta,
+    address from,
+    address to,
+    address fromOracle,
+    address toOracle
+  ) internal {
+    address calc = IPriceChecker(priceChecker).EXPECTED_OUT_CALCULATOR();
+    uint256 outCalc = ICalculator(calc).getExpectedOut(
+      amountIn,
+      from,
+      to,
+      _encodeOracles(fromOracle, toOracle)
+    );
+
+    assertApproxEqRel(outCalc, expectedOut, maxDelta);
+  }
+
+  function _encodeOracles(
+    address fromOracle,
+    address toOracle
+  ) internal pure returns (bytes memory) {
+    address[] memory paths = new address[](2);
+    paths[0] = fromOracle;
+    paths[1] = toOracle;
+
+    bool[] memory reverses = new bool[](2);
+    reverses[1] = true;
+
+    return abi.encode(paths, reverses);
+  }
+}
+
+interface IPriceChecker {
+  function EXPECTED_OUT_CALCULATOR() external returns (address);
+}
+
+interface ICalculator {
+  function getExpectedOut(
+    uint256 _amountIn,
+    address _fromToken,
+    address _toToken,
+    bytes calldata _data
+  ) external view returns (uint256);
 }
