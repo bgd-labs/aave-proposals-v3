ca7845aed97c752f4b8414903c677cc13256858e
Harsh Pandey <harshsatishpandey@gmail.com>
2024-08-26 14:00:25 +0530

    feat: gho stewards update

---
+++ src/20240826_AaveV3Ethereum_GHOStewardUpdate/AaveV3Ethereum_GHOStewardUpdate_20240826.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IProposalGenericExecutor} from 'aave-helpers/src/interfaces/IProposalGenericExecutor.sol';
+import {IGhoStewardV2} from './interfaces/IGhoStewardV2.sol';
+import {IGhoToken} from './interfaces/IGho.sol';
+import {IGsm} from './interfaces/IGSM.sol';
+import {AaveV3Ethereum} from 'aave-address-book/AaveV3Ethereum.sol';
+import {MiscEthereum} from 'aave-address-book/MiscEthereum.sol';
+
+/**
+ * @title GHO Steward Update
+ * @author BGD Labs (@bgdlabs)
+ * - Snapshot: TODO
+ * - Discussion: TODO
+ */
+contract AaveV3Ethereum_GHOStewardUpdate_20240826 is IProposalGenericExecutor {
+  address public constant GHO_STEWARD = 0x1180eE41eC15Dd0accC13a1e646B3152bECFf8F6;
+  address public constant OLD_GHO_STEWARD = 0xb9481a29f0f996BCAc759aB201FB3844c81866c4;
+
+  function execute() external {
+    // Remove Risk admin role to the old steward
+    AaveV3Ethereum.ACL_MANAGER.removeRiskAdmin(OLD_GHO_STEWARD);
+
+    // Revoke old steward's bucket manager role
+    IGhoToken(MiscEthereum.GHO_TOKEN).revokeRole(
+      IGhoToken(MiscEthereum.GHO_TOKEN).BUCKET_MANAGER_ROLE(),
+      OLD_GHO_STEWARD
+    );
+
+    // Revoke old steward's configurator role on usdc, usdt gsm
+    IGsm(MiscEthereum.GSM_USDC).revokeRole(
+      IGsm(MiscEthereum.GSM_USDC).CONFIGURATOR_ROLE(),
+      OLD_GHO_STEWARD
+    );
+    IGsm(MiscEthereum.GSM_USDT).revokeRole(
+      IGsm(MiscEthereum.GSM_USDT).CONFIGURATOR_ROLE(),
+      OLD_GHO_STEWARD
+    );
+
+    // Give Risk admin role to the steward
+    AaveV3Ethereum.ACL_MANAGER.addRiskAdmin(GHO_STEWARD);
+
+    // Give bucket manager role to the steward
+    IGhoToken(MiscEthereum.GHO_TOKEN).grantRole(
+      IGhoToken(MiscEthereum.GHO_TOKEN).BUCKET_MANAGER_ROLE(),
+      GHO_STEWARD
+    );
+
+    // Give configurator role on usdc, usdt gsm to the stewards
+    IGsm(MiscEthereum.GSM_USDC).grantRole(
+      IGsm(MiscEthereum.GSM_USDC).CONFIGURATOR_ROLE(),
+      GHO_STEWARD
+    );
+    IGsm(MiscEthereum.GSM_USDT).grantRole(
+      IGsm(MiscEthereum.GSM_USDT).CONFIGURATOR_ROLE(),
+      GHO_STEWARD
+    );
+
+    // Whitelist all the facilitators on the stewards, including: GhoAToken, GhoFlashMinter, GSM USDC, GSM USDT
+    IGhoStewardV2(GHO_STEWARD).setControlledFacilitator(
+      IGhoToken(MiscEthereum.GHO_TOKEN).getFacilitatorsList(),
+      true
+    );
+  }
+}
---
+++ src/20240826_AaveV3Ethereum_GHOStewardUpdate/GHOStewardUpdate.md
@@ -0,0 +1,22 @@
+---
+title: "GHO Steward Update"
+author: "BGD Labs (@bgdlabs)"
+discussions: ""
+---
+
+## Simple Summary
+
+## Motivation
+
+## Specification
+
+## References
+
+- Implementation: [AaveV3Ethereum](https://github.com/bgd-labs/aave-proposals-v3/blob/main/src/20240826_AaveV3Ethereum_GHOStewardUpdate/AaveV3Ethereum_GHOStewardUpdate_20240826.sol)
+- Tests: [AaveV3Ethereum](https://github.com/bgd-labs/aave-proposals-v3/blob/main/src/20240826_AaveV3Ethereum_GHOStewardUpdate/AaveV3Ethereum_GHOStewardUpdate_20240826.t.sol)
+- [Snapshot](TODO)
+- [Discussion](TODO)
+
+## Copyright
+
+Copyright and related rights waived via [CC0](https://creativecommons.org/publicdomain/zero/1.0/).
