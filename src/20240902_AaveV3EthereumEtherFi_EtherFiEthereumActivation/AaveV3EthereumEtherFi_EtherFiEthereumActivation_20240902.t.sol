95a93b53765ad0ecd6ee5c1e6df1ead108be75ad
kartojal <david@catapulta.sh>
2024-09-02 15:23:03 +0000

    chore: add test to assert etherfi and lido libraries

--- src/20240902_AaveV3EthereumEtherFi_EtherFiEthereumActivation/AaveV3EthereumEtherFi_EtherFiEthereumActivation_20240902.t.sol
+++ src/20240902_AaveV3EthereumEtherFi_EtherFiEthereumActivation/AaveV3EthereumEtherFi_EtherFiEthereumActivation_20240902.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import {GovV3Helpers} from 'aave-helpers/src/GovV3Helpers.sol';
 import {AaveV3EthereumEtherFi} from 'aave-address-book/AaveV3EthereumEtherFi.sol';
 import {AaveV3Ethereum} from 'aave-address-book/AaveV3Ethereum.sol';
+import {AaveV3EthereumLido} from 'aave-address-book/AaveV3EthereumLido.sol';
 import {IERC20} from 'solidity-utils/contracts/oz-common/interfaces/IERC20.sol';
 
 import 'forge-std/Test.sol';
@@ -11,7 +12,7 @@ import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/src/ProtocolV3Test
 import {AaveV3EthereumEtherFi_EtherFiEthereumActivation_20240902} from './AaveV3EthereumEtherFi_EtherFiEthereumActivation_20240902.sol';
 import {IPoolAddressesProviderRegistry} from 'aave-v3-core/contracts/interfaces/IPoolAddressesProviderRegistry.sol';
 import {GovernanceV3Ethereum} from 'aave-address-book/GovernanceV3Ethereum.sol';
-
+import {IAaveV3ConfigEngine} from 'aave-v3-periphery/contracts/v3-config-engine/IAaveV3ConfigEngine.sol';
 /**
  * @dev Test for AaveV3EthereumEtherFi_EtherFiEthereumActivation_20240902
  * command: FOUNDRY_PROFILE=mainnet forge test --match-path=src/20240902_AaveV3EthereumEtherFi_EtherFiEthereumActivation/AaveV3EthereumEtherFi_EtherFiEthereumActivation_20240902.t.sol -vv
@@ -94,6 +95,53 @@ contract AaveV3EthereumEtherFi_EtherFiEthereumActivation_20240902_Test is Protoc
     );
     assertTrue(
       AaveV3EthereumEtherFi.ACL_MANAGER.isRiskAdmin(AaveV3EthereumEtherFi.CAPS_PLUS_RISK_STEWARD)
+    );
+  }
+
+  function test_check_etherfi_v3_libraries_against_lido_v3_instance() public {
+    // Check Pool libraries
+    assertEq(
+      AaveV3EthereumEtherFi.POOL.getFlashLoanLogic(),
+      AaveV3EthereumLido.POOL.getFlashLoanLogic()
+    );
+    assertEq(AaveV3EthereumEtherFi.POOL.getBorrowLogic(), AaveV3EthereumLido.POOL.getBorrowLogic());
+    assertEq(AaveV3EthereumEtherFi.POOL.getBridgeLogic(), AaveV3EthereumLido.POOL.getBridgeLogic());
+    assertEq(AaveV3EthereumEtherFi.POOL.getEModeLogic(), AaveV3EthereumLido.POOL.getEModeLogic());
+    assertEq(
+      AaveV3EthereumEtherFi.POOL.getLiquidationLogic(),
+      AaveV3EthereumLido.POOL.getLiquidationLogic()
+    );
+    assertEq(AaveV3EthereumEtherFi.POOL.getPoolLogic(), AaveV3EthereumLido.POOL.getPoolLogic());
+    assertEq(AaveV3EthereumEtherFi.POOL.getSupplyLogic(), AaveV3EthereumLido.POOL.getSupplyLogic());
+
+    // Check AaveV3Engine libraries
+    assertEq(
+      IAaveV3ConfigEngine(AaveV3EthereumEtherFi.CONFIG_ENGINE).BORROW_ENGINE(),
+      IAaveV3ConfigEngine(AaveV3EthereumLido.CONFIG_ENGINE).BORROW_ENGINE()
+    );
+    assertEq(
+      IAaveV3ConfigEngine(AaveV3EthereumEtherFi.CONFIG_ENGINE).CAPS_ENGINE(),
+      IAaveV3ConfigEngine(AaveV3EthereumLido.CONFIG_ENGINE).CAPS_ENGINE()
+    );
+    assertEq(
+      IAaveV3ConfigEngine(AaveV3EthereumEtherFi.CONFIG_ENGINE).COLLATERAL_ENGINE(),
+      IAaveV3ConfigEngine(AaveV3EthereumLido.CONFIG_ENGINE).COLLATERAL_ENGINE()
+    );
+    assertEq(
+      IAaveV3ConfigEngine(AaveV3EthereumEtherFi.CONFIG_ENGINE).EMODE_ENGINE(),
+      IAaveV3ConfigEngine(AaveV3EthereumLido.CONFIG_ENGINE).EMODE_ENGINE()
+    );
+    assertEq(
+      IAaveV3ConfigEngine(AaveV3EthereumEtherFi.CONFIG_ENGINE).LISTING_ENGINE(),
+      IAaveV3ConfigEngine(AaveV3EthereumLido.CONFIG_ENGINE).LISTING_ENGINE()
+    );
+    assertEq(
+      IAaveV3ConfigEngine(AaveV3EthereumEtherFi.CONFIG_ENGINE).PRICE_FEED_ENGINE(),
+      IAaveV3ConfigEngine(AaveV3EthereumLido.CONFIG_ENGINE).PRICE_FEED_ENGINE()
+    );
+    assertEq(
+      IAaveV3ConfigEngine(AaveV3EthereumEtherFi.CONFIG_ENGINE).RATE_ENGINE(),
+      IAaveV3ConfigEngine(AaveV3EthereumLido.CONFIG_ENGINE).RATE_ENGINE()
     );
   }
 }
