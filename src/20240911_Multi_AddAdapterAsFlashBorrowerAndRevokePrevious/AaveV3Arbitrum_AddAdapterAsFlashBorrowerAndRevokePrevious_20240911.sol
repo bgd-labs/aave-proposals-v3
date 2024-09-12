594e05584bad2e2b3e6b377d350f2c5b2a823b7d
YBM <yan@avara.xyz>
2024-09-11 19:01:40 -0500

    feat: rescueTokens on all assets in arb

--- src/20240911_Multi_AddAdapterAsFlashBorrowerAndRevokePrevious/AaveV3Arbitrum_AddAdapterAsFlashBorrowerAndRevokePrevious_20240911.sol
+++ src/20240911_Multi_AddAdapterAsFlashBorrowerAndRevokePrevious/AaveV3Arbitrum_AddAdapterAsFlashBorrowerAndRevokePrevious_20240911.sol
@@ -19,12 +19,257 @@ contract AaveV3Arbitrum_AddAdapterAsFlashBorrowerAndRevokePrevious_20240911 is
   address public constant NEW_FLASH_BORROWER = 0x0000000000000000000000000000000000000001;
 
   function execute() external {
-    AaveV3Arbitrum.ACL_MANAGER.addFlashBorrower(NEW_FLASH_BORROWER);
-
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.DAI_UNDERLYING)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.DAI_A_TOKEN)
+    );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.DAI_V_TOKEN)
+    // );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.DAI_S_TOKEN)
+    // );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.DAI_STATA_TOKEN)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.LINK_UNDERLYING)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.LINK_A_TOKEN)
+    );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.LINK_V_TOKEN)
+    // );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.LINK_S_TOKEN)
+    // );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.LINK_STATA_TOKEN)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.USDC_UNDERLYING)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.USDC_A_TOKEN)
+    );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.USDC_V_TOKEN)
+    // );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.USDC_S_TOKEN)
+    // );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.USDC_STATA_TOKEN)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.WBTC_UNDERLYING)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.WBTC_A_TOKEN)
+    );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.WBTC_V_TOKEN)
+    // );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.WBTC_S_TOKEN)
+    // );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.WBTC_STATA_TOKEN)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.WETH_UNDERLYING)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.WETH_A_TOKEN)
+    );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.WETH_V_TOKEN)
+    // );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.WETH_S_TOKEN)
+    // );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.WETH_STATA_TOKEN)
+    );
     IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
       IERC20(AaveV3ArbitrumAssets.USDT_UNDERLYING)
     );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.USDT_A_TOKEN)
+    );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.USDT_V_TOKEN)
+    // );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.USDT_S_TOKEN)
+    // );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.USDT_STATA_TOKEN)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.AAVE_UNDERLYING)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.AAVE_A_TOKEN)
+    );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.AAVE_V_TOKEN)
+    // );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.AAVE_S_TOKEN)
+    // );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.AAVE_STATA_TOKEN)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.EURS_UNDERLYING)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.EURS_A_TOKEN)
+    );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.EURS_V_TOKEN)
+    // );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.EURS_S_TOKEN)
+    // );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.EURS_STATA_TOKEN)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.wstETH_UNDERLYING)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.wstETH_A_TOKEN)
+    );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.wstETH_V_TOKEN)
+    // );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.wstETH_S_TOKEN)
+    // );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.wstETH_STATA_TOKEN)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.MAI_UNDERLYING)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.MAI_A_TOKEN)
+    );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.MAI_V_TOKEN)
+    // );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.MAI_S_TOKEN)
+    // );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.MAI_STATA_TOKEN)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.rETH_UNDERLYING)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.rETH_A_TOKEN)
+    );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.rETH_V_TOKEN)
+    // );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.rETH_S_TOKEN)
+    // );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.rETH_STATA_TOKEN)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.LUSD_UNDERLYING)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.LUSD_A_TOKEN)
+    );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.LUSD_V_TOKEN)
+    // );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.LUSD_S_TOKEN)
+    // );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.LUSD_STATA_TOKEN)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.USDCn_UNDERLYING)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.USDCn_A_TOKEN)
+    );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.USDCn_V_TOKEN)
+    // );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.USDCn_S_TOKEN)
+    // );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.USDCn_STATA_TOKEN)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.FRAX_UNDERLYING)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.FRAX_A_TOKEN)
+    );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.FRAX_V_TOKEN)
+    // );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.FRAX_S_TOKEN)
+    // );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.FRAX_STATA_TOKEN)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.ARB_UNDERLYING)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.ARB_A_TOKEN)
+    );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.ARB_V_TOKEN)
+    // );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.ARB_S_TOKEN)
+    // );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.ARB_STATA_TOKEN)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.weETH_UNDERLYING)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.weETH_A_TOKEN)
+    );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.weETH_V_TOKEN)
+    // );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.weETH_S_TOKEN)
+    // );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.GHO_UNDERLYING)
+    );
+    IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+      IERC20(AaveV3ArbitrumAssets.GHO_A_TOKEN)
+    );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.GHO_V_TOKEN)
+    // );
+    // IBaseParaSwapAdapter(AaveV3Arbitrum.DEBT_SWAP_ADAPTER).rescueTokens(
+    //   IERC20(AaveV3ArbitrumAssets.GHO_S_TOKEN)
+    // );
 
+    AaveV3Arbitrum.ACL_MANAGER.addFlashBorrower(NEW_FLASH_BORROWER);
     AaveV3Arbitrum.ACL_MANAGER.removeFlashBorrower(AaveV3Arbitrum.DEBT_SWAP_ADAPTER);
   }
 }
