5e3ad993df6250d6ad58c6593f1b95a24194c4b2
YBM <yan@avara.xyz>
2024-09-17 20:15:14 -0500

    fix: govV2 script

--- src/20240916_Multi_RescueTokensFromAdapters/RescueTokensFromAdaptersGovV2_20240916.s.sol
+++ src/20240916_Multi_RescueTokensFromAdapters/RescueTokensFromAdaptersGovV2_20240916.s.sol
@@ -6,13 +6,11 @@ import {GovHelpers} from 'aave-helpers/src/GovHelpers.sol';
 import {EthereumScript, ArbitrumScript, PolygonScript, AvalancheScript} from 'solidity-utils/contracts/utils/ScriptUtils.sol';
 import {AaveV3Ethereum_RescueTokensFromAdapters_20240916_GovV2} from './AaveV3Ethereum_RescueTokensFromAdapters_20240916_GovV2.sol';
 import {AaveV3Arbitrum_RescueTokensFromAdapters_20240916_GovV2} from './AaveV3Arbitrum_RescueTokensFromAdapters_20240916_GovV2.sol';
-// import {AaveV3_Polygon_AddDebtSwapAdapterAsFlashBorrower_20230809} from './AaveV3_Polygon_AddDebtSwapAdapterAsFlashBorrower_20230809.sol';
-// import {AaveV3_Avalanche_AddDebtSwapAdapterAsFlashBorrower_20230809} from './AaveV3_Avalanche_AddDebtSwapAdapterAsFlashBorrower_20230809.sol';
-// import {AaveV3_Base_AddDebtSwapAdapterAsFlashBorrower_20230809} from './AaveV3_Base_AddDebtSwapAdapterAsFlashBorrower_20230809.sol';
+import {AaveV3Polygon_RescueTokensFromAdapters_20240916_GovV2} from './AaveV3Polygon_RescueTokensFromAdapters_20240916_GovV2.sol';
 
 /**
  * @dev Deploy AaveV3Ethereum_RescueTokensFromAdapters_20240916_GovV2
- * command: make deploy-ledger contract=src/20240916_Multi_RescueTokensFromAdapters/AaveV3_AddDebtSwapAdapterAsFlashBorrower_20230809.s.sol:DeployEthereum chain=mainnet
+ * command: make deploy-ledger contract=src/20240916_Multi_RescueTokensFromAdapters/RescueTokensFromAdaptersGovV2_20240916.s.sol:DeployEthereum chain=mainnet
  */
 contract DeployEthereum is EthereumScript {
   function run() external broadcast {
@@ -22,11 +20,21 @@ contract DeployEthereum is EthereumScript {
 
 /**
  * @dev Deploy AaveV3Arbitrum_RescueTokensFromAdapters_20240916_GovV2
- * command: make deploy-ledger contract=src/20240916_Multi_RescueTokensFromAdapters/AaveV3Arbitrum_RescueTokensFromAdapters_20240916_GovV2.s.sol:DeployArbitrum chain=arbitrum
+ * command: make deploy-ledger contract=src/20240916_Multi_RescueTokensFromAdapters/RescueTokensFromAdaptersGovV2_20240916.s.sol:DeployArbitrum chain=arbitrum
  */
 contract DeployArbitrum is ArbitrumScript {
   function run() external broadcast {
     new AaveV3Arbitrum_RescueTokensFromAdapters_20240916_GovV2();
+  }
+}
+
+/**
+ * @dev Deploy AaveV3Polygon_RescueTokensFromAdapters_20240916_GovV2
+ * command: make deploy-ledger contract=src/20240916_Multi_RescueTokensFromAdapters/RescueTokensFromAdaptersGovV2_20240916.s.sol:DeployPolygon chain=polygon
+ */
+contract DeployPolygon is PolygonScript {
+  function run() external broadcast {
+    new AaveV3Polygon_RescueTokensFromAdapters_20240916_GovV2();
   }
 }
 
@@ -39,6 +47,7 @@ contract CreateProposal is EthereumScript {
     GovHelpers.Payload[] memory payloads = new GovHelpers.Payload[](2);
     payloads[0] = GovHelpers.buildMainnet(0xbCb167bDCF14a8F791d6f4A6EDd964aed2F8813B); //TODO: fill in proposal address
     payloads[1] = GovHelpers.buildArbitrum(0xbCb167bDCF14a8F791d6f4A6EDd964aed2F8813B); //TODO: fill in proposal address
+    payloads[2] = GovHelpers.buildPolygon(0xbCb167bDCF14a8F791d6f4A6EDd964aed2F8813B); //TODO: fill in proposal address
     // GovV3Helpers.createProposal(
     //   vm,
     //   payloads,
