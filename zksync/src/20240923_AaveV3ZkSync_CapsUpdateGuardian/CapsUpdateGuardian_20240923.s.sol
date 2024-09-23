7233433391a55b8ec4972051f598e3d561a668de
Harsh Pandey <harshsatishpandey@gmail.com>
2024-09-23 12:20:58 +0530

    chore: deploy script

--- zksync/src/20240923_AaveV3ZkSync_CapsUpdateGuardian/CapsUpdateGuardian_20240923.s.sol
+++ zksync/src/20240923_AaveV3ZkSync_CapsUpdateGuardian/CapsUpdateGuardian_20240923.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 import {GovV3Helpers, IPayloadsControllerCore} from 'aave-helpers/src/GovV3Helpers.sol';
-import {ZkSyncScript} from 'aave-helpers/src/ScriptUtils.sol';
+import {ZkSyncScript} from 'solidity-utils/contracts/utils/ScriptUtils.sol';
 import {AaveV3ZkSync_CapsUpdateGuardian_20240923} from './AaveV3ZkSync_CapsUpdateGuardian_20240923.sol';
 
 // @dev wrapper factory contract for deploying the payload
@@ -18,7 +18,7 @@ contract Deploy_AaveV3ZkSync_CapsUpdateGuardian_20240923 {
 
 /**
  * @dev Deploy ZkSync
- * deploy-command: make deploy-ledger contract=zksync/src/20240923_AaveV3ZkSync_CapsUpdateGuardian/CapsUpdateGuardian_20240923.s.sol:DeployZkSync chain=zksync
+ * deploy-command: make deploy-pk contract=zksync/src/20240923_AaveV3ZkSync_CapsUpdateGuardian/CapsUpdateGuardian_20240923.s.sol:DeployZkSync chain=zksync
  * verify-command: FOUNDRY_PROFILE=zksync npx catapulta-verify -b broadcast/CapsUpdateGuardian_20240923.s.sol/324/run-latest.json
  */
 contract DeployZkSync is ZkSyncScript {
@@ -30,8 +30,5 @@ contract DeployZkSync is ZkSyncScript {
     IPayloadsControllerCore.ExecutionAction[]
       memory actions = new IPayloadsControllerCore.ExecutionAction[](1);
     actions[0] = GovV3Helpers.buildAction(payload0);
-
-    // register action at payloadsController
-    GovV3Helpers.createPayload(actions);
   }
 }
