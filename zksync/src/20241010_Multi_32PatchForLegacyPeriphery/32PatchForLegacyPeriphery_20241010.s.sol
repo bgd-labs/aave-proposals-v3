e0c0411f9ed7e7a80d80151e8cb99a1215968b34
Lukas <lukasstrassel@googlemail.com>
2024-10-10 13:27:14 +0200

    fix: patch zksync generator

--- zksync/src/20241010_Multi_32PatchForLegacyPeriphery/32PatchForLegacyPeriphery_20241010.s.sol
+++ zksync/src/20241010_Multi_32PatchForLegacyPeriphery/32PatchForLegacyPeriphery_20241010.s.sol
@@ -2,12 +2,12 @@
 pragma solidity ^0.8.0;
 
 import {GovV3Helpers, IPayloadsControllerCore} from 'aave-helpers/src/GovV3Helpers.sol';
-import {ZkSyncScript} from 'aave-helpers/src/ScriptUtils.sol';
+import {ZkSyncScript} from 'solidity-utils/contracts/utils/ScriptUtils.sol';
 import {Payloads} from '../../../src/20241010_Multi_32PatchForLegacyPeriphery/Payloads.sol';
 
 /**
  * @dev Deploy ZkSync
- * deploy-command: make deploy-ledger contract=zksync/src/20241010_Multi_32PatchForLegacyPeriphery/32PatchForLegacyPeriphery_20241010.s.sol:DeployZkSync chain=zksync
+ * deploy-command: make deploy-pk contract=zksync/src/20241010_Multi_32PatchForLegacyPeriphery/32PatchForLegacyPeriphery_20241010.s.sol:DeployZkSync chain=zksync
  * verify-command: FOUNDRY_PROFILE=zksync npx catapulta-verify -b broadcast/32PatchForLegacyPeriphery_20241010.s.sol/324/run-latest.json
  */
 contract DeployZkSync is ZkSyncScript {
