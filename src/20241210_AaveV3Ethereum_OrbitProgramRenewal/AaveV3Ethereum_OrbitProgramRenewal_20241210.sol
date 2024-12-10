d7bfee9fd4eb147f20553f0d4972a4d695586864
MartinGbz <martin.grabarz@outlook.fr>
2024-12-10 11:17:53 +0100

    ✨ setup aip

---
+++ src/20241210_AaveV3Ethereum_OrbitProgramRenewal/AaveV3Ethereum_OrbitProgramRenewal_20241210.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IProposalGenericExecutor} from 'aave-helpers/src/interfaces/IProposalGenericExecutor.sol';
+/**
+ * @title Orbit Program Renewal
+ * @author ACI
+ * - Snapshot: https://snapshot.box/#/s:aave.eth/proposal/0x60613deb2c662057cc8028b431df84fe6e763d38f48f70594a7cb7fd91a8cb93
+ * - Discussion: https://governance.aave.com/t/arfc-orbit-program-renewal-q4-2024/20084
+ */
+contract AaveV3Ethereum_OrbitProgramRenewal_20241210 is IProposalGenericExecutor {
+  function execute() external {
+    // custom code goes here
+  }
+}
