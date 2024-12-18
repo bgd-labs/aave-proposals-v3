bcdd6c201482eb148e9364f73000b7f7575055c4
Cheyenne Atapour <cheyenneatapour@gmail.com>
2024-12-18 17:24:31 +0900

    fix: Avax proposal tests

--- src/20241106_Multi_GHOAvaxLaunch/AaveV3Avalanche_GHOAvaxLaunch_20241106.t.sol
+++ src/20241106_Multi_GHOAvaxLaunch/AaveV3Avalanche_GHOAvaxLaunch_20241106.t.sol
@@ -44,7 +44,7 @@ contract AaveV3Avalanche_GHOAvaxLaunch_20241106_Test is ProtocolV3TestBase {
   address public constant CCIP_ROUTER = 0xF4c7E640EdA248ef95972845a62bdC74237805dB;
   address public constant ETH_TOKEN_POOL = MiscEthereum.GHO_CCIP_TOKEN_POOL;
   address public constant ARB_TOKEN_POOL = MiscArbitrum.GHO_CCIP_TOKEN_POOL;
-  address public constant GHO_TOKEN = 0xb025950B02b9cfe851C6a4C041f9D6c0942f0eB1;
+  address public constant GHO_TOKEN = 0x2e234DAe75C793f67A35089C9d99245E1C58470b;
   UpgradeableGhoToken public GHO = UpgradeableGhoToken(GHO_TOKEN);
   UpgradeableBurnMintTokenPool public TOKEN_POOL;
 
@@ -54,6 +54,9 @@ contract AaveV3Avalanche_GHOAvaxLaunch_20241106_Test is ProtocolV3TestBase {
 
   function setUp() public {
     vm.createSelectFork(vm.rpcUrl('avalanche'), 53559217);
+
+    // TODO: Decide if we want to deploy this beforehand or in AIP
+    _deployGhoToken();
 
     // TODO: Remove this deployment once we have deployed pool address
     address tokenPool = _deployCcipTokenPool(GHO_TOKEN);
