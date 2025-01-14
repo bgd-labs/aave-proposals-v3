f2940b91dd47d700d1d8ec8632c262b1409e71a9
Alice <121383428+Rozengarden@users.noreply.github.com>
2025-01-14 14:33:36 +0100

    [Aavechan]  Onboard sUSDe and weETH on zksync (#565)
    * onboard USDe, sUSDe, weETH AIP
    
    * add aip description
    
    * make weeth borrowable in emode (to macth mainnet)
    
    * 🐛 remove USDe from asset implemented
    
    * 🐛 remove USDe + weETH as borrowable asset + weETH from ETH correlated emode + add new weETH correlated emode
    
    * ✨ add weETH + ETH to new "weETH correlated" emode
    
    * ✅ ran tests
    
    * Update src/20250110_AaveV3ZkSync_OnboardSUSDeUSDeAndWeETHToAaveV3OnZkSync/OnboardSUSDeUSDeAndWeETHToAaveV3OnZkSync.md
    
    Co-authored-by: Harsh Pandey <harshsatishpandey@gmail.com>
    
    * Update src/20250110_AaveV3ZkSync_OnboardSUSDeUSDeAndWeETHToAaveV3OnZkSync/OnboardSUSDeUSDeAndWeETHToAaveV3OnZkSync.md
    
    Co-authored-by: Harsh Pandey <harshsatishpandey@gmail.com>
    
    * Update src/20250110_AaveV3ZkSync_OnboardSUSDeUSDeAndWeETHToAaveV3OnZkSync/OnboardSUSDeUSDeAndWeETHToAaveV3OnZkSync_20250110.s.sol
    
    Co-authored-by: Harsh Pandey <harshsatishpandey@gmail.com>
    
    ---------
    
    Co-authored-by: MartinGbz <martin.grabarz@outlook.fr>
    Co-authored-by: Marc Zeller <21088542+marczeller@users.noreply.github.com>
    Co-authored-by: Harsh Pandey <harshsatishpandey@gmail.com>

---
+++ src/20250110_AaveV3ZkSync_OnboardSUSDeUSDeAndWeETHToAaveV3OnZkSync/OnboardSUSDeUSDeAndWeETHToAaveV3OnZkSync_20250110.s.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {GovV3Helpers, IPayloadsControllerCore, PayloadsControllerUtils} from 'aave-helpers/src/GovV3Helpers.sol';
+import {AaveV3OnZkSync} from 'aave-address-book/AaveV3OnZkSync.sol';
+import {GovernanceV3Ethereum} from 'aave-address-book/GovernanceV3Ethereum.sol';
+import {EthereumScript} from 'solidity-utils/contracts/utils/ScriptUtils.sol';
+
+/**
+ * @dev Create Proposal
+ * command: make deploy-ledger contract=src/20250110_AaveV3ZkSync_OnboardSUSDeUSDeAndWeETHToAaveV3OnZkSync/OnboardSUSDeUSDeAndWeETHToAaveV3OnZkSync_20250110.s.sol:CreateProposal chain=mainnet
+ */
+contract CreateProposal is EthereumScript {
+  function run() external {
+    // create payloads
+    PayloadsControllerUtils.Payload[] memory payloads = new PayloadsControllerUtils.Payload[](1);
+
+    // compose actions for validation
+    IPayloadsControllerCore.ExecutionAction[]
+      memory actionsZkSync = new IPayloadsControllerCore.ExecutionAction[](1);
+    actionsZkSync[0] = GovV3Helpers.buildActionZkSync(
+      vm,
+      'AaveV3ZkSync_OnboardSUSDeUSDeAndWeETHToAaveV3OnZkSync_20250110'
+    );
+    payloads[0] = PayloadsControllerUtils.Payload({
+      chain: ChainIds.ZKSYNC,
+      accessLevel: PayloadsControllerUtils.AccessControl.Level_1,
+      payloadsController: address(GovernanceV3ZkSync.PAYLOADS_CONTROLLER),
+      payloadId: 10
+    });
+
+    // create proposal
+    vm.startBroadcast();
+    GovV3Helpers.createProposal(
+      vm,
+      payloads,
+      GovernanceV3Ethereum.VOTING_PORTAL_ETH_POL,
+      GovV3Helpers.ipfsHashFile(
+        vm,
+        'src/20250110_AaveV3ZkSync_OnboardSUSDeUSDeAndWeETHToAaveV3OnZkSync/OnboardSUSDeUSDeAndWeETHToAaveV3OnZkSync.md'
+      )
+    );
+  }
+}
