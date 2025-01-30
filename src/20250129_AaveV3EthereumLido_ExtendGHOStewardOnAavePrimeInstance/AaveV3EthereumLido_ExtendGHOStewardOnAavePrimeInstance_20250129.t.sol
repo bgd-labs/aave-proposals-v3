8f356c5119afc6c2e4ec3384777e0950d053c1b8
LucasWongC <wm111898@gmail.com>
2025-01-30 13:48:42 -0500

    used constant for council address

--- src/20250129_AaveV3EthereumLido_ExtendGHOStewardOnAavePrimeInstance/AaveV3EthereumLido_ExtendGHOStewardOnAavePrimeInstance_20250129.t.sol
+++ src/20250129_AaveV3EthereumLido_ExtendGHOStewardOnAavePrimeInstance/AaveV3EthereumLido_ExtendGHOStewardOnAavePrimeInstance_20250129.t.sol
@@ -23,6 +23,10 @@ contract AaveV3EthereumLido_ExtendGHOStewardOnAavePrimeInstance_20250129_Test is
   IGhoAaveSteward public constant NEW_GHO_AAVE_STEWARD =
     IGhoAaveSteward(0x5C905d62B22e4DAa4967E517C4a047Ff6026C731);
 
+  // The address of council
+  // https://etherscan.io/address/0x8513e6F37dBc52De87b166980Fa3F50639694B60
+  address public constant COUNCIL = 0x8513e6F37dBc52De87b166980Fa3F50639694B60;
+
   function setUp() public {
     vm.createSelectFork(vm.rpcUrl('mainnet'), 21738505);
     proposal = new AaveV3EthereumLido_ExtendGHOStewardOnAavePrimeInstance_20250129();
@@ -48,9 +52,7 @@ contract AaveV3EthereumLido_ExtendGHOStewardOnAavePrimeInstance_20250129_Test is
       address(AaveV3EthereumLido.AAVE_PROTOCOL_DATA_PROVIDER)
     );
     assertEq(NEW_GHO_AAVE_STEWARD.GHO_TOKEN(), AaveV3EthereumLidoAssets.GHO_UNDERLYING);
-    // The address of council
-    // https://etherscan.io/address/0x8513e6F37dBc52De87b166980Fa3F50639694B60
-    assertEq(NEW_GHO_AAVE_STEWARD.RISK_COUNCIL(), 0x8513e6F37dBc52De87b166980Fa3F50639694B60);
+    assertEq(NEW_GHO_AAVE_STEWARD.RISK_COUNCIL(), COUNCIL);
     assertEq(
       NEW_GHO_AAVE_STEWARD.getBorrowRateConfig(),
       IGhoAaveSteward.BorrowRateConfig({
