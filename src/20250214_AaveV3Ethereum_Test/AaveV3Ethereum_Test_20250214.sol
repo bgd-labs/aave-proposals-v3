d5bf8ea335af895141844aff52e70898398ae6ba
Rozengarden <github@rozengarden.eu>
2025-02-14 15:33:58 +0100

    minimum working bug
    DO NOT MERGE

---
+++ src/20250214_AaveV3Ethereum_Test/AaveV3Ethereum_Test_20250214.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
+import {AaveV3PayloadEthereum} from 'aave-helpers/src/v3-config-engine/AaveV3PayloadEthereum.sol';
+import {EngineFlags} from 'aave-v3-origin/contracts/extensions/v3-config-engine/EngineFlags.sol';
+import {IAaveV3ConfigEngine} from 'aave-v3-origin/contracts/extensions/v3-config-engine/IAaveV3ConfigEngine.sol';
+/**
+ * @title test
+ * @author ltv0test
+ * - Snapshot: TODO
+ * - Discussion: TODO
+ */
+contract AaveV3Ethereum_Test_20250214 is AaveV3PayloadEthereum {
+  function collateralsUpdates()
+    public
+    pure
+    override
+    returns (IAaveV3ConfigEngine.CollateralUpdate[] memory)
+  {
+    IAaveV3ConfigEngine.CollateralUpdate[]
+      memory collateralUpdate = new IAaveV3ConfigEngine.CollateralUpdate[](1);
+
+    collateralUpdate[0] = IAaveV3ConfigEngine.CollateralUpdate({
+      asset: AaveV3EthereumAssets.WETH_UNDERLYING,
+      ltv: 0,
+      liqThreshold: EngineFlags.KEEP_CURRENT,
+      liqBonus: EngineFlags.KEEP_CURRENT,
+      debtCeiling: EngineFlags.KEEP_CURRENT,
+      liqProtocolFee: EngineFlags.KEEP_CURRENT
+    });
+
+    return collateralUpdate;
+  }
+}
---
+++ src/20250214_AaveV3Ethereum_Test/Test.md
@@ -0,0 +1,23 @@
+---
+title: "test"
+author: "ltv0test"
+discussions: TODO
+snapshot: TODO
+---
+
+## Simple Summary
+
+## Motivation
+
+## Specification
+
+## References
+
+- Implementation: [AaveV3Ethereum](https://github.com/bgd-labs/aave-proposals-v3/blob/main/src/20250214_AaveV3Ethereum_Test/AaveV3Ethereum_Test_20250214.sol)
+- Tests: [AaveV3Ethereum](https://github.com/bgd-labs/aave-proposals-v3/blob/main/src/20250214_AaveV3Ethereum_Test/AaveV3Ethereum_Test_20250214.t.sol)
+  [Snapshot](TODO)
+- [Discussion](TODO)
+
+## Copyright
+
+Copyright and related rights waived via [CC0](https://creativecommons.org/publicdomain/zero/1.0/).
