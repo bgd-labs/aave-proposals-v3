9645b060891fa0727e4c5ca627ddacd303f88cf6
Lukas <ls@bgdlabs.com>
2025-02-18 12:47:12 +0100

    Update src/20250205_Multi_UpgradeAaveInstancesToV33/UpgradeAaveInstancesToV33_20250205.s.sol
    Co-authored-by: Andrey <kyzia.ru@gmail.com>

--- src/20250205_Multi_UpgradeAaveInstancesToV33/UpgradeAaveInstancesToV33_20250205.s.sol
+++ src/20250205_Multi_UpgradeAaveInstancesToV33/UpgradeAaveInstancesToV33_20250205.s.sol
@@ -17,7 +17,6 @@ library Payloads {
   address internal constant SCROLL = 0xb77fc84a549ecc0b410d6fa15159C2df207545a3;
   address internal constant BNB = 0x5E76E98E0963EcDC6A065d1435F84065b7523f39;
   address internal constant PROTO = 0x79691D2DFE962E4059F3b56B53D7Cc081F494aE0;
-  address internal constant ZKSYNC = 0xcE69DAb549C757432B7e4b62ec496fdc50a66218;
   address internal constant METIS = 0xb85d72EC1EfE48168c4aBC4eB855f8Cbcd05cE38;
   address internal constant LIDO = 0x5Deec870CAA11742BAE0f0B650EC8A549D1814Fa;
   address internal constant ETHERFI = 0xceDd3bC91c502aA5d74eF4065fbBE5D0b5ECb4C3;
