9300a9a4dc282242576edeba9a0e872b14335c36
Alice <121383428+Rozengarden@users.noreply.github.com>
2025-02-20 13:24:39 +0100

    feat: adjust rf and LTV on polygon (#613)
    * feat: adjust rf and LTV on polygon
    
    * feat: set ltv to  0 instead of freeze
    
    * ⬆️ bump aave-helpers
    
    * forge install: aave-helpers
    
    v3.0.0
    
    * ➖ remove aave-helpers
    
    * forge install: aave-helpers
    
    v3.0.0
    
    * ⬆️ bump aave-helpers to last commit
    
    * Update src/20250210_Multi_AdjustRiskParametersForAaveV2AndV3OnPolygon/AdjustRiskParametersForAaveV2AndV3OnPolygon.md
    
    Co-authored-by: Harsh Pandey <harshsatishpandey@gmail.com>
    
    * Update src/20250210_Multi_AdjustRiskParametersForAaveV2AndV3OnPolygon/AdjustRiskParametersForAaveV2AndV3OnPolygon.md
    
    Co-authored-by: Harsh Pandey <harshsatishpandey@gmail.com>
    
    * Update src/20250210_Multi_AdjustRiskParametersForAaveV2AndV3OnPolygon/AdjustRiskParametersForAaveV2AndV3OnPolygon.md
    
    Co-authored-by: Harsh Pandey <harshsatishpandey@gmail.com>
    
    * Update AdjustRiskParametersForAaveV2AndV3OnPolygon.md
    
    ---------
    
    Co-authored-by: MartinGbz <martin.grabarz@outlook.fr>
    Co-authored-by: Harsh Pandey <harshsatishpandey@gmail.com>

---
+++ src/20250210_Multi_AdjustRiskParametersForAaveV2AndV3OnPolygon/AaveV3Polygon_AdjustRiskParametersForAaveV2AndV3OnPolygon_20250210.t.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {AaveV3Polygon} from 'aave-address-book/AaveV3Polygon.sol';
+
+import 'forge-std/Test.sol';
+import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/src/ProtocolV3TestBase.sol';
+import {AaveV3Polygon_AdjustRiskParametersForAaveV2AndV3OnPolygon_20250210} from './AaveV3Polygon_AdjustRiskParametersForAaveV2AndV3OnPolygon_20250210.sol';
+
+/**
+ * @dev Test for AaveV3Polygon_AdjustRiskParametersForAaveV2AndV3OnPolygon_20250210
+ * command: FOUNDRY_PROFILE=polygon forge test --match-path=src/20250210_Multi_AdjustRiskParametersForAaveV2AndV3OnPolygon/AaveV3Polygon_AdjustRiskParametersForAaveV2AndV3OnPolygon_20250210.t.sol -vv
+ */
+contract AaveV3Polygon_AdjustRiskParametersForAaveV2AndV3OnPolygon_20250210_Test is
+  ProtocolV3TestBase
+{
+  AaveV3Polygon_AdjustRiskParametersForAaveV2AndV3OnPolygon_20250210 internal proposal;
+
+  function setUp() public {
+    vm.createSelectFork(vm.rpcUrl('polygon'), 68044431);
+    proposal = new AaveV3Polygon_AdjustRiskParametersForAaveV2AndV3OnPolygon_20250210();
+  }
+
+  /**
+   * @dev executes the generic test suite including e2e and config snapshots
+   */
+  function test_defaultProposalExecution() public {
+    defaultTest(
+      'AaveV3Polygon_AdjustRiskParametersForAaveV2AndV3OnPolygon_20250210',
+      AaveV3Polygon.POOL,
+      address(proposal)
+    );
+  }
+}
