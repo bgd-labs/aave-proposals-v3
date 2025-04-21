602492360e7e53fd8dd13162d46f9c9fb6ed499b
luigy-lemon <luigy-lemon@protonmail.com>
2025-04-21 11:48:07 +0100

    feat: gho listing on Gnosis - missing required smart contracts and test adjustments

---
+++ src/20250421_Multi_GHOGnosisLaunch/AaveV3Ethereum_GHOGnosisLaunch_20250421.t.sol
@@ -0,0 +1,462 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import 'forge-std/Test.sol';
+
+import {IUpgradeableLockReleaseTokenPool_1_5_1} from 'src/interfaces/ccip/tokenPool/IUpgradeableLockReleaseTokenPool.sol';
+import {IPool as IPool_CCIP} from 'src/interfaces/ccip/tokenPool/IPool.sol';
+import {IClient} from 'src/interfaces/ccip/IClient.sol';
+import {IInternal} from 'src/interfaces/ccip/IInternal.sol';
+import {IRouter} from 'src/interfaces/ccip/IRouter.sol';
+import {IRateLimiter} from 'src/interfaces/ccip/IRateLimiter.sol';
+import {IEVM2EVMOnRamp} from 'src/interfaces/ccip/IEVM2EVMOnRamp.sol';
+import {IEVM2EVMOffRamp_1_5} from 'src/interfaces/ccip/IEVM2EVMOffRamp.sol';
+import {ITokenAdminRegistry} from 'src/interfaces/ccip/ITokenAdminRegistry.sol';
+import {IGhoToken} from 'src/interfaces/IGhoToken.sol';
+import {IGhoCcipSteward} from 'src/interfaces/IGhoCcipSteward.sol';
+
+import {ProtocolV3TestBase} from 'aave-helpers/src/ProtocolV3TestBase.sol';
+import {AaveV3Arbitrum} from 'aave-address-book/AaveV3Arbitrum.sol';
+import {AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
+import {AaveV3ArbitrumAssets} from 'aave-address-book/AaveV3Arbitrum.sol';
+
+import {CCIPUtils} from './utils/CCIPUtils.sol';
+
+import {AaveV3Ethereum_GHOGnosisLaunch_20250421} from './AaveV3Ethereum_GHOGnosisLaunch_20250421.sol';
+
+/**
+ * @dev Test for AaveV3Ethereum_GHOGnosisLaunch_20250421
+ * command: FOUNDRY_PROFILE=mainnet forge test --match-path=src/20250421_Multi_GHOGnosisLaunch/AaveV3Ethereum_GHOGnosisLaunch_20250421.t.sol -vv
+ */
+contract AaveV3Ethereum_GHOGnosisLaunch_20250421_Test is ProtocolV3TestBase {
+  struct CCIPSendParams {
+    address sender;
+    uint256 amount;
+    uint64 destChainSelector;
+  }
+
+  uint64 internal constant ARB_CHAIN_SELECTOR = CCIPUtils.ARB_CHAIN_SELECTOR;
+  uint64 internal constant BASE_CHAIN_SELECTOR = CCIPUtils.BASE_CHAIN_SELECTOR;
+  uint64 internal constant ETH_CHAIN_SELECTOR = CCIPUtils.ETH_CHAIN_SELECTOR;
+  uint256 internal constant CCIP_RATE_LIMIT_CAPACITY = 300_000e18;
+  uint256 internal constant CCIP_RATE_LIMIT_REFILL_RATE = 60e18;
+
+  IGhoToken internal constant GHO = IGhoToken(AaveV3EthereumAssets.GHO_UNDERLYING);
+  ITokenAdminRegistry internal constant TOKEN_ADMIN_REGISTRY =
+    ITokenAdminRegistry(0xb22764f98dD05c789929716D677382Df22C05Cb6);
+  IEVM2EVMOnRamp internal constant ARB_ON_RAMP =
+    IEVM2EVMOnRamp(0x69eCC4E2D8ea56E2d0a05bF57f4Fd6aEE7f2c284);
+  IEVM2EVMOnRamp internal constant BASE_ON_RAMP =
+    IEVM2EVMOnRamp(0xb8a882f3B88bd52D1Ff56A873bfDB84b70431937);
+  IEVM2EVMOffRamp_1_5 internal constant ARB_OFF_RAMP =
+    IEVM2EVMOffRamp_1_5(0xdf615eF8D4C64d0ED8Fd7824BBEd2f6a10245aC9);
+  IEVM2EVMOffRamp_1_5 internal constant BASE_OFF_RAMP =
+    IEVM2EVMOffRamp_1_5(0x6B4B6359Dd5B47Cdb030E5921456D2a0625a9EbD);
+
+  address public constant NEW_REMOTE_TOKEN_BASE = 0x6Bb7a212910682DCFdbd5BCBb3e28FB4E8da10Ee;
+  address internal constant NEW_REMOTE_POOL_ARB = 0xB94Ab28c6869466a46a42abA834ca2B3cECCA5eB;
+  address internal constant NEW_REMOTE_POOL_BASE = 0x98217A06721Ebf727f2C8d9aD7718ec28b7aAe34;
+  address internal constant RISK_COUNCIL = 0x8513e6F37dBc52De87b166980Fa3F50639694B60; // common across all chains
+  IRouter internal constant ROUTER = IRouter(0x80226fc0Ee2b096224EeAc085Bb9a8cba1146f7D);
+  IGhoCcipSteward internal constant NEW_GHO_CCIP_STEWARD =
+    IGhoCcipSteward(0xC5BcC58BE6172769ca1a78B8A45752E3C5059c39);
+  IUpgradeableLockReleaseTokenPool_1_5_1 internal constant NEW_TOKEN_POOL =
+    IUpgradeableLockReleaseTokenPool_1_5_1(0x06179f7C1be40863405f374E7f5F8806c728660A);
+
+  AaveV3Ethereum_GHOGnosisLaunch_20250421 internal proposal;
+
+  address internal alice = makeAddr('alice');
+  address internal bob = makeAddr('bob');
+  address internal carol = makeAddr('carol');
+
+  event Locked(address indexed sender, uint256 amount);
+  event Released(address indexed sender, address indexed recipient, uint256 amount);
+  event CCIPSendRequested(IInternal.EVM2EVMMessage message);
+
+  error CallerIsNotARampOnRouter(address);
+  error InvalidSourcePoolAddress(bytes);
+
+  function setUp() public {
+    vm.createSelectFork(vm.rpcUrl('mainnet'), 21722753);
+    proposal = new AaveV3Ethereum_GHOGnosisLaunch_20250421();
+    _validateConstants();
+    executePayload(vm, address(proposal));
+  }
+
+  function _validateConstants() private view {
+    assertEq(proposal.BASE_CHAIN_SELECTOR(), BASE_CHAIN_SELECTOR);
+    assertEq(address(proposal.TOKEN_POOL()), address(NEW_TOKEN_POOL));
+    assertEq(proposal.REMOTE_TOKEN_POOL_BASE(), NEW_REMOTE_POOL_BASE);
+    assertEq(proposal.REMOTE_GHO_TOKEN_BASE(), NEW_REMOTE_TOKEN_BASE);
+    assertEq(proposal.CCIP_RATE_LIMIT_CAPACITY(), CCIP_RATE_LIMIT_CAPACITY);
+    assertEq(proposal.CCIP_RATE_LIMIT_REFILL_RATE(), CCIP_RATE_LIMIT_REFILL_RATE);
+
+    assertEq(TOKEN_ADMIN_REGISTRY.typeAndVersion(), 'TokenAdminRegistry 1.5.0');
+    assertEq(NEW_TOKEN_POOL.typeAndVersion(), 'LockReleaseTokenPool 1.5.1');
+    assertEq(ROUTER.typeAndVersion(), 'Router 1.2.0');
+
+    _assertOnRamp(ARB_ON_RAMP, ETH_CHAIN_SELECTOR, ARB_CHAIN_SELECTOR, ROUTER);
+    _assertOnRamp(BASE_ON_RAMP, ETH_CHAIN_SELECTOR, BASE_CHAIN_SELECTOR, ROUTER);
+    _assertOffRamp(ARB_OFF_RAMP, ARB_CHAIN_SELECTOR, ETH_CHAIN_SELECTOR, ROUTER);
+    _assertOffRamp(BASE_OFF_RAMP, BASE_CHAIN_SELECTOR, ETH_CHAIN_SELECTOR, ROUTER);
+
+    assertEq(NEW_GHO_CCIP_STEWARD.RISK_COUNCIL(), RISK_COUNCIL);
+    assertEq(NEW_GHO_CCIP_STEWARD.GHO_TOKEN(), AaveV3EthereumAssets.GHO_UNDERLYING);
+    assertEq(NEW_GHO_CCIP_STEWARD.GHO_TOKEN_POOL(), address(NEW_TOKEN_POOL));
+    assertTrue(NEW_GHO_CCIP_STEWARD.BRIDGE_LIMIT_ENABLED());
+  }
+
+  function _assertOnRamp(
+    IEVM2EVMOnRamp onRamp,
+    uint64 srcSelector,
+    uint64 dstSelector,
+    IRouter router
+  ) internal view {
+    assertEq(onRamp.typeAndVersion(), 'EVM2EVMOnRamp 1.5.0');
+    assertEq(onRamp.getStaticConfig().chainSelector, srcSelector);
+    assertEq(onRamp.getStaticConfig().destChainSelector, dstSelector);
+    assertEq(onRamp.getDynamicConfig().router, address(router));
+    assertEq(router.getOnRamp(dstSelector), address(onRamp));
+  }
+
+  function _assertOffRamp(
+    IEVM2EVMOffRamp_1_5 offRamp,
+    uint64 srcSelector,
+    uint64 dstSelector,
+    IRouter router
+  ) internal view {
+    assertEq(offRamp.typeAndVersion(), 'EVM2EVMOffRamp 1.5.0');
+    assertEq(offRamp.getStaticConfig().sourceChainSelector, srcSelector);
+    assertEq(offRamp.getStaticConfig().chainSelector, dstSelector);
+    assertEq(offRamp.getDynamicConfig().router, address(router));
+    assertTrue(router.isOffRamp(srcSelector, address(offRamp)));
+  }
+
+  function _getTokenMessage(
+    CCIPSendParams memory params
+  ) internal returns (IClient.EVM2AnyMessage memory, IInternal.EVM2EVMMessage memory) {
+    IClient.EVM2AnyMessage memory message = CCIPUtils.generateMessage(params.sender, 1);
+    message.tokenAmounts[0] = IClient.EVMTokenAmount({token: address(GHO), amount: params.amount});
+
+    uint256 feeAmount = ROUTER.getFee(params.destChainSelector, message);
+    deal(params.sender, feeAmount);
+
+    IInternal.EVM2EVMMessage memory eventArg = CCIPUtils.messageToEvent(
+      CCIPUtils.MessageToEventParams({
+        message: message,
+        router: ROUTER,
+        sourceChainSelector: ETH_CHAIN_SELECTOR,
+        destChainSelector: params.destChainSelector,
+        feeTokenAmount: feeAmount,
+        originalSender: params.sender,
+        sourceToken: address(GHO),
+        destinationToken: address(
+          params.destChainSelector == BASE_CHAIN_SELECTOR
+            ? NEW_REMOTE_TOKEN_BASE
+            : AaveV3ArbitrumAssets.GHO_UNDERLYING
+        )
+      })
+    );
+
+    return (message, eventArg);
+  }
+
+  function _tokenBucketToConfig(
+    IRateLimiter.TokenBucket memory bucket
+  ) internal pure returns (IRateLimiter.Config memory) {
+    return
+      IRateLimiter.Config({
+        isEnabled: bucket.isEnabled,
+        capacity: bucket.capacity,
+        rate: bucket.rate
+      });
+  }
+
+  function _getDisabledConfig() internal pure returns (IRateLimiter.Config memory) {
+    return IRateLimiter.Config({isEnabled: false, capacity: 0, rate: 0});
+  }
+
+  function _getImplementation(address proxy) internal view returns (address) {
+    bytes32 slot = bytes32(uint256(keccak256('eip1967.proxy.implementation')) - 1);
+    return address(uint160(uint256(vm.load(proxy, slot))));
+  }
+
+  function _readInitialized(address proxy) internal view returns (uint8) {
+    return uint8(uint256(vm.load(proxy, bytes32(0))));
+  }
+
+  function _getRateLimiterConfig() internal view returns (IRateLimiter.Config memory) {
+    return
+      IRateLimiter.Config({
+        isEnabled: true,
+        capacity: proposal.CCIP_RATE_LIMIT_CAPACITY(),
+        rate: proposal.CCIP_RATE_LIMIT_REFILL_RATE()
+      });
+  }
+
+  function _getOutboundRefillTime(uint256 amount) internal pure returns (uint256) {
+    return (amount / CCIP_RATE_LIMIT_REFILL_RATE) + 1; // account for rounding
+  }
+
+  function _getInboundRefillTime(uint256 amount) internal pure returns (uint256) {
+    return amount / CCIP_RATE_LIMIT_REFILL_RATE + 1; // account for rounding
+  }
+
+  function _min(uint256 a, uint256 b) internal pure returns (uint256) {
+    return a < b ? a : b;
+  }
+
+  function assertEq(
+    IRateLimiter.TokenBucket memory bucket,
+    IRateLimiter.Config memory config
+  ) internal pure {
+    assertEq(bucket.isEnabled, config.isEnabled);
+    assertEq(bucket.capacity, config.capacity);
+    assertEq(bucket.rate, config.rate);
+  }
+
+  function test_basePoolConfig() public view {
+    assertEq(NEW_TOKEN_POOL.getSupportedChains().length, 2);
+    assertEq(NEW_TOKEN_POOL.getSupportedChains()[0], ARB_CHAIN_SELECTOR);
+    assertEq(NEW_TOKEN_POOL.getSupportedChains()[1], BASE_CHAIN_SELECTOR);
+    assertEq(
+      NEW_TOKEN_POOL.getRemoteToken(ARB_CHAIN_SELECTOR),
+      abi.encode(address(AaveV3ArbitrumAssets.GHO_UNDERLYING))
+    );
+    assertEq(
+      NEW_TOKEN_POOL.getRemoteToken(BASE_CHAIN_SELECTOR),
+      abi.encode(address(NEW_REMOTE_TOKEN_BASE))
+    );
+    assertEq(NEW_TOKEN_POOL.getRemotePools(BASE_CHAIN_SELECTOR).length, 1);
+    assertEq(
+      NEW_TOKEN_POOL.getRemotePools(BASE_CHAIN_SELECTOR)[0],
+      abi.encode(address(NEW_REMOTE_POOL_BASE))
+    );
+    assertEq(NEW_TOKEN_POOL.getRemotePools(ARB_CHAIN_SELECTOR).length, 2);
+    assertEq(
+      NEW_TOKEN_POOL.getRemotePools(ARB_CHAIN_SELECTOR)[1], // 0th is the 1.4 token pool
+      abi.encode(address(NEW_REMOTE_POOL_ARB))
+    );
+    assertEq(
+      NEW_TOKEN_POOL.getCurrentInboundRateLimiterState(ARB_CHAIN_SELECTOR),
+      _getRateLimiterConfig()
+    );
+    assertEq(
+      NEW_TOKEN_POOL.getCurrentOutboundRateLimiterState(ARB_CHAIN_SELECTOR),
+      _getRateLimiterConfig()
+    );
+    assertEq(
+      NEW_TOKEN_POOL.getCurrentInboundRateLimiterState(BASE_CHAIN_SELECTOR),
+      _getRateLimiterConfig()
+    );
+    assertEq(
+      NEW_TOKEN_POOL.getCurrentOutboundRateLimiterState(BASE_CHAIN_SELECTOR),
+      _getRateLimiterConfig()
+    );
+  }
+
+  function test_sendMessageToBaseSucceeds(uint256 amount) public {
+    uint256 bridgeableAmount = _min(
+      NEW_TOKEN_POOL.getBridgeLimit() - NEW_TOKEN_POOL.getCurrentBridgedAmount(),
+      CCIP_RATE_LIMIT_CAPACITY
+    );
+    amount = bound(amount, 1, bridgeableAmount);
+    skip(_getOutboundRefillTime(amount)); // wait for the rate limiter to refill
+
+    deal(address(GHO), alice, amount);
+    vm.prank(alice);
+    GHO.approve(address(ROUTER), amount);
+
+    uint256 aliceBalance = GHO.balanceOf(alice);
+    uint256 currentBridgedAmount = NEW_TOKEN_POOL.getCurrentBridgedAmount();
+
+    (
+      IClient.EVM2AnyMessage memory message,
+      IInternal.EVM2EVMMessage memory eventArg
+    ) = _getTokenMessage(
+        CCIPSendParams({amount: amount, sender: alice, destChainSelector: BASE_CHAIN_SELECTOR})
+      );
+
+    vm.expectEmit(address(NEW_TOKEN_POOL));
+    emit Locked(address(BASE_ON_RAMP), amount);
+    vm.expectEmit(address(BASE_ON_RAMP));
+    emit CCIPSendRequested(eventArg);
+
+    vm.prank(alice);
+    ROUTER.ccipSend{value: eventArg.feeTokenAmount}(BASE_CHAIN_SELECTOR, message);
+
+    assertEq(GHO.balanceOf(alice), aliceBalance - amount);
+    assertEq(NEW_TOKEN_POOL.getCurrentBridgedAmount(), currentBridgedAmount + amount);
+  }
+
+  function test_sendMessageToArbSucceeds(uint256 amount) public {
+    uint256 bridgeableAmount = _min(
+      NEW_TOKEN_POOL.getBridgeLimit() - NEW_TOKEN_POOL.getCurrentBridgedAmount(),
+      CCIP_RATE_LIMIT_CAPACITY
+    );
+    amount = bound(amount, 1, bridgeableAmount);
+    skip(_getOutboundRefillTime(amount)); // wait for the rate limiter to refill
+
+    deal(address(GHO), alice, amount);
+    vm.prank(alice);
+    GHO.approve(address(ROUTER), amount);
+
+    uint256 aliceBalance = GHO.balanceOf(alice);
+    uint256 currentBridgedAmount = NEW_TOKEN_POOL.getCurrentBridgedAmount();
+
+    (
+      IClient.EVM2AnyMessage memory message,
+      IInternal.EVM2EVMMessage memory eventArg
+    ) = _getTokenMessage(
+        CCIPSendParams({amount: amount, sender: alice, destChainSelector: ARB_CHAIN_SELECTOR})
+      );
+
+    vm.expectEmit(address(NEW_TOKEN_POOL));
+    emit Locked(address(ARB_ON_RAMP), amount);
+    vm.expectEmit(address(ARB_ON_RAMP));
+    emit CCIPSendRequested(eventArg);
+
+    vm.prank(alice);
+    ROUTER.ccipSend{value: eventArg.feeTokenAmount}(ARB_CHAIN_SELECTOR, message);
+
+    assertEq(GHO.balanceOf(alice), aliceBalance - amount);
+    assertEq(NEW_TOKEN_POOL.getCurrentBridgedAmount(), currentBridgedAmount + amount);
+  }
+
+  function test_offRampViaBaseSucceeds(uint256 amount) public {
+    uint256 bridgeableAmount = _min(
+      NEW_TOKEN_POOL.getCurrentBridgedAmount(),
+      CCIP_RATE_LIMIT_CAPACITY
+    );
+    amount = bound(amount, 1, bridgeableAmount);
+    skip(_getInboundRefillTime(amount));
+
+    uint256 aliceBalance = GHO.balanceOf(alice);
+    uint256 poolBalance = GHO.balanceOf(address(NEW_TOKEN_POOL));
+    uint256 currentBridgedAmount = NEW_TOKEN_POOL.getCurrentBridgedAmount();
+
+    vm.expectEmit(address(NEW_TOKEN_POOL));
+    emit Released(address(BASE_OFF_RAMP), alice, amount);
+
+    vm.prank(address(BASE_OFF_RAMP));
+    NEW_TOKEN_POOL.releaseOrMint(
+      IPool_CCIP.ReleaseOrMintInV1({
+        originalSender: abi.encode(alice),
+        remoteChainSelector: BASE_CHAIN_SELECTOR,
+        receiver: alice,
+        amount: amount,
+        localToken: address(GHO),
+        sourcePoolAddress: abi.encode(address(NEW_REMOTE_POOL_BASE)),
+        sourcePoolData: new bytes(0),
+        offchainTokenData: new bytes(0)
+      })
+    );
+
+    assertEq(GHO.balanceOf(address(NEW_TOKEN_POOL)), poolBalance - amount);
+    assertEq(GHO.balanceOf(alice), aliceBalance + amount);
+    assertEq(NEW_TOKEN_POOL.getCurrentBridgedAmount(), currentBridgedAmount - amount);
+    assertEq(NEW_TOKEN_POOL.getCurrentBridgedAmount(), GHO.balanceOf(address(NEW_TOKEN_POOL)));
+  }
+
+  function test_offRampViaArbSucceeds(uint256 amount) public {
+    uint256 bridgeableAmount = _min(
+      NEW_TOKEN_POOL.getCurrentBridgedAmount(),
+      CCIP_RATE_LIMIT_CAPACITY
+    );
+    amount = bound(amount, 1, bridgeableAmount);
+    skip(_getInboundRefillTime(amount));
+
+    uint256 aliceBalance = GHO.balanceOf(alice);
+    uint256 poolBalance = GHO.balanceOf(address(NEW_TOKEN_POOL));
+    uint256 currentBridgedAmount = NEW_TOKEN_POOL.getCurrentBridgedAmount();
+
+    vm.expectEmit(address(NEW_TOKEN_POOL));
+    emit Released(address(ARB_OFF_RAMP), alice, amount);
+
+    vm.prank(address(ARB_OFF_RAMP));
+    NEW_TOKEN_POOL.releaseOrMint(
+      IPool_CCIP.ReleaseOrMintInV1({
+        originalSender: abi.encode(alice),
+        remoteChainSelector: ARB_CHAIN_SELECTOR,
+        receiver: alice,
+        amount: amount,
+        localToken: address(GHO),
+        sourcePoolAddress: abi.encode(address(NEW_REMOTE_POOL_ARB)),
+        sourcePoolData: new bytes(0),
+        offchainTokenData: new bytes(0)
+      })
+    );
+
+    assertEq(GHO.balanceOf(address(NEW_TOKEN_POOL)), poolBalance - amount);
+    assertEq(GHO.balanceOf(alice), aliceBalance + amount);
+    assertEq(NEW_TOKEN_POOL.getCurrentBridgedAmount(), currentBridgedAmount - amount);
+    assertEq(NEW_TOKEN_POOL.getCurrentBridgedAmount(), GHO.balanceOf(address(NEW_TOKEN_POOL)));
+  }
+
+  function test_cannotUseBaseOffRampForArbMessages() public {
+    uint256 amount = 100e18;
+    skip(_getInboundRefillTime(amount));
+
+    vm.expectRevert(
+      abi.encodeWithSelector(CallerIsNotARampOnRouter.selector, address(BASE_OFF_RAMP))
+    );
+    vm.prank(address(BASE_OFF_RAMP));
+    NEW_TOKEN_POOL.releaseOrMint(
+      IPool_CCIP.ReleaseOrMintInV1({
+        originalSender: abi.encode(alice),
+        remoteChainSelector: ARB_CHAIN_SELECTOR,
+        receiver: alice,
+        amount: amount,
+        localToken: address(GHO),
+        sourcePoolAddress: abi.encode(address(NEW_REMOTE_POOL_ARB)),
+        sourcePoolData: new bytes(0),
+        offchainTokenData: new bytes(0)
+      })
+    );
+  }
+
+  function test_cannotOffRampOtherChainMessages() public {
+    uint256 amount = 100e18;
+    skip(_getInboundRefillTime(amount));
+
+    vm.expectRevert(
+      abi.encodeWithSelector(
+        InvalidSourcePoolAddress.selector,
+        abi.encode(address(NEW_REMOTE_POOL_ARB))
+      )
+    );
+    vm.prank(address(BASE_OFF_RAMP));
+    NEW_TOKEN_POOL.releaseOrMint(
+      IPool_CCIP.ReleaseOrMintInV1({
+        originalSender: abi.encode(alice),
+        remoteChainSelector: BASE_CHAIN_SELECTOR,
+        receiver: alice,
+        amount: amount,
+        localToken: address(GHO),
+        sourcePoolAddress: abi.encode(address(NEW_REMOTE_POOL_ARB)),
+        sourcePoolData: new bytes(0),
+        offchainTokenData: new bytes(0)
+      })
+    );
+
+    vm.expectRevert(
+      abi.encodeWithSelector(
+        InvalidSourcePoolAddress.selector,
+        abi.encode(address(NEW_REMOTE_POOL_BASE))
+      )
+    );
+    vm.prank(address(ARB_OFF_RAMP));
+    NEW_TOKEN_POOL.releaseOrMint(
+      IPool_CCIP.ReleaseOrMintInV1({
+        originalSender: abi.encode(alice),
+        remoteChainSelector: ARB_CHAIN_SELECTOR,
+        receiver: alice,
+        amount: amount,
+        localToken: address(GHO),
+        sourcePoolAddress: abi.encode(address(NEW_REMOTE_POOL_BASE)),
+        sourcePoolData: new bytes(0),
+        offchainTokenData: new bytes(0)
+      })
+    );
+  }
+}
---
+++ src/20250421_Multi_GHOGnosisLaunch/GHOGnosisLaunch_20250421.s.sol
@@ -0,0 +1,151 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {GovV3Helpers, IPayloadsControllerCore, PayloadsControllerUtils} from 'aave-helpers/src/GovV3Helpers.sol';
+import {GovernanceV3Ethereum} from 'aave-address-book/GovernanceV3Ethereum.sol';
+import {EthereumScript, ArbitrumScript, BaseScript, GnosisScript} from 'solidity-utils/contracts/utils/ScriptUtils.sol';
+import {AaveV3Ethereum_GHOGnosisLaunch_20250421} from './AaveV3Ethereum_GHOGnosisLaunch_20250421.sol';
+import {AaveV3Arbitrum_GHOGnosisLaunch_20250421} from './AaveV3Arbitrum_GHOGnosisLaunch_20250421.sol';
+import {AaveV3Base_GHOGnosisLaunch_20250421} from './AaveV3Base_GHOGnosisLaunch_20250421.sol';
+import {AaveV3Gnosis_GHOGnosisLaunch_20250421} from './AaveV3Gnosis_GHOGnosisLaunch_20250421.sol';
+import {AaveV3Gnosis_GHOGnosisListing_20250421} from './AaveV3Gnosis_GHOGnosisListing_20250421.sol';
+/**
+ * @dev Deploy Ethereum
+ * deploy-command: make deploy-ledger contract=src/20250421_Multi_GHOGnosisLaunch/GHOGnosisLaunch_20250421.s.sol:DeployEthereum chain=mainnet
+ * verify-command: FOUNDRY_PROFILE=mainnet npx catapulta-verify -b broadcast/GHOGnosisLaunch_20250421.s.sol/1/run-latest.json
+ */
+contract DeployEthereum is EthereumScript {
+  function run() external broadcast {
+    // deploy payloads
+    address payload0 = GovV3Helpers.deployDeterministic(
+      type(AaveV3Ethereum_GHOGnosisLaunch_20250421).creationCode
+    );
+
+    // compose action
+    IPayloadsControllerCore.ExecutionAction[]
+      memory actions = new IPayloadsControllerCore.ExecutionAction[](1);
+    actions[0] = GovV3Helpers.buildAction(payload0);
+
+    // register action at payloadsController
+    GovV3Helpers.createPayload(actions);
+  }
+}
+
+/**
+ * @dev Deploy Arbitrum
+ * deploy-command: make deploy-ledger contract=src/20250421_Multi_GHOGnosisLaunch/GHOGnosisLaunch_20250421.s.sol:DeployArbitrum chain=arbitrum
+ * verify-command: FOUNDRY_PROFILE=arbitrum npx catapulta-verify -b broadcast/GHOGnosisLaunch_20250421.s.sol/42161/run-latest.json
+ */
+contract DeployArbitrum is ArbitrumScript {
+  function run() external broadcast {
+    // deploy payloads
+    address payload0 = GovV3Helpers.deployDeterministic(
+      type(AaveV3Arbitrum_GHOGnosisLaunch_20250421).creationCode
+    );
+
+    // compose action
+    IPayloadsControllerCore.ExecutionAction[]
+      memory actions = new IPayloadsControllerCore.ExecutionAction[](1);
+    actions[0] = GovV3Helpers.buildAction(payload0);
+
+    // register action at payloadsController
+    GovV3Helpers.createPayload(actions);
+  }
+}
+
+/**
+ * @dev Deploy Base
+ * deploy-command: make deploy-ledger contract=src/20250421_Multi_GHOGnosisLaunch/GHOGnosisLaunch_20250421.s.sol:DeployBase chain=base
+ * verify-command: FOUNDRY_PROFILE=base npx catapulta-verify -b broadcast/GHOGnosisLaunch_20250421.s.sol/8453/run-latest.json
+ */
+contract DeployBase is BaseScript {
+  function run() external broadcast {
+    // deploy payloads
+    address payload0 = GovV3Helpers.deployDeterministic(
+      type(AaveV3Arbitrum_GHOGnosisLaunch_20250421).creationCode
+    );
+
+    // compose action
+    IPayloadsControllerCore.ExecutionAction[]
+      memory actions = new IPayloadsControllerCore.ExecutionAction[](1);
+    actions[0] = GovV3Helpers.buildAction(payload0);
+
+    // register action at payloadsController
+    GovV3Helpers.createPayload(actions);
+  }
+}
+
+/**
+ * @dev Deploy Gnosis
+ * deploy-command: make deploy-ledger contract=src/20250421_Multi_GHOGnosisLaunch/GHOGnosisLaunch_20250421.s.sol:DeployGnosis chain=gnosis
+ * verify-command: FOUNDRY_PROFILE=gnosis npx catapulta-verify -b broadcast/GHOGnosisLaunch_20250421.s.sol/100/run-latest.json
+ */
+contract DeployGnosis is GnosisScript {
+  function run() external broadcast {
+    // compose action
+    IPayloadsControllerCore.ExecutionAction[]
+      memory launchActions = new IPayloadsControllerCore.ExecutionAction[](1);
+    launchActions[0] = GovV3Helpers.buildAction(
+      GovV3Helpers.deployDeterministic(type(AaveV3Gnosis_GHOGnosisLaunch_20250421).creationCode)
+    );
+
+    IPayloadsControllerCore.ExecutionAction[]
+      memory listingActions = new IPayloadsControllerCore.ExecutionAction[](1);
+    listingActions[0] = GovV3Helpers.buildAction(
+      GovV3Helpers.deployDeterministic(type(AaveV3Gnosis_GHOBaseListing_20250421).creationCode)
+    );
+
+    // register both actions separately at payloadsController
+    GovV3Helpers.createPayload(launchActions);
+    GovV3Helpers.createPayload(listingActions);
+  }
+}
+
+/**
+ * @dev Create Proposal
+ * command: make deploy-ledger contract=src/20250421_Multi_GHOGnosisLaunch/GHOGnosisLaunch_20250421.s.sol:CreateProposal chain=mainnet
+ */
+contract CreateProposal is EthereumScript {
+  function run() external {
+    // create payloads
+    PayloadsControllerUtils.Payload[] memory payloads = new PayloadsControllerUtils.Payload[](4);
+
+    // compose actions for validation
+    IPayloadsControllerCore.ExecutionAction[]
+      memory actionsEthereum = new IPayloadsControllerCore.ExecutionAction[](1);
+    actionsEthereum[0] = GovV3Helpers.buildAction(
+      type(AaveV3Ethereum_GHOGnosisLaunch_20250421).creationCode
+    );
+    payloads[0] = GovV3Helpers.buildMainnetPayload(vm, actionsEthereum);
+
+    IPayloadsControllerCore.ExecutionAction[]
+      memory actionsArbitrum = new IPayloadsControllerCore.ExecutionAction[](1);
+    actionsArbitrum[0] = GovV3Helpers.buildAction(
+      type(AaveV3Arbitrum_GHOGnosisLaunch_20250421).creationCode
+    );
+    payloads[1] = GovV3Helpers.buildArbitrumPayload(vm, actionsArbitrum);
+
+    IPayloadsControllerCore.ExecutionAction[]
+      memory actionsBaseLaunch = new IPayloadsControllerCore.ExecutionAction[](1);
+    actionsBaseLaunch[0] = GovV3Helpers.buildAction(
+      type(AaveV3Base_GHOGnosisLaunch_20250421).creationCode
+    );
+    payloads[2] = GovV3Helpers.buildBasePayload(vm, actionsBaseLaunch);
+
+    IPayloadsControllerCore.ExecutionAction[]
+      memory actionsBaseListing = new IPayloadsControllerCore.ExecutionAction[](1);
+    actionsBaseListing[0] = GovV3Helpers.buildAction(
+      type(AaveV3Base_GHOBaseListing_20250421).creationCode
+    );
+    payloads[3] = GovV3Helpers.buildBasePayload(vm, actionsBaseListing);
+
+    // create proposal
+    vm.startBroadcast();
+    GovV3Helpers.createProposal(
+      vm,
+      payloads,
+      GovernanceV3Ethereum.VOTING_PORTAL_ETH_POL,
+      GovV3Helpers.ipfsHashFile(vm, 'src/20250421_Multi_GHOGnosisLaunch/GHOGnosisLaunch.md')
+    );
+  }
+}
