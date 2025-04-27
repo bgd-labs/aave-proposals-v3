9d2fd1b988baf57a4ea1b8bbc6b7f3df5328e118
defijesus.eth <defijesus@duck.com>
2025-04-27 15:26:16 +0100

    fix arb payload

--- src/20250426_Multi_MayFundingUpdate/AaveV3Arbitrum_MayFundingUpdate_20250426.sol
+++ src/20250426_Multi_MayFundingUpdate/AaveV3Arbitrum_MayFundingUpdate_20250426.sol
@@ -17,127 +17,47 @@ import {IERC20} from 'openzeppelin-contracts/contracts/token/ERC20/IERC20.sol';
 contract AaveV3Arbitrum_MayFundingUpdate_20250426 is IProposalGenericExecutor {
   using CollectorUtils for ICollector;
 
-  address public constant USDC_GATEWAY = 0x096760F208390250649E3e8763348E783AEF5562;
-  address public constant WETH_GATEWAY = 0x6c411aD3E74De3E7Bd422b94A27770f5B86C623B;
-  address public constant WSTETH_GATEWAY = 0x07D4692291B9E30E326fd31706f686f83f331B82;
+  address public constant DAI = AaveV3ArbitrumAssets.DAI_UNDERLYING;
+  address public constant DAI_GATEWAY = 0x467194771dAe2967Aef3ECbEDD3Bf9a310C76C65;
+
+  address public constant USDT = AaveV3ArbitrumAssets.USDT_UNDERLYING;
   address public constant USDT_GATEWAY = 0x14E4A1B13bf7F943c8ff7C51fb60FA964A298D92;
-  address public constant WBTC_GATEWAY = 0x09e9222E96E7B4AE2a407B98d48e330053351EEe;
-  address public constant WEETH_GATEWAY = 0x09e9222E96E7B4AE2a407B98d48e330053351EEe;
+
+  address public constant USDC = AaveV3ArbitrumAssets.USDC_UNDERLYING;
+  address public constant USDC_GATEWAY = 0x096760F208390250649E3e8763348E783AEF5562;
+
+  address public constant BRIDGE = MiscArbitrum.AAVE_ARB_ETH_BRIDGE;
+  address public constant COLLECTOR = address(AaveV3Arbitrum.COLLECTOR);
 
   function execute() external {
-    /// USDC
-    uint256 usdcAmountWithdrawn = CollectorUtils.withdrawFromV3(
-      AaveV3Arbitrum.COLLECTOR,
-      CollectorUtils.IOInput({
-        pool: address(AaveV3Arbitrum.POOL),
-        underlying: AaveV3ArbitrumAssets.USDC_UNDERLYING,
-        amount: IERC20(AaveV3ArbitrumAssets.USDC_A_TOKEN).balanceOf(
-          address(AaveV3Arbitrum.COLLECTOR)
-        )
-      }),
-      MiscArbitrum.AAVE_ARB_ETH_BRIDGE
-    );
-    IAaveArbEthERC20Bridge(MiscArbitrum.AAVE_ARB_ETH_BRIDGE).bridge(
-      AaveV3ArbitrumAssets.USDC_UNDERLYING,
-      AaveV3EthereumAssets.USDC_UNDERLYING,
-      USDC_GATEWAY,
-      usdcAmountWithdrawn
-    );
-
-    /// WETH
-    uint256 wethAmountWithdrawn = CollectorUtils.withdrawFromV3(
-      AaveV3Arbitrum.COLLECTOR,
-      CollectorUtils.IOInput({
-        pool: address(AaveV3Arbitrum.POOL),
-        underlying: AaveV3ArbitrumAssets.WETH_UNDERLYING,
-        amount: IERC20(AaveV3ArbitrumAssets.WETH_A_TOKEN).balanceOf(
-          address(AaveV3Arbitrum.COLLECTOR)
-        )
-      }),
-      MiscArbitrum.AAVE_ARB_ETH_BRIDGE
-    );
-    IAaveArbEthERC20Bridge(MiscArbitrum.AAVE_ARB_ETH_BRIDGE).bridge(
-      AaveV3ArbitrumAssets.WETH_UNDERLYING,
-      AaveV3EthereumAssets.WETH_UNDERLYING,
-      WETH_GATEWAY,
-      wethAmountWithdrawn
-    );
-
-    /// WSTETH https://github.com/bgd-labs/aave-helpers/blob/main/src/bridges/arbitrum/README.md
-    /// docs say this cant be done?
-    uint256 wsethAmountWithdrawn = CollectorUtils.withdrawFromV3(
-      AaveV3Arbitrum.COLLECTOR,
-      CollectorUtils.IOInput({
-        pool: address(AaveV3Arbitrum.POOL),
-        underlying: AaveV3ArbitrumAssets.wstETH_UNDERLYING,
-        amount: IERC20(AaveV3ArbitrumAssets.wstETH_A_TOKEN).balanceOf(
-          address(AaveV3Arbitrum.COLLECTOR)
-        )
-      }),
-      MiscArbitrum.AAVE_ARB_ETH_BRIDGE
-    );
-    IAaveArbEthERC20Bridge(MiscArbitrum.AAVE_ARB_ETH_BRIDGE).bridge(
-      AaveV3ArbitrumAssets.wstETH_UNDERLYING,
-      AaveV3EthereumAssets.wstETH_UNDERLYING,
-      WSTETH_GATEWAY,
-      wsethAmountWithdrawn
+    /// DAI
+    uint256 daiBalance = IERC20(DAI).balanceOf(COLLECTOR);
+    AaveV3Arbitrum.COLLECTOR.transfer(IERC20(DAI), BRIDGE, daiBalance);
+    IAaveArbEthERC20Bridge(BRIDGE).bridge(
+      DAI,
+      AaveV3EthereumAssets.DAI_UNDERLYING,
+      DAI_GATEWAY,
+      daiBalance
     );
 
     /// USDT
-    uint256 usdtAmountWithdrawn = CollectorUtils.withdrawFromV3(
-      AaveV3Arbitrum.COLLECTOR,
-      CollectorUtils.IOInput({
-        pool: address(AaveV3Arbitrum.POOL),
-        underlying: AaveV3ArbitrumAssets.USDT_UNDERLYING,
-        amount: IERC20(AaveV3ArbitrumAssets.USDT_A_TOKEN).balanceOf(
-          address(AaveV3Arbitrum.COLLECTOR)
-        )
-      }),
-      MiscArbitrum.AAVE_ARB_ETH_BRIDGE
-    );
-    IAaveArbEthERC20Bridge(MiscArbitrum.AAVE_ARB_ETH_BRIDGE).bridge(
-      AaveV3ArbitrumAssets.USDT_UNDERLYING,
+    uint256 usdtBalance = IERC20(USDT).balanceOf(COLLECTOR);
+    AaveV3Arbitrum.COLLECTOR.transfer(IERC20(USDT), BRIDGE, usdtBalance);
+    IAaveArbEthERC20Bridge(BRIDGE).bridge(
+      USDT,
       AaveV3EthereumAssets.USDT_UNDERLYING,
       USDT_GATEWAY,
-      usdtAmountWithdrawn
+      usdtBalance
     );
 
-    /// WBTC
-    uint256 wbtcAmountWithdrawn = CollectorUtils.withdrawFromV3(
-      AaveV3Arbitrum.COLLECTOR,
-      CollectorUtils.IOInput({
-        pool: address(AaveV3Arbitrum.POOL),
-        underlying: AaveV3ArbitrumAssets.WBTC_UNDERLYING,
-        amount: IERC20(AaveV3ArbitrumAssets.WBTC_A_TOKEN).balanceOf(
-          address(AaveV3Arbitrum.COLLECTOR)
-        )
-      }),
-      MiscArbitrum.AAVE_ARB_ETH_BRIDGE
-    );
-    IAaveArbEthERC20Bridge(MiscArbitrum.AAVE_ARB_ETH_BRIDGE).bridge(
-      AaveV3ArbitrumAssets.WBTC_UNDERLYING,
-      AaveV3EthereumAssets.WBTC_UNDERLYING,
-      WBTC_GATEWAY,
-      wbtcAmountWithdrawn
-    );
-
-    /// WEETH
-    uint256 weethAmountWithdrawn = CollectorUtils.withdrawFromV3(
-      AaveV3Arbitrum.COLLECTOR,
-      CollectorUtils.IOInput({
-        pool: address(AaveV3Arbitrum.POOL),
-        underlying: AaveV3ArbitrumAssets.weETH_UNDERLYING,
-        amount: IERC20(AaveV3ArbitrumAssets.weETH_A_TOKEN).balanceOf(
-          address(AaveV3Arbitrum.COLLECTOR)
-        )
-      }),
-      MiscArbitrum.AAVE_ARB_ETH_BRIDGE
-    );
-    IAaveArbEthERC20Bridge(MiscArbitrum.AAVE_ARB_ETH_BRIDGE).bridge(
-      AaveV3ArbitrumAssets.weETH_UNDERLYING,
-      AaveV3EthereumAssets.weETH_UNDERLYING,
-      WEETH_GATEWAY,
-      weethAmountWithdrawn
+    /// USDC
+    uint256 usdcBalance = IERC20(USDC).balanceOf(COLLECTOR);
+    AaveV3Arbitrum.COLLECTOR.transfer(IERC20(USDC), BRIDGE, usdcBalance);
+    IAaveArbEthERC20Bridge(BRIDGE).bridge(
+      USDC,
+      AaveV3EthereumAssets.USDC_UNDERLYING,
+      USDC_GATEWAY,
+      usdcBalance
     );
   }
 }
