21f4eebe1fc099384b2ee02b9129814e987e669e
Lukas <lukasstrassel@googlemail.com>
2025-06-24 18:24:45 +0200

    fix: inject zksync id

--- src/20250610_Multi_UpgradeAaveInstancesToV34/UpgradeAaveInstancesToV34_20250610.s.sol
+++ src/20250610_Multi_UpgradeAaveInstancesToV34/UpgradeAaveInstancesToV34_20250610.s.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.8.0;
 
 import {GovV3Helpers, IPayloadsControllerCore, PayloadsControllerUtils} from 'aave-helpers/src/GovV3Helpers.sol';
 import {GovernanceV3Ethereum} from 'aave-address-book/GovernanceV3Ethereum.sol';
-import {EthereumScript, PolygonScript, AvalancheScript, OptimismScript, ArbitrumScript, MetisScript, BaseScript, GnosisScript, ScrollScript, BNBScript, LineaScript, CeloScript, SonicScript, SoneiumScript} from 'solidity-utils/contracts/utils/ScriptUtils.sol';
+import {GovernanceV3ZkSync} from 'aave-address-book/GovernanceV3ZkSync.sol';
+import {ChainIds, EthereumScript, PolygonScript, AvalancheScript, OptimismScript, ArbitrumScript, MetisScript, BaseScript, GnosisScript, ScrollScript, BNBScript, LineaScript, CeloScript, SonicScript, SoneiumScript} from 'solidity-utils/contracts/utils/ScriptUtils.sol';
 import {AuditReimbursement} from './AuditReimbursement.sol';
 
 library Deployments {
@@ -399,10 +400,12 @@ contract CreateProposal is EthereumScript {
     actionsBNB[0] = GovV3Helpers.buildAction(Deployments.BNB);
     payloads[10] = GovV3Helpers.buildBNBPayload(vm, actionsBNB);
 
-    IPayloadsControllerCore.ExecutionAction[]
-      memory actionsZkSync = new IPayloadsControllerCore.ExecutionAction[](1);
-    actionsZkSync[0] = GovV3Helpers.buildAction(Deployments.ZKSYNC);
-    payloads[11] = GovV3Helpers.buildZkSyncPayload(vm, actionsZkSync);
+    payloads[11] = PayloadsControllerUtils.Payload({
+      chain: ChainIds.ZKSYNC,
+      accessLevel: PayloadsControllerUtils.AccessControl.Level_1,
+      payloadsController: address(GovernanceV3ZkSync.PAYLOADS_CONTROLLER),
+      payloadId: 28
+    });
 
     IPayloadsControllerCore.ExecutionAction[]
       memory actionsLinea = new IPayloadsControllerCore.ExecutionAction[](1);
