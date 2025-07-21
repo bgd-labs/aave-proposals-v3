885934e469c72168b2a1f39cb94e8814317113af
Rozengarden <github@rozengarden.eu>
2025-07-21 13:44:20 +0200

    feat: update rf and slope2 of wsteth on core & prime

---
+++ src/20250721_Multi_InterestRateUpdateWETHAndWstETHEthereum/AaveV3EthereumLido_InterestRateUpdateWETHAndWstETHEthereum_20250721.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {AaveV3EthereumLidoAssets} from 'aave-address-book/AaveV3EthereumLido.sol';
+import {AaveV3PayloadEthereumLido} from 'aave-helpers/src/v3-config-engine/AaveV3PayloadEthereumLido.sol';
+import {EngineFlags} from 'aave-v3-origin/contracts/extensions/v3-config-engine/EngineFlags.sol';
+import {IAaveV3ConfigEngine} from 'aave-v3-origin/contracts/extensions/v3-config-engine/IAaveV3ConfigEngine.sol';
+
+/**
+ * @title Interest Rate Update - WETH and wstETH Ethereum
+ * @author Aave-Chan Initiative
+ * - Snapshot: Direct-to-AIP
+ * - Discussion: https://governance.aave.com/t/risk-stewards-interest-rate-update-weth-and-wsteth-ethereum/22625
+ */
+contract AaveV3EthereumLido_InterestRateUpdateWETHAndWstETHEthereum_20250721 is
+  AaveV3PayloadEthereumLido
+{
+  function rateStrategiesUpdates()
+    public
+    pure
+    override
+    returns (IAaveV3ConfigEngine.RateStrategyUpdate[] memory)
+  {
+    IAaveV3ConfigEngine.RateStrategyUpdate[]
+      memory rateStrategies = new IAaveV3ConfigEngine.RateStrategyUpdate[](1);
+    rateStrategies[0] = IAaveV3ConfigEngine.RateStrategyUpdate({
+      asset: AaveV3EthereumLidoAssets.wstETH_UNDERLYING,
+      params: IAaveV3ConfigEngine.InterestRateInputData({
+        optimalUsageRatio: EngineFlags.KEEP_CURRENT,
+        baseVariableBorrowRate: EngineFlags.KEEP_CURRENT,
+        variableRateSlope1: EngineFlags.KEEP_CURRENT,
+        variableRateSlope2: 40_00
+      })
+    });
+
+    return rateStrategies;
+  }
+}
---
+++ src/20250721_Multi_InterestRateUpdateWETHAndWstETHEthereum/config.ts
@@ -0,0 +1,58 @@
+import {ConfigFile} from '../../generator/types';
+export const config: ConfigFile = {
+  rootOptions: {
+    author: 'Aave-Chan Initiative',
+    pools: ['AaveV3Ethereum', 'AaveV3EthereumLido'],
+    title: 'Interest Rate Update - WETH and wstETH Ethereum',
+    shortName: 'InterestRateUpdateWETHAndWstETHEthereum',
+    date: '20250721',
+    discussion:
+      'https://governance.aave.com/t/risk-stewards-interest-rate-update-weth-and-wsteth-ethereum/22625',
+    snapshot: 'Direct-to-AIP',
+    votingNetwork: 'POLYGON',
+  },
+  poolOptions: {
+    AaveV3Ethereum: {
+      configs: {
+        RATE_UPDATE_V3: [
+          {
+            asset: 'wstETH',
+            params: {
+              optimalUtilizationRate: '',
+              baseVariableBorrowRate: '',
+              variableRateSlope1: '',
+              variableRateSlope2: '40',
+            },
+          },
+        ],
+        BORROWS_UPDATE: [
+          {
+            enabledToBorrow: 'KEEP_CURRENT',
+            flashloanable: 'KEEP_CURRENT',
+            borrowableInIsolation: 'KEEP_CURRENT',
+            withSiloedBorrowing: 'KEEP_CURRENT',
+            reserveFactor: '35',
+            asset: 'wstETH',
+          },
+        ],
+      },
+      cache: {blockNumber: 22967219},
+    },
+    AaveV3EthereumLido: {
+      configs: {
+        RATE_UPDATE_V3: [
+          {
+            asset: 'wstETH',
+            params: {
+              optimalUtilizationRate: '',
+              baseVariableBorrowRate: '',
+              variableRateSlope1: '',
+              variableRateSlope2: '40',
+            },
+          },
+        ],
+      },
+      cache: {blockNumber: 22967227},
+    },
+  },
+};
