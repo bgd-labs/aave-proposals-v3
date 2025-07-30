c562a1c9ab1b79d9078f4355bf9cbe690fb7e693
Alice <121383428+Rozengarden@users.noreply.github.com>
2025-07-30 13:17:47 +0200

    Interest Rate Update - WETH and wstETH Ethereum (#770)
    * feat: update rf and slope2 of wsteth on core & prime
    
    * fix: title
    
    * 🚨 lint md
    
    * Update src/20250721_Multi_InterestRateUpdateWETHAndWstETHEthereum/InterestRateUpdateWETHAndWstETHEthereum.md
    
    Co-authored-by: Harsh Pandey <harshsatishpandey@gmail.com>
    
    * RF prime update
    
    * update core slope1
    
    * uOpt changes
    
    * rm uOpt changes
    
    * rm prime changes except RF
    
    * rerun tests
    
    * revert
    
    * update MD
    
    * update MD
    
    ---------
    
    Co-authored-by: MartinGbz <martin.grabarz@outlook.fr>
    Co-authored-by: marczeller <pierarte@gmail.com>
    Co-authored-by: Marc Zeller <21088542+marczeller@users.noreply.github.com>
    Co-authored-by: Harsh Pandey <harshsatishpandey@gmail.com>

---
+++ src/20250721_Multi_InterestRateUpdateWETHAndWstETHEthereum/AaveV3Ethereum_InterestRateUpdateWETHAndWstETHEthereum_20250721.t.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {AaveV3Ethereum} from 'aave-address-book/AaveV3Ethereum.sol';
+
+import 'forge-std/Test.sol';
+import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/src/ProtocolV3TestBase.sol';
+import {AaveV3Ethereum_InterestRateUpdateWETHAndWstETHEthereum_20250721} from './AaveV3Ethereum_InterestRateUpdateWETHAndWstETHEthereum_20250721.sol';
+
+/**
+ * @dev Test for AaveV3Ethereum_InterestRateUpdateWETHAndWstETHEthereum_20250721
+ * command: FOUNDRY_PROFILE=test forge test --match-path=src/20250721_Multi_InterestRateUpdateWETHAndWstETHEthereum/AaveV3Ethereum_InterestRateUpdateWETHAndWstETHEthereum_20250721.t.sol -vv
+ */
+contract AaveV3Ethereum_InterestRateUpdateWETHAndWstETHEthereum_20250721_Test is
+  ProtocolV3TestBase
+{
+  AaveV3Ethereum_InterestRateUpdateWETHAndWstETHEthereum_20250721 internal proposal;
+
+  function setUp() public {
+    vm.createSelectFork(vm.rpcUrl('mainnet'), 22967219);
+    proposal = new AaveV3Ethereum_InterestRateUpdateWETHAndWstETHEthereum_20250721();
+  }
+
+  /**
+   * @dev executes the generic test suite including e2e and config snapshots
+   */
+  function test_defaultProposalExecution() public {
+    defaultTest(
+      'AaveV3Ethereum_InterestRateUpdateWETHAndWstETHEthereum_20250721',
+      AaveV3Ethereum.POOL,
+      address(proposal)
+    );
+  }
+}
