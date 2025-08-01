1dd904858988a4b086ba73d43689ff35d36373be
Lukas <ls@bgdlabs.com>
2025-08-01 16:44:32 +0200

    Feat/v3.5 (#775)
    * feat: v3.5
    
    * fix: add script
    
    * fix:script
    
    * fix: typo
    
    * fix: adjust script
    
    * fix: add zksync payload id

---
+++ src/20250722_Multi_UpgradeAaveInstancesToV35/Deployments.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.10;
+
+library Deployments {
+  address public constant BASE = 0x2865680741199B50870eD452fE9b590501791c87; // cancun
+  address public constant GNOSIS = 0x32A1b4a26e0bA9164bc05eb764D3cd7337d431aD; // cancun
+  address public constant POLYGON = 0x27a69BE794648aa6A08E449490d60f1669aEd2e0; // cancun
+  address public constant ARBITRUM = 0xC1a25A94cB35C4973BFD435CeAB6aeb800278584; // cancun
+  address public constant OPTIMISM = 0x93f16Fd74eF98e7652bA5C5aD844473Cbe7377A5; // cancun
+  address public constant AVALANCHE = 0xe02C2cDA7e2000D75dE8AE9A93A8151afA8186f4; // cancun
+  address public constant SONIC = 0x85163f633bC6918a803C8D70791570D948b312Bc; // cancun
+  address public constant METIS = 0x6fF38fBBD238b8363b04C0C54ffAE0622Bf1B289; //shanghai
+  address public constant BNB = 0x6d70Ed63F662f325135e4C97af2B030068f536B4; // cancun
+  address public constant CELO = 0xD8d64BAb7C362E5F762ea0E7c334801Fba604028; // cancun
+  address public constant SCROLL = 0x7467F305c2a269C22548C0821E6a33caa2C7Ec9B; // cancun
+  address public constant LINEA = 0xc0799CF19914081684f0203cC87FE5436d5d717F; // london
+  address public constant ZKSYNC = 0x1bC646CD1Fc56024f16B1Abf2197Fc0100c00Cf8; // cancun
+  address public constant SONEIUM = 0xc1082221D1a99BAc4e1FcBE406c9A4AE8c6a825C; // cancun
+  address public constant MAINNET_CORE = 0xD51B0B941cfcF3388c17ABa5DcBab766d34fD45b; // cancun
+  address public constant MAINNET_LIDO = 0x6ab8c847371911C778963Fa0C07645f3e587B012; // cancun
+  address public constant MAINNET_ETHERFI = 0xb705bdF5DF2c8658e3C3f3bdcCA6084000C026d3; // cancun
+}
--- zksync/src/20250722_Multi_UpgradeAaveInstancesToV35/UpgradeAaveInstancesToV35_20250722.s.sol
+++ zksync/src/20250722_Multi_UpgradeAaveInstancesToV35/UpgradeAaveInstancesToV35_20250722.s.sol
@@ -2,22 +2,19 @@
 pragma solidity ^0.8.0;
 
 import {GovV3Helpers, IPayloadsControllerCore} from 'aave-helpers/src/GovV3Helpers.sol';
+
 import {ZkSyncScript} from 'solidity-utils/contracts/utils/ScriptUtils.sol';
-import {AaveV3ZkSync_ConfigurationMaintenance_20250519} from './AaveV3ZkSync_ConfigurationMaintenance_20250519.sol';
 
 /**
  * @dev Deploy ZkSync
- * deploy-command: FOUNDRY_PROFILE=zksync make deploy-ledger contract=zksync/src/20250519_Multi_ConfigurationMaintenance/ConfigurationMaintenance_20250519.s.sol:DeployZkSync chain=zksync
+ * deploy-command: make deploy-ledger-zk contract=zksync/src/20250722_Multi_UpgradeAaveInstancesToV35/UpgradeAaveInstancesToV35_20250722.s.sol:DeployZkSync chain=zksync
  */
 contract DeployZkSync is ZkSyncScript {
   function run() external broadcast {
-    // deploy payloads
-    address payload0 = address(new AaveV3ZkSync_ConfigurationMaintenance_20250519{salt: 'aave'}());
-
     // compose action
     IPayloadsControllerCore.ExecutionAction[]
       memory actions = new IPayloadsControllerCore.ExecutionAction[](1);
-    actions[0] = GovV3Helpers.buildAction(payload0);
+    actions[0] = GovV3Helpers.buildAction(0x1bC646CD1Fc56024f16B1Abf2197Fc0100c00Cf8);
 
     // register action at payloadsController
     GovV3Helpers.createPayload(actions);
