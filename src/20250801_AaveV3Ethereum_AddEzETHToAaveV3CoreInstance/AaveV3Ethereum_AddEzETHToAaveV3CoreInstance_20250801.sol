f7d46477f982d68f7e7fa33c666f9b8f11193f1f
0xAurelou <0xaurelou@gmail.com>
2025-08-05 15:20:37 +0200

    fix: asset name use dedicated files

--- src/20250801_AaveV3Ethereum_AddEzETHToAaveV3CoreInstance/AaveV3Ethereum_AddEzETHToAaveV3CoreInstance_20250801.sol
+++ src/20250801_AaveV3Ethereum_AddEzETHToAaveV3CoreInstance/AaveV3Ethereum_AddEzETHToAaveV3CoreInstance_20250801.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
+import {AaveV3EthereumLido} from 'aave-address-book/AaveV3EthereumLido.sol';
 import {AaveV3PayloadEthereum} from 'aave-helpers/src/v3-config-engine/AaveV3PayloadEthereum.sol';
 import {EngineFlags} from 'aave-v3-origin/contracts/extensions/v3-config-engine/EngineFlags.sol';
 import {IAaveV3ConfigEngine} from 'aave-v3-origin/contracts/extensions/v3-config-engine/IAaveV3ConfigEngine.sol';
@@ -17,8 +18,6 @@ import {SafeERC20} from 'openzeppelin-contracts/contracts/token/ERC20/utils/Safe
 contract AaveV3Ethereum_AddEzETHToAaveV3CoreInstance_20250801 is AaveV3PayloadEthereum {
   using SafeERC20 for IERC20;
 
-  address public constant ezETH = 0xbf5495Efe5DB9ce00f80364C8B423567e58d2110;
-  address public constant ezETH_PRICE_FEED = 0xF3d49021fF3bbBFDfC1992A4b09E5D1d141D044C;
   uint256 public constant ezETH_SEED_AMOUNT = 1e16;
 
   function _postExecute() internal override {
@@ -71,9 +70,9 @@ contract AaveV3Ethereum_AddEzETHToAaveV3CoreInstance_20250801 is AaveV3PayloadEt
     IAaveV3ConfigEngine.Listing[] memory listings = new IAaveV3ConfigEngine.Listing[](1);
 
     listings[0] = IAaveV3ConfigEngine.Listing({
-      asset: ezETH,
+      asset: AaveV3EthereumLido.ezETH_UNDERLYING,
       assetSymbol: 'ezETH',
-      priceFeed: ezETH_PRICE_FEED,
+      priceFeed: AaveV3EthereumLido.ezETH_ORACLE,
       enabledToBorrow: EngineFlags.DISABLED,
       borrowableInIsolation: EngineFlags.DISABLED,
       withSiloedBorrowing: EngineFlags.DISABLED,
