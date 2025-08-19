8d8cc0101a7d68164c13b783a1cdb459b01ef8f5
Alan Donoso Naumczuk <adonoso@itba.edu.ar>
2025-08-19 16:11:05 -0300

    feat: CCIP remote lanes added

---
+++ src/20250814_Multi_GHOInkLaunch/remote-lanes/Arbitrum_Ink_AaveV3GHOLane_20250814.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IUpgradeableBurnMintTokenPool_1_5_1} from 'src/interfaces/ccip/tokenPool/IUpgradeableBurnMintTokenPool.sol';
+import {AaveV3GHOLane} from '../abstraction/AaveV3GHOLane.sol';
+import {GhoCCIPChains} from '../abstraction/constants/GhoCCIPChains.sol';
+
+/**
+ * @title Arbitrum<>Ink GHO CCIP Lane
+ * @author Aave Labs
+ * - Discussion: https://governance.aave.com/t/arfc-launch-gho-on-ink-set-aci-as-emissions-manager-for-rewards/22664
+ */
+contract Arbitrum_Ink_AaveV3GHOLane_20250814 is AaveV3GHOLane {
+  constructor() AaveV3GHOLane(GhoCCIPChains.ARBITRUM()) {}
+
+  function lanesToAdd()
+    public
+    pure
+    override
+    returns (IUpgradeableBurnMintTokenPool_1_5_1.ChainUpdate[] memory)
+  {
+    return _asArray(_asChainUpdateWithDefaultRateLimiterConfig(GhoCCIPChains.INK()));
+  }
+}
