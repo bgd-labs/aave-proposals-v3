40d07e32bc6ecab47fd50e37067bb7ef9623e873
Alan Donoso Naumczuk <adonoso@itba.edu.ar>
2025-08-19 16:08:15 -0300

    feat: GHO launch abstraction code added

---
+++ src/20250814_Multi_GHOInkLaunch/abstraction/constants/CCIPChainTokenAdminRegistries.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library CCIPChainTokenAdminRegistries {
+  address constant ARBITRUM = 0x39AE1032cF4B334a1Ed41cdD0833bdD7c7E7751E;
+  address constant BASE = 0x6f6C373d09C07425BaAE72317863d7F6bb731e37;
+  address constant ETHEREUM = 0xb22764f98dD05c789929716D677382Df22C05Cb6;
+  address constant AVALANCHE = 0xc8df5D618c6a59Cc6A311E96a39450381001464F;
+  address constant GNOSIS = 0x73BC11423CBF14914998C23B0aFC9BE0cb5B2229;
+  address constant INK = 0xEb062d21c713A3d940BB0FaECFdC387d6Ea23697;
+}
