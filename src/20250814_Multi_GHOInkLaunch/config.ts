8b2e09a420e60d469fbe983403572562f7645bd5
Alan Donoso Naumczuk <adonoso@itba.edu.ar>
2025-08-19 16:10:47 -0300

    feat: Listing (not ready yet) and config.ts files added

---
+++ src/20250814_Multi_GHOInkLaunch/config.ts
@@ -0,0 +1,61 @@
+import {ConfigFile, VOTING_NETWORK} from '../../generator/types';
+export const config: ConfigFile = {
+  rootOptions: {
+    pools: [
+      'AaveV3Ethereum',
+      'AaveV3Arbitrum',
+      'AaveV3Base',
+      'AaveV3Avalanche',
+      'AaveV3Gnosis',
+      //   'AaveV3Ink', // TODO: AaveV3Ink type needed
+    ],
+    title: 'Launch GHO on Ink',
+    shortName: 'GHOInkLaunch',
+    date: '20250814',
+    author: 'Aave Labs',
+    discussion:
+      'https://governance.aave.com/t/arfc-launch-gho-on-ink-set-aci-as-emissions-manager-for-rewards/22664',
+    snapshot: '',
+    votingNetwork: VOTING_NETWORK.POLYGON,
+  },
+  poolOptions: {
+    AaveV3Avalanche: {configs: {OTHERS: {}}, cache: {blockNumber: 67272548}},
+    AaveV3Arbitrum: {configs: {OTHERS: {}}, cache: {blockNumber: 369812065}},
+    AaveV3Base: {configs: {OTHERS: {}}, cache: {blockNumber: 34377046}},
+    AaveV3Ethereum: {configs: {OTHERS: {}}, cache: {blockNumber: 23169876}},
+    AaveV3Gnosis: {configs: {OTHERS: {}}, cache: {blockNumber: 41670939}},
+    // AaveV3Ink: { // TODO: AaveV3Ink type needed
+    //   configs: {
+    //     ASSET_LISTING: [
+    //       {
+    //         assetSymbol: 'GHO',
+    //         decimals: 18,
+    //         priceFeed: '0x20fd5f3FCac8883a3A0A2bBcD658A2d2c6EFa6B6',
+    //         ltv: '0',
+    //         liqThreshold: '0',
+    //         liqBonus: '0',
+    //         debtCeiling: '0',
+    //         liqProtocolFee: '0',
+    //         enabledToBorrow: 'ENABLED',
+    //         flashloanable: 'ENABLED',
+    //         borrowableInIsolation: 'DISABLED',
+    //         withSiloedBorrowing: 'DISABLED',
+    //         reserveFactor: '10',
+    //         supplyCap: '5000000',
+    //         borrowCap: '4500000',
+    //         rateStrategyParams: {
+    //           optimalUtilizationRate: '90',
+    //           baseVariableBorrowRate: '0',
+    //           variableRateSlope1: '5.5',
+    //           variableRateSlope2: '50',
+    //         },
+    //         asset: '0xfc421aD3C883Bf9E7C4f42dE845C4e4405799e73',
+    //         admin: '0xac140648435d03f784879cd789130F22Ef588Fcd', // Emission Admin - ACI multisig address
+    //         eModeCategory: '',
+    //       },
+    //     ],
+    //   },
+    //   cache: {blockNumber: 22045090},
+    // },
+  },
+};
