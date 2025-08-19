bae71561b97ec2ac26c7896ae9628e5afc646b38
Alan Donoso Naumczuk <adonoso@itba.edu.ar>
2025-08-19 16:11:56 -0300

    feat: Ink GHO Launch - CCIP lanes

---
+++ src/20250814_Multi_GHOInkLaunch/AaveV3Ink_GHOInkLaunch_20250814.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IUpgradeableBurnMintTokenPool_1_5_1} from 'src/interfaces/ccip/tokenPool/IUpgradeableBurnMintTokenPool.sol';
+import {AaveV3GHOLaunch} from './abstraction/AaveV3GHOLaunch.sol';
+import {GhoCCIPChains} from './abstraction/constants/GhoCCIPChains.sol';
+
+/**
+ * @title GHO Ink Launch
+ * @author Aave Labs
+ * - Discussion: https://governance.aave.com/t/arfc-launch-gho-on-ink-set-aci-as-emissions-manager-for-rewards/22664
+ */
+contract AaveV3Ink_GHOInkLaunch_20250814 is AaveV3GHOLaunch {
+  constructor() AaveV3GHOLaunch(GhoCCIPChains.INK()) {}
+
+  function lanesToAdd()
+    public
+    pure
+    override
+    returns (IUpgradeableBurnMintTokenPool_1_5_1.ChainUpdate[] memory)
+  {
+    IUpgradeableBurnMintTokenPool_1_5_1.ChainUpdate[]
+      memory chainsToAdd = new IUpgradeableBurnMintTokenPool_1_5_1.ChainUpdate[](5);
+
+    chainsToAdd[0] = _asChainUpdateWithDefaultRateLimiterConfig(GhoCCIPChains.ETHEREUM());
+    chainsToAdd[1] = _asChainUpdateWithDefaultRateLimiterConfig(GhoCCIPChains.ARBITRUM());
+    chainsToAdd[2] = _asChainUpdateWithDefaultRateLimiterConfig(GhoCCIPChains.BASE());
+    chainsToAdd[3] = _asChainUpdateWithDefaultRateLimiterConfig(GhoCCIPChains.AVALANCHE());
+    chainsToAdd[4] = _asChainUpdateWithDefaultRateLimiterConfig(GhoCCIPChains.GNOSIS());
+
+    return chainsToAdd;
+  }
+
+  function _setupGhoAaveSteward() internal override {
+    // Do not setup Aave Core Steward, it will be done through the Aave V3 Ink activation proposal.
+  }
+}
