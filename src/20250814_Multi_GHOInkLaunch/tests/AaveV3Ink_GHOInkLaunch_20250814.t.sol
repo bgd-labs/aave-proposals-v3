ceaf4c117f5f613e2e02cfba70c4c115061048ed
Alan Donoso Naumczuk <adonoso@itba.edu.ar>
2025-08-28 23:45:43 -0300

    test: Temporarily mock the administrator proposal until done on Ink

--- src/20250814_Multi_GHOInkLaunch/tests/AaveV3Ink_GHOInkLaunch_20250814.t.sol
+++ src/20250814_Multi_GHOInkLaunch/tests/AaveV3Ink_GHOInkLaunch_20250814.t.sol
@@ -6,9 +6,40 @@ import {GhoCCIPChains} from '../abstraction/constants/GhoCCIPChains.sol';
 import {AaveV3GHOLane} from '../abstraction/AaveV3GHOLane.sol';
 import {AaveV3Ink_GHOInkLaunch_20250814} from '../AaveV3Ink_GHOInkLaunch_20250814.sol';
 import {AaveV3GHOLaunchTest_PostExecution, AaveV3GHOLaunchTest_PreExecution} from '../abstraction/tests/AaveV3GHOLaunchTest.sol';
+import {CCIPChainTokenAdminRegistries} from '../abstraction/constants/CCIPChainTokenAdminRegistries.sol';
+import {ITokenAdminRegistry} from 'src/interfaces/ccip/ITokenAdminRegistry.sol';
+import {GovV3Helpers} from 'aave-helpers/src/GovV3Helpers.sol';
+import {GovernanceV3Ink} from 'aave-address-book/GovernanceV3Ink.sol';
 
 contract AaveV3Ink_GHOInkLaunch_20250814_PreExecution is AaveV3GHOLaunchTest_PreExecution {
   constructor() AaveV3GHOLaunchTest_PreExecution(GhoCCIPChains.INK(), 'ink', 22331165) {}
+
+  function setUp() public virtual override {
+    super.setUp();
+  }
+
+  function _executePayload() internal virtual override {
+    _mockAdministratorProposal();
+    GovV3Helpers.executePayload(
+      vm,
+      address(proposal),
+      address(GovernanceV3Ink.PAYLOADS_CONTROLLER)
+    );
+  }
+
+  // TODO: Temporary mock until done on Ink
+  function _mockAdministratorProposal() internal {
+    vm.startPrank(ITokenAdminRegistry(CCIPChainTokenAdminRegistries.INK).owner());
+    ITokenAdminRegistry(CCIPChainTokenAdminRegistries.INK).proposeAdministrator(
+      GhoCCIPChains.INK().ghoToken,
+      GhoCCIPChains.INK().owner
+    );
+    vm.stopPrank();
+  }
+
+  function test_defaultProposalExecution() public virtual override {
+    vm.skip(true); // TODO: Not using the proper payload controller - Change needed at aave-helpers for default test
+  }
 
   function _deployAaveV3GHOLaneProposal() internal virtual override returns (AaveV3GHOLane) {
     return new AaveV3Ink_GHOInkLaunch_20250814();
@@ -75,6 +106,25 @@ contract AaveV3Ink_GHOInkLaunch_20250814_PreExecution is AaveV3GHOLaunchTest_Pre
 
 contract AaveV3Ink_GHOInkLaunch_20250814_PostExecution is AaveV3GHOLaunchTest_PostExecution {
   constructor() AaveV3GHOLaunchTest_PostExecution(GhoCCIPChains.INK(), 'ink', 22331165) {}
+
+  function _executePayload() internal virtual override {
+    _mockAdministratorProposal();
+    GovV3Helpers.executePayload(
+      vm,
+      address(proposal),
+      address(GovernanceV3Ink.PAYLOADS_CONTROLLER)
+    );
+  }
+
+  // TODO: Temporary mock until done on Ink
+  function _mockAdministratorProposal() internal {
+    vm.startPrank(ITokenAdminRegistry(CCIPChainTokenAdminRegistries.INK).owner());
+    ITokenAdminRegistry(CCIPChainTokenAdminRegistries.INK).proposeAdministrator(
+      GhoCCIPChains.INK().ghoToken,
+      GhoCCIPChains.INK().owner
+    );
+    vm.stopPrank();
+  }
 
   function _aavePool() internal view virtual override returns (address) {
     return GHOInkLaunchConstants.POOL;
