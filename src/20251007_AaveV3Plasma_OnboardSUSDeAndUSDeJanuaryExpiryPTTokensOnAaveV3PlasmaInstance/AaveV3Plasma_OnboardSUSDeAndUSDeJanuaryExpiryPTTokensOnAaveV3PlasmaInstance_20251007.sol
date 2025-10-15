cb9260cfe15d07398d47656698483ac56c7ac556
Nandy Bâ <nandyba@yahoo.fr>
2025-10-15 15:08:34 +0800

    PT sUSDe and USDe on Plasma (#853)
    * Onboard USDe & sUSDe PT token on plasma
    
    * Add AIP description
    
    * Update AaveV3Plasma_OnboardSUSDeAndUSDeJanuaryExpiryPTTokensOnAaveV3PlasmaInstance_20251007.sol
    
    fix: risk steward role were swapped
    
    * new edge contract but cooler
    
    * rerun test
    
    * Apply suggestions from code review
    
    Co-authored-by: Harsh Pandey <harshsatishpandey@gmail.com>
    
    * Use AutomationCompatibleInterface instead of ExecutionChainRobotKeeper
    
    * cleanup MD file
    
    * Update src/20251007_AaveV3Plasma_OnboardSUSDeAndUSDeJanuaryExpiryPTTokensOnAaveV3PlasmaInstance/OnboardSUSDeAndUSDeJanuaryExpiryPTTokensOnAaveV3PlasmaInstance.md
    
    Co-authored-by: Harsh Pandey <harshsatishpandey@gmail.com>
    
    * use EDGE address from address-book
    
    * use address from address book in test
    
    ---------
    
    Co-authored-by: Rozengarden <git@rozengarden.eu>
    Co-authored-by: Alice <121383428+Rozengarden@users.noreply.github.com>
    Co-authored-by: Harsh Pandey <harshsatishpandey@gmail.com>

---
+++ src/20251007_AaveV3Plasma_OnboardSUSDeAndUSDeJanuaryExpiryPTTokensOnAaveV3PlasmaInstance/AaveV3Plasma_OnboardSUSDeAndUSDeJanuaryExpiryPTTokensOnAaveV3PlasmaInstance_20251007.sol
@@ -0,0 +1,232 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {AaveV3Plasma, AaveV3PlasmaAssets} from 'aave-address-book/AaveV3Plasma.sol';
+import {AaveV3PayloadPlasma} from 'aave-helpers/src/v3-config-engine/AaveV3PayloadPlasma.sol';
+import {EngineFlags} from 'aave-v3-origin/contracts/extensions/v3-config-engine/EngineFlags.sol';
+import {IAaveV3ConfigEngine, IPool} from 'aave-v3-origin/contracts/extensions/v3-config-engine/IAaveV3ConfigEngine.sol';
+import {IERC20} from 'openzeppelin-contracts/contracts/token/ERC20/IERC20.sol';
+import {SafeERC20} from 'openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol';
+import {IEmissionManager} from 'aave-v3-origin/contracts/rewards/interfaces/IEmissionManager.sol';
+import {IAaveStewardInjector} from '../interfaces/IAaveStewardInjector.sol';
+/**
+ * @title Onboard sUSDe and USDe January expiry PT tokens on Aave V3 Plasma Instance
+ * @author Aave_chan Initiative
+ * - Snapshot: direct-to-aip
+ * - Discussion: https://governance.aave.com/t/direct-to-aip-onboard-susde-and-usde-january-expiry-pt-tokens-on-aave-v3-plasma-instance/23196
+ */
+contract AaveV3Plasma_OnboardSUSDeAndUSDeJanuaryExpiryPTTokensOnAaveV3PlasmaInstance_20251007 is
+  AaveV3PayloadPlasma
+{
+  using SafeERC20 for IERC20;
+  address public constant EDGE_INJECTOR_PENDLE_EMODE = AaveV3Plasma.EDGE_INJECTOR_PENDLE_EMODE;
+  address public constant EDGE_INJECTOR_DISCOUNT_RATE = AaveV3Plasma.EDGE_INJECTOR_DISCOUNT_RATE;
+  address public constant EDGE_RISK_STEWARD_PENDLE_EMODE =
+    AaveV3Plasma.EDGE_RISK_STEWARD_PENDLE_EMODE;
+  address public constant EDGE_RISK_STEWARD_DISCOUNT_RATE =
+    AaveV3Plasma.EDGE_RISK_STEWARD_DISCOUNT_RATE;
+
+  address public constant PT_USDe_15JAN2026 = 0x93B544c330F60A2aa05ceD87aEEffB8D38FD8c9a;
+  uint256 public constant PT_USDe_15JAN2026_SEED_AMOUNT = 100e18;
+  address public constant PT_USDe_15JAN2026_LM_ADMIN = 0xac140648435d03f784879cd789130F22Ef588Fcd;
+
+  address public constant PT_sUSDE_15JAN2026 = 0x02FCC4989B4C9D435b7ceD3fE1Ba4CF77BBb5Dd8;
+  uint256 public constant PT_sUSDE_15JAN2026_SEED_AMOUNT = 100e18;
+  address public constant PT_sUSDE_15JAN2026_LM_ADMIN = 0xac140648435d03f784879cd789130F22Ef588Fcd;
+
+  function _postExecute() internal override {
+    // onboard new risk steward for ARGS
+    AaveV3Plasma.ACL_MANAGER.addRiskAdmin(EDGE_RISK_STEWARD_DISCOUNT_RATE);
+    AaveV3Plasma.ACL_MANAGER.addRiskAdmin(EDGE_RISK_STEWARD_PENDLE_EMODE);
+    // we whitelist the four newly created eModeId on the injector
+    uint8 nextID = _findFirstUnusedEmodeCategory(AaveV3Plasma.POOL);
+    address[] memory marketsToWhitelist = new address[](4);
+    marketsToWhitelist[0] = address(uint160(nextID - 1)); // on the injector we encode eModeId to address
+    marketsToWhitelist[1] = address(uint160(nextID - 2));
+    marketsToWhitelist[2] = address(uint160(nextID - 3));
+    marketsToWhitelist[3] = address(uint160(nextID - 4));
+    IAaveStewardInjector(EDGE_INJECTOR_PENDLE_EMODE).addMarkets(marketsToWhitelist);
+
+    address[] memory assetToWhitelist = new address[](2);
+    assetToWhitelist[0] = PT_USDe_15JAN2026;
+    assetToWhitelist[1] = PT_sUSDE_15JAN2026;
+    IAaveStewardInjector(EDGE_INJECTOR_DISCOUNT_RATE).addMarkets(assetToWhitelist);
+
+    IERC20(PT_USDe_15JAN2026).forceApprove(
+      address(AaveV3Plasma.POOL),
+      PT_USDe_15JAN2026_SEED_AMOUNT
+    );
+    AaveV3Plasma.POOL.supply(
+      PT_USDe_15JAN2026,
+      PT_USDe_15JAN2026_SEED_AMOUNT,
+      AaveV3Plasma.DUST_BIN,
+      0
+    );
+
+    address aPT_USDe_15JAN2026 = AaveV3Plasma.POOL.getReserveAToken(PT_USDe_15JAN2026);
+    IEmissionManager(AaveV3Plasma.EMISSION_MANAGER).setEmissionAdmin(
+      PT_USDe_15JAN2026,
+      PT_USDe_15JAN2026_LM_ADMIN
+    );
+    IEmissionManager(AaveV3Plasma.EMISSION_MANAGER).setEmissionAdmin(
+      aPT_USDe_15JAN2026,
+      PT_USDe_15JAN2026_LM_ADMIN
+    );
+
+    IERC20(PT_sUSDE_15JAN2026).forceApprove(
+      address(AaveV3Plasma.POOL),
+      PT_sUSDE_15JAN2026_SEED_AMOUNT
+    );
+    AaveV3Plasma.POOL.supply(
+      PT_sUSDE_15JAN2026,
+      PT_sUSDE_15JAN2026_SEED_AMOUNT,
+      AaveV3Plasma.DUST_BIN,
+      0
+    );
+
+    address aPT_sUSDE_15JAN2026 = AaveV3Plasma.POOL.getReserveAToken(PT_sUSDE_15JAN2026);
+    IEmissionManager(AaveV3Plasma.EMISSION_MANAGER).setEmissionAdmin(
+      PT_sUSDE_15JAN2026,
+      PT_sUSDE_15JAN2026_LM_ADMIN
+    );
+    IEmissionManager(AaveV3Plasma.EMISSION_MANAGER).setEmissionAdmin(
+      aPT_sUSDE_15JAN2026,
+      PT_sUSDE_15JAN2026_LM_ADMIN
+    );
+  }
+
+  function eModeCategoryCreations()
+    public
+    pure
+    override
+    returns (IAaveV3ConfigEngine.EModeCategoryCreation[] memory)
+  {
+    IAaveV3ConfigEngine.EModeCategoryCreation[]
+      memory eModeCreations = new IAaveV3ConfigEngine.EModeCategoryCreation[](4);
+
+    address[] memory collateralAssets_PT_USDe_15JAN2026Stablecoin = new address[](1);
+    address[] memory borrowableAssets_PT_USDe_15JAN2026Stablecoin = new address[](2);
+
+    collateralAssets_PT_USDe_15JAN2026Stablecoin[0] = PT_USDe_15JAN2026;
+    borrowableAssets_PT_USDe_15JAN2026Stablecoin[0] = AaveV3PlasmaAssets.USDT0_UNDERLYING;
+    borrowableAssets_PT_USDe_15JAN2026Stablecoin[1] = AaveV3PlasmaAssets.USDe_UNDERLYING;
+
+    eModeCreations[0] = IAaveV3ConfigEngine.EModeCategoryCreation({
+      ltv: 85_40,
+      liqThreshold: 87_40,
+      liqBonus: 4_90,
+      label: 'PT-USDe Stablecoins Jan 2026',
+      collaterals: collateralAssets_PT_USDe_15JAN2026Stablecoin,
+      borrowables: borrowableAssets_PT_USDe_15JAN2026Stablecoin
+    });
+
+    address[] memory collateralAssets_PT_USDe_15JAN2026USDe = new address[](1);
+    address[] memory borrowableAssets_PT_USDe_15JAN2026USDe = new address[](1);
+
+    collateralAssets_PT_USDe_15JAN2026USDe[0] = PT_USDe_15JAN2026;
+    borrowableAssets_PT_USDe_15JAN2026USDe[0] = AaveV3PlasmaAssets.USDe_UNDERLYING;
+
+    eModeCreations[1] = IAaveV3ConfigEngine.EModeCategoryCreation({
+      ltv: 86_20,
+      liqThreshold: 88_20,
+      liqBonus: 3_90,
+      label: 'PT-USDe USDe Jan 2026',
+      collaterals: collateralAssets_PT_USDe_15JAN2026USDe,
+      borrowables: borrowableAssets_PT_USDe_15JAN2026USDe
+    });
+
+    address[] memory collateralAssets_PT_sUSDe_15JAN2026Stablecoin = new address[](1);
+    address[] memory borrowableAssets_PT_sUSDe_15JAN2026Stablecoin = new address[](2);
+
+    collateralAssets_PT_sUSDe_15JAN2026Stablecoin[0] = PT_sUSDE_15JAN2026;
+    borrowableAssets_PT_sUSDe_15JAN2026Stablecoin[0] = AaveV3PlasmaAssets.USDT0_UNDERLYING;
+    borrowableAssets_PT_sUSDe_15JAN2026Stablecoin[1] = AaveV3PlasmaAssets.USDe_UNDERLYING;
+
+    eModeCreations[2] = IAaveV3ConfigEngine.EModeCategoryCreation({
+      ltv: 83_90,
+      liqThreshold: 85_90,
+      liqBonus: 6_00,
+      label: 'PT-sUSDe Stablecoins Jan 2026',
+      collaterals: collateralAssets_PT_sUSDe_15JAN2026Stablecoin,
+      borrowables: borrowableAssets_PT_sUSDe_15JAN2026Stablecoin
+    });
+
+    address[] memory collateralAssets_PT_sUSDe_15JAN2026USDe = new address[](1);
+    address[] memory borrowableAssets_PT_sUSDe_15JAN2026USDe = new address[](1);
+
+    collateralAssets_PT_sUSDe_15JAN2026USDe[0] = PT_sUSDE_15JAN2026;
+    borrowableAssets_PT_sUSDe_15JAN2026USDe[0] = AaveV3PlasmaAssets.USDe_UNDERLYING;
+
+    eModeCreations[3] = IAaveV3ConfigEngine.EModeCategoryCreation({
+      ltv: 84_50,
+      liqThreshold: 86_50,
+      liqBonus: 5_20,
+      label: 'PT-sUSDe USDe Jan 2026',
+      collaterals: collateralAssets_PT_sUSDe_15JAN2026USDe,
+      borrowables: borrowableAssets_PT_sUSDe_15JAN2026USDe
+    });
+
+    return eModeCreations;
+  }
+
+  function newListings() public pure override returns (IAaveV3ConfigEngine.Listing[] memory) {
+    IAaveV3ConfigEngine.Listing[] memory listings = new IAaveV3ConfigEngine.Listing[](2);
+
+    listings[0] = IAaveV3ConfigEngine.Listing({
+      asset: PT_USDe_15JAN2026,
+      assetSymbol: 'PT_USDe_15JAN2026',
+      priceFeed: 0x30cb6ff8649Cc02cEa91971D4730EebeD5A8D2F1,
+      enabledToBorrow: EngineFlags.DISABLED,
+      borrowableInIsolation: EngineFlags.DISABLED,
+      withSiloedBorrowing: EngineFlags.DISABLED,
+      flashloanable: EngineFlags.ENABLED,
+      ltv: 5,
+      liqThreshold: 10,
+      liqBonus: 7_50,
+      reserveFactor: 45_00,
+      supplyCap: 200_000_000,
+      borrowCap: 1,
+      debtCeiling: 0,
+      liqProtocolFee: 10_00,
+      rateStrategyParams: IAaveV3ConfigEngine.InterestRateInputData({
+        optimalUsageRatio: 45_00,
+        baseVariableBorrowRate: 0,
+        variableRateSlope1: 10_00,
+        variableRateSlope2: 300_00
+      })
+    });
+    listings[1] = IAaveV3ConfigEngine.Listing({
+      asset: PT_sUSDE_15JAN2026,
+      assetSymbol: 'PT_sUSDE_15JAN2026',
+      priceFeed: 0x3eca1c7836eA09DB3dc85be7B5526Ce80E2609a1,
+      enabledToBorrow: EngineFlags.DISABLED,
+      borrowableInIsolation: EngineFlags.DISABLED,
+      withSiloedBorrowing: EngineFlags.DISABLED,
+      flashloanable: EngineFlags.ENABLED,
+      ltv: 5,
+      liqThreshold: 10,
+      liqBonus: 7_50,
+      reserveFactor: 45_00,
+      supplyCap: 200_000_000,
+      borrowCap: 1,
+      debtCeiling: 0,
+      liqProtocolFee: 10_00,
+      rateStrategyParams: IAaveV3ConfigEngine.InterestRateInputData({
+        optimalUsageRatio: 45_00,
+        baseVariableBorrowRate: 0,
+        variableRateSlope1: 10_00,
+        variableRateSlope2: 300_00
+      })
+    });
+
+    return listings;
+  }
+
+  function _findFirstUnusedEmodeCategory(IPool pool) private view returns (uint8) {
+    // eMode id 0 is skipped intentionally as it is the reserved default
+    for (uint8 i = 1; i < 256; i++) {
+      if (pool.getEModeCategoryCollateralConfig(i).liquidationThreshold == 0) return i;
+    }
+    revert('NoAvailableEmodeCategory');
+  }
+}
---
+++ src/20251007_AaveV3Plasma_OnboardSUSDeAndUSDeJanuaryExpiryPTTokensOnAaveV3PlasmaInstance/OnboardSUSDeAndUSDeJanuaryExpiryPTTokensOnAaveV3PlasmaInstance.md
@@ -0,0 +1,122 @@
+---
+title: "Onboard sUSDe and USDe January expiry PT tokens on Aave V3 Plasma Instance"
+author: "Aave_chan Initiative"
+discussions: "https://governance.aave.com/t/direct-to-aip-onboard-susde-and-usde-january-expiry-pt-tokens-on-aave-v3-plasma-instance/23196"
+---
+
+## Simple Summary
+
+This AIP proposes to onboard sUSDe and USDe January expiry PT tokens on Aave V3 Plasma Instance.
+
+## Motivation
+
+The previous Ethena (sUSDe and USDe) PT tokens that were onboarded have brought significant inflows to Aave, given the success of the Plasma instance launch along with the significant USDe and sUSDe liquidity on this instance we believe that onboarding Ethena PT tokens there is a logical next step.
+
+## Specification
+
+The table below illustrates the configured risk parameters for **PT_USDe_15JAN2026** and **PT_sUSDE_15JAN2026**
+
+| Parameter                 | PT_USDe_15JAN2026 | PT_sUSDE_15JAN2026 |
+| ------------------------- | ----------------: | -----------------: |
+| Isolation Mode            |             false |              false |
+| Borrowable                |          DISABLED |           DISABLED |
+| Collateral Enabled        |              true |               true |
+| Supply Cap                |       200,000,000 |        200,000,000 |
+| Borrow Cap                |                 1 |                  1 |
+| Debt Ceiling              |             USD 0 |              USD 0 |
+| LTV                       |            0.05 % |             0.05 % |
+| LT                        |             0.1 % |              0.1 % |
+| Liquidation Bonus         |             7.5 % |              7.5 % |
+| Liquidation Protocol Fee  |              10 % |               10 % |
+| Reserve Factor            |              45 % |               45 % |
+| Base Variable Borrow Rate |               0 % |                0 % |
+| Variable Slope 1          |              10 % |               10 % |
+| Variable Slope 2          |             300 % |              300 % |
+| Uoptimal                  |              45 % |               45 % |
+| Flashloanable             |           ENABLED |            ENABLED |
+| Siloed Borrowing          |          DISABLED |           DISABLED |
+| Borrowable in Isolation   |          DISABLED |           DISABLED |
+
+**Pricefeed details**
+
+| Parameter              |                                                                                                             PT_USDe_15JAN2026 |                                                                                                             PT_sUSDE_15JAN2026 |
+| ---------------------- | ----------------------------------------------------------------------------------------------------------------------------: | -----------------------------------------------------------------------------------------------------------------------------: |
+| Oracle                 | [PT Capped USDe USDT/USD linear discount 15JAN2026](https://plasmascan.to/address/0x30cb6ff8649Cc02cEa91971D4730EebeD5A8D2F1) | [PT Capped sUSDe USDT/USD linear discount 15JAN2026](https://plasmascan.to/address/0x3eca1c7836eA09DB3dc85be7B5526Ce80E2609a1) |
+| Underlying Oracle      |                                   [Capped USDT/USD](https://plasmascan.to/address/0xdBbB0b5DD13E7AC9C56624834ef193df87b022c3) |                                    [Capped USDT/USD](https://plasmascan.to/address/0xdBbB0b5DD13E7AC9C56624834ef193df87b022c3) |
+| Underlying Oracle      |                                [Chainlink USDT/USD](https://plasmascan.to/address/0x70b77FcdbE2293423e41AdD2FB599808396807BC) |                                 [Chainlink USDT/USD](https://plasmascan.to/address/0x70b77FcdbE2293423e41AdD2FB599808396807BC) |
+| Oracle Latest Answer   |                                                                                                    (2025-10-08) USD .97880894 |                                                                                                     (2025-10-08) USD .97767085 |
+| discountRatePerYear    |                                                                                                                         7.96% |                                                                                                                          8.38% |
+| maxDiscountRatePerYear |                                                                                                                        33.88% |                                                                                                                         36.82% |
+
+Additionally [0xac140648435d03f784879cd789130F22Ef588Fcd](https://plasmascan.to/address/0xac140648435d03f784879cd789130F22Ef588Fcd) has been set as the emission admin for PT_sUSDE_15JAN2026, PT_USDe_15JAN2026 and the corresponding aTokens.
+
+### E-mode
+
+**PT-USDe/Stablecoins E-mode**
+
+| **Asset**         | **PT-USDe-15JAN2026**  | **USDT0** | **USDe** |
+| ----------------- | ---------------------- | --------- | -------- |
+| Collateral        | Yes                    | No        | No       |
+| Borrowable        | No                     | Yes       | Yes      |
+| LTV               | Subject to Risk Oracle | -         | -        |
+| LT                | Subject to Risk Oracle | -         | -        |
+| Liquidation Bonus | Subject to Risk Oracle | -         | -        |
+
+**PT-USDe/USDe E-mode**
+
+| **Asset**         | **PT-USDe-15JAN2026**  | **USDe** |
+| ----------------- | ---------------------- | -------- |
+| Collateral        | Yes                    | No       |
+| Borrowable        | No                     | Yes      |
+| LTV               | Subject to Risk Oracle | -        |
+| LT                | Subject to Risk Oracle | -        |
+| Liquidation Bonus | Subject to Risk Oracle | -        |
+
+**Initial E-mode Risk Oracle for PT-USDe-15JAN2026**
+
+| **Parameter** | **Value**   | **Value** |
+| ------------- | ----------- | --------- |
+| E-Mode        | Stablecoins | USDe      |
+| LTV           | 85.4%       | 86.2%     |
+| LT            | 87.4%       | 88.2%     |
+| LB            | 4.9%        | 3.9%      |
+
+**PT-sUSDe/Stablecoins E-mode**
+
+| **Asset**         | **PT-sUSDe-15JAN2026** | **USDT0** | **USDe** |
+| ----------------- | ---------------------- | --------- | -------- |
+| Collateral        | Yes                    | No        | No       |
+| Borrowable        | No                     | Yes       | Yes      |
+| LTV               | Subject to Risk Oracle | -         | -        |
+| LT                | Subject to Risk Oracle | -         | -        |
+| Liquidation Bonus | Subject to Risk Oracle | -         | -        |
+
+**PT-sUSDe/USDe E-mode**
+
+| **Asset**         | **PT-sUSDe-15JAN2026** | **USDe** |
+| ----------------- | ---------------------- | -------- |
+| Collateral        | Yes                    | No       |
+| Borrowable        | No                     | Yes      |
+| LTV               | Subject to Risk Oracle | -        |
+| LT                | Subject to Risk Oracle | -        |
+| Liquidation Bonus | Subject to Risk Oracle | -        |
+
+**Initial E-mode Risk Oracle for PT-sUSDe-15JAN2026**
+
+| **Parameter** | **Value**   | **Value** |
+| ------------- | ----------- | --------- |
+| E-Mode        | Stablecoins | USDe      |
+| LTV           | 83.9%       | 84.5%     |
+| LT            | 85.9%       | 86.5%     |
+| LB            | 6.0%        | 5.2%      |
+
+## References
+
+- Implementation: [AaveV3Plasma](https://github.com/bgd-labs/aave-proposals-v3/blob/main/src/20251007_AaveV3Plasma_OnboardSUSDeAndUSDeJanuaryExpiryPTTokensOnAaveV3PlasmaInstance/AaveV3Plasma_OnboardSUSDeAndUSDeJanuaryExpiryPTTokensOnAaveV3PlasmaInstance_20251007.sol)
+- Tests: [AaveV3Plasma](https://github.com/bgd-labs/aave-proposals-v3/blob/main/src/20251007_AaveV3Plasma_OnboardSUSDeAndUSDeJanuaryExpiryPTTokensOnAaveV3PlasmaInstance/AaveV3Plasma_OnboardSUSDeAndUSDeJanuaryExpiryPTTokensOnAaveV3PlasmaInstance_20251007.t.sol)
+- Snapshot: Direct-to-AIP
+- [Discussion](https://governance.aave.com/t/direct-to-aip-onboard-susde-and-usde-january-expiry-pt-tokens-on-aave-v3-plasma-instance/23196)
+
+## Copyright
+
+Copyright and related rights waived via [CC0](https://creativecommons.org/publicdomain/zero/1.0/).
