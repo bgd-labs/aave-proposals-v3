b42f1cd9bdd1c12f4c3c3a4cbcd722ce8ee98a48
efecarranza <fcarr004@fiu.edu>
2025-10-29 12:03:45 -0300

    feat: stk tokens updates

---
+++ src/20251029_AaveV3Ethereum_SafetyModuleEmissionsUpdate/AaveV3Ethereum_SafetyModuleEmissionsUpdate_20251029.sol
@@ -0,0 +1,105 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IERC20} from 'openzeppelin-contracts/contracts/token/ERC20/IERC20.sol';
+import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
+import {MiscEthereum} from 'aave-address-book/MiscEthereum.sol';
+import {AaveSafetyModule} from 'aave-address-book/AaveSafetyModule.sol';
+import {IStakeToken} from 'aave-address-book/common/IStakeToken.sol';
+import {IProposalGenericExecutor} from 'aave-helpers/src/interfaces/IProposalGenericExecutor.sol';
+
+/**
+ * @title Safety Module Emissions Update
+ * @author @TokenLogic
+ * - Snapshot: Direct-to-AIP
+ * - Discussion: https://governance.aave.com/t/arfc-amend-safety-module-emissions/16640/26
+ */
+contract AaveV3Ethereum_SafetyModuleEmissionsUpdate_20251029 is IProposalGenericExecutor {
+  uint256 public constant DISTRIBUTION_DURATION = 90 days;
+
+  // stkAAVE
+  uint128 public constant AAVE_EMISSION_PER_SECOND_STK_AAVE = uint128(260 ether) / 1 days;
+
+  // stkABPT
+  uint128 public constant AAVE_EMISSION_PER_SECOND_STK_BPT = uint128(130 ether) / 1 days;
+
+  function execute() external {
+    _stkAAVE();
+    _stkABPT();
+    // _stkABPT_V1();
+  }
+
+  function _stkAAVE() internal {
+    // Excess allowance that has not been claimed
+    uint256 allowanceToKeep = IERC20(AaveV3EthereumAssets.AAVE_UNDERLYING).allowance(
+      address(MiscEthereum.ECOSYSTEM_RESERVE),
+      AaveSafetyModule.STK_AAVE
+    );
+
+    uint256 newAllowance = DISTRIBUTION_DURATION * AAVE_EMISSION_PER_SECOND_STK_AAVE;
+
+    IStakeToken.AssetConfigInput[] memory config = new IStakeToken.AssetConfigInput[](1);
+    config[0] = IStakeToken.AssetConfigInput({
+      emissionPerSecond: AAVE_EMISSION_PER_SECOND_STK_AAVE,
+      totalStaked: 0, // it's overwritten internally
+      underlyingAsset: AaveSafetyModule.STK_AAVE
+    });
+    IStakeToken(AaveSafetyModule.STK_AAVE).configureAssets(config);
+
+    MiscEthereum.AAVE_ECOSYSTEM_RESERVE_CONTROLLER.approve(
+      MiscEthereum.ECOSYSTEM_RESERVE,
+      AaveV3EthereumAssets.AAVE_UNDERLYING,
+      AaveSafetyModule.STK_AAVE,
+      0
+    );
+
+    MiscEthereum.AAVE_ECOSYSTEM_RESERVE_CONTROLLER.approve(
+      MiscEthereum.ECOSYSTEM_RESERVE,
+      AaveV3EthereumAssets.AAVE_UNDERLYING,
+      AaveSafetyModule.STK_AAVE,
+      newAllowance + allowanceToKeep
+    );
+  }
+
+  function _stkABPT() internal {
+    uint256 newAllowance = DISTRIBUTION_DURATION * AAVE_EMISSION_PER_SECOND_STK_BPT;
+
+    // Excess allowance that has not been claimed
+    uint256 allowanceToKeep = IERC20(AaveV3EthereumAssets.AAVE_UNDERLYING).allowance(
+      address(MiscEthereum.ECOSYSTEM_RESERVE),
+      AaveSafetyModule.STK_AAVE_WSTETH_BPTV2
+    );
+
+    IStakeToken.AssetConfigInput[] memory config = new IStakeToken.AssetConfigInput[](1);
+    config[0] = IStakeToken.AssetConfigInput({
+      emissionPerSecond: AAVE_EMISSION_PER_SECOND_STK_BPT,
+      totalStaked: 0, // it's overwritten internally
+      underlyingAsset: AaveSafetyModule.STK_AAVE_WSTETH_BPTV2
+    });
+    IStakeToken(AaveSafetyModule.STK_AAVE_WSTETH_BPTV2).configureAssets(config);
+    IStakeToken(AaveSafetyModule.STK_AAVE_WSTETH_BPTV2).setDistributionEnd(
+      block.timestamp + DISTRIBUTION_DURATION
+    );
+
+    MiscEthereum.AAVE_ECOSYSTEM_RESERVE_CONTROLLER.approve(
+      MiscEthereum.ECOSYSTEM_RESERVE,
+      AaveV3EthereumAssets.AAVE_UNDERLYING,
+      AaveSafetyModule.STK_AAVE_WSTETH_BPTV2,
+      0
+    );
+
+    MiscEthereum.AAVE_ECOSYSTEM_RESERVE_CONTROLLER.approve(
+      MiscEthereum.ECOSYSTEM_RESERVE,
+      AaveV3EthereumAssets.AAVE_UNDERLYING,
+      AaveSafetyModule.STK_AAVE_WSTETH_BPTV2,
+      newAllowance + allowanceToKeep
+    );
+  }
+
+  function _stkABPT_V1() internal {
+    // Extend distribution end to allow claiming of left-over allowance
+    IStakeToken(AaveSafetyModule.STK_ABPT).setDistributionEnd(
+      block.timestamp + DISTRIBUTION_DURATION
+    );
+  }
+}
---
+++ src/20251029_AaveV3Ethereum_SafetyModuleEmissionsUpdate/SafetyModuleEmissionsUpdate.md
@@ -0,0 +1,38 @@
+---
+title: "Safety Module Emissions Update"
+author: "@TokenLogic"
+discussions: "https://governance.aave.com/t/arfc-amend-safety-module-emissions/16640/26"
+---
+
+## Simple Summary
+
+Re-enable stkBPT incentives emissions for 90 days after previous period ended.
+
+## Motivation
+
+With the conclusion of the recent reward cycle, this proposal aims to renew $AAVE emissions for stkBPT holders. The stkBPT module has proven to be a crucial mechanism as an AAVE supply sink, currently holding a TVL of $1.6B. It is recommended to maintain these emissions in anticipation of upcoming Umbrella developments, with adjustments to be made following the outcomes of the Umbrella upgrade.
+
+More information on stkBPT can be found in the following dashboard, [here](https://dune.com/xmc2/aave-safety-module).
+
+Re-enable distribution for V1 of the staked token so remaining allowance can be claimed.
+
+## Specification
+
+Set the following params for staked tokens:
+
+| Token      | Emissions per day | Duration |
+| ---------- | ----------------- | -------- |
+| stkAAVE    | 260               | -        |
+| stkABPT_V2 | 130               | 90 days  |
+| stkABPT_V1 | -                 | 90 days  |
+
+## References
+
+- Implementation: [AaveV3Ethereum](https://github.com/bgd-labs/aave-proposals-v3/blob/main/src/20251029_AaveV3Ethereum_SafetyModuleEmissionsUpdate/AaveV3Ethereum_SafetyModuleEmissionsUpdate_20251029.sol)
+- Tests: [AaveV3Ethereum](https://github.com/bgd-labs/aave-proposals-v3/blob/main/src/20251029_AaveV3Ethereum_SafetyModuleEmissionsUpdate/AaveV3Ethereum_SafetyModuleEmissionsUpdate_20251029.t.sol)
+- Snapshot: Direct-to-AIP
+- [Discussion](https://governance.aave.com/t/arfc-amend-safety-module-emissions/16640/26)
+
+## Copyright
+
+Copyright and related rights waived via [CC0](https://creativecommons.org/publicdomain/zero/1.0/).
