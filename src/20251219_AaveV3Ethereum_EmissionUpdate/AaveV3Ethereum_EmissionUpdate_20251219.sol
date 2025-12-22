e0001f7f7af01f6241decf034a330689b465f213
Joaquin Failo <73504599+Joafai@users.noreply.github.com>
2025-12-22 01:17:57 -0300

    fixed unused import

--- src/20251219_AaveV3Ethereum_EmissionUpdate/AaveV3Ethereum_EmissionUpdate_20251219.sol
+++ src/20251219_AaveV3Ethereum_EmissionUpdate/AaveV3Ethereum_EmissionUpdate_20251219.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {IERC20} from 'openzeppelin-contracts/contracts/token/ERC20/IERC20.sol';
 import {IProposalGenericExecutor} from 'aave-helpers/src/interfaces/IProposalGenericExecutor.sol';
 import {IStakeToken} from 'aave-helpers/lib/aave-address-book/src/common/IStakeToken.sol';
 import {AaveSafetyModule} from 'aave-address-book/AaveSafetyModule.sol';
