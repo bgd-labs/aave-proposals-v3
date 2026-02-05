f09057201f11fdb13dbe88432b2ea8cd2e7815d9
Nandy Bâ <nandyba@yahoo.fr>
2026-02-05 01:53:15 +0100

    feat: update test to simulate tx with right e-mode id

--- src/20260130_AaveV3Multi_UpdateSyrupUSDCLiquidationProtocolFee/AaveV3Ethereum_UpdateSyrupUSDCLiquidationProtocolFee_20260130.t.sol
+++ src/20260130_AaveV3Multi_UpdateSyrupUSDCLiquidationProtocolFee/AaveV3Ethereum_UpdateSyrupUSDCLiquidationProtocolFee_20260130.t.sol
@@ -5,7 +5,11 @@ import {AaveV3Ethereum} from 'aave-address-book/AaveV3Ethereum.sol';
 
 import 'forge-std/Test.sol';
 import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/src/ProtocolV3TestBase.sol';
+
+import {GovV3Helpers} from 'aave-helpers/src/GovV3Helpers.sol';
 import {AaveV3Ethereum_UpdateSyrupUSDCLiquidationProtocolFee_20260130} from './AaveV3Ethereum_UpdateSyrupUSDCLiquidationProtocolFee_20260130.sol';
+import {AaveV3Ethereum_ListingPTEthenaMay_20260129} from '../20260129_AaveV3Ethereum_ListingPTEthenaMay/AaveV3Ethereum_ListingPTEthenaMay_20260129.sol';
+import {AaveV3Ethereum_OnboardStrataSrUSDePTTokensToV3CoreInstance_20260120} from '../20260120_AaveV3Ethereum_OnboardStrataSrUSDePTTokensToV3CoreInstance/AaveV3Ethereum_OnboardStrataSrUSDePTTokensToV3CoreInstance_20260120.sol';
 
 /**
  * @dev Test for AaveV3Ethereum_Test_20260204
@@ -13,9 +17,15 @@ import {AaveV3Ethereum_UpdateSyrupUSDCLiquidationProtocolFee_20260130} from './A
  */
 contract AaveV3Ethereum_UpdateSyrupUSDCLiquidationProtocolFee_20260130_Test is ProtocolV3TestBase {
   AaveV3Ethereum_UpdateSyrupUSDCLiquidationProtocolFee_20260130 internal proposal;
+  AaveV3Ethereum_ListingPTEthenaMay_20260129 internal ptEthenaProposal;
+  AaveV3Ethereum_OnboardStrataSrUSDePTTokensToV3CoreInstance_20260120 internal strataProposal;
 
   function setUp() public {
     vm.createSelectFork(vm.rpcUrl('mainnet'), 24379660);
+    ptEthenaProposal = new AaveV3Ethereum_ListingPTEthenaMay_20260129();
+    GovV3Helpers.executePayload(vm, address(ptEthenaProposal));
+    strataProposal = new AaveV3Ethereum_OnboardStrataSrUSDePTTokensToV3CoreInstance_20260120();
+    GovV3Helpers.executePayload(vm, address(strataProposal));
     proposal = new AaveV3Ethereum_UpdateSyrupUSDCLiquidationProtocolFee_20260130();
   }
 
