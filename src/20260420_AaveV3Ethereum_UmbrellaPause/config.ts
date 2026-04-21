3344a3c81a1954accf2f324d27acfc0ac44992e1
YBM <31329384+yan-man@users.noreply.github.com>
2026-04-20 20:47:08 -0500

    fix: config

--- src/20260420_AaveV3Ethereum_UmbrellaPause/config.ts
+++ src/20260420_AaveV3Ethereum_UmbrellaPause/config.ts
@@ -6,8 +6,9 @@ export const config: ConfigFile = {
     shortName: 'UmbrellaPause',
     date: '20260420',
     author: 'Aave Labs',
-    discussion: '',
-    snapshot: '',
+    discussion:
+      'https://governance.aave.com/t/direct-to-aip-pause-stkwaweth-umbrella-staked-token-on-ethereum-v3/24595',
+    snapshot: 'direct-to-aip',
     votingNetwork: 'AVALANCHE',
   },
   poolOptions: {AaveV3Ethereum: {configs: {}, cache: {blockNumber: 24921270}}},
