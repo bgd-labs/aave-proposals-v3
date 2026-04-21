bdc5ae600c1b69822ca378d72f97b20aebbf991a
YBM <31329384+yan-man@users.noreply.github.com>
2026-04-21 12:31:30 -0500

    fix: clean up imports

--- src/20260420_AaveV3Ethereum_UmbrellaPause/AaveV3Ethereum_UmbrellaPause_20260420.sol
+++ src/20260420_AaveV3Ethereum_UmbrellaPause/AaveV3Ethereum_UmbrellaPause_20260420.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 import {MiscEthereum} from 'aave-address-book/MiscEthereum.sol';
 import {UmbrellaEthereum, UmbrellaEthereumAssets} from 'aave-address-book/UmbrellaEthereum.sol';
 import {UmbrellaConfiguration} from 'aave-umbrella/umbrella/UmbrellaConfiguration.sol';
-import {IAccessControl} from 'openzeppelin-contracts/contracts/access/IAccessControl.sol';
 import {IProposalGenericExecutor} from 'aave-helpers/src/interfaces/IProposalGenericExecutor.sol';
 
 /**
@@ -15,11 +14,13 @@ import {IProposalGenericExecutor} from 'aave-helpers/src/interfaces/IProposalGen
  */
 contract AaveV3Ethereum_UmbrellaPause_20260420 is IProposalGenericExecutor {
   function execute() external override {
-    address umbrellaAddress = address(UmbrellaEthereum.UMBRELLA);
+    UmbrellaConfiguration umbrellaConfiguration = UmbrellaConfiguration(
+      address(UmbrellaEthereum.UMBRELLA)
+    );
 
     UmbrellaEthereum.UMBRELLA.pauseStk(UmbrellaEthereumAssets.STK_WA_WETH_V1);
-    IAccessControl(umbrellaAddress).grantRole(
-      UmbrellaConfiguration(umbrellaAddress).PAUSE_GUARDIAN_ROLE(),
+    umbrellaConfiguration.grantRole(
+      umbrellaConfiguration.PAUSE_GUARDIAN_ROLE(),
       MiscEthereum.PROTOCOL_GUARDIAN
     );
   }
