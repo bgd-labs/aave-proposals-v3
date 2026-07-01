7b7cc679a07f8476fef2dcd74a9e9db7417c11ed
Alexandru Niculae <43644109+avniculae@users.noreply.github.com>
2026-07-01 15:58:30 +0300

    fix: set snapshot to direct-to-aip in config

--- src/20260623_AaveV3Ethereum_StkGhoMigratorClaimHelper/config.ts
+++ src/20260623_AaveV3Ethereum_StkGhoMigratorClaimHelper/config.ts
@@ -7,7 +7,7 @@ export const config: ConfigFile = {
     date: '20260623',
     author: 'Aave Labs',
     discussion: 'https://governance.aave.com/t/direct-to-aip-stkgho-sgho-migration-tool/25250',
-    snapshot: '',
+    snapshot: 'direct-to-aip',
     votingNetwork: 'AVALANCHE',
   },
   marketOptions: {AaveV3Ethereum: {configs: {OTHERS: {}}, cache: {blockNumber: 25378904}}},
